41762bcbd56acb81648b17210f6d39ea148ffe30
yubathom <yubathom@gmail.com>
2019-09-16 14:21:20 -0300

    Export surge variables

--- prdeploy.sh
+++ prdeploy.sh
@@ -4,8 +4,8 @@ echo “Not a PR. Skipping surge deployment.”
 else
   npm i -g surge
 
-  SURGE_LOGIN=${SURGE_LOGIN}
-  SURGE_TOKEN=${SURGE_TOKEN}
+  export SURGE_LOGIN=${SURGE_LOGIN}
+  export SURGE_TOKEN=${SURGE_TOKEN}
   DEPLOY_DOMAIN=https://postwoman-preview-pr-${TRAVIS_PULL_REQUEST}.surge.sh
 
   surge --project ./dist --domain $DEPLOY_DOMAIN;

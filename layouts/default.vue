5c7f3c282b5492516ffa6037ab62f8bcc8cdfa45
Liyas Thomas <liyasthomas@gmail.com>
2019-12-08 09:20:09 +0530

    :sparkles: Bottom navbar on mobile device, minor UI revamp

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,90 +1,5 @@
 <template>
   <div class="wrapper">
-    <header class="header">
-      <div class="flex-wrap">
-        <span class="slide-in">
-          <nuxt-link :to="localePath('index')">
-            <h1 class="logo">Postwoman</h1>
-          </nuxt-link>
-        </span>
-        <span>
-          <a
-            href="https://github.com/liyasthomas/postwoman"
-            target="_blank"
-            aria-label="GitHub"
-            rel="noopener"
-          >
-            <button class="icon" aria-label="GitHub" v-tooltip="'GitHub'">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                class="material-icons"
-              >
-                <path
-                  d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-                />
-              </svg>
-            </button>
-          </a>
-          <button
-            class="icon"
-            id="installPWA"
-            @click.prevent="showInstallPrompt()"
-            v-tooltip="$t('install_pwa')"
-          >
-            <i class="material-icons">offline_bolt</i>
-          </button>
-          <v-popover>
-            <button class="icon" v-tooltip="'More'">
-              <i class="material-icons">more_vert</i>
-            </button>
-            <template slot="popover">
-              <div>
-                <button
-                  class="icon"
-                  @click="showShortcuts = true"
-                  v-close-popover
-                >
-                  <i class="material-icons">keyboard</i>
-                  <span>{{ $t("shortcuts") }}</span>
-                </button>
-              </div>
-              <div>
-                <button
-                  class="icon"
-                  @click="showSupport = true"
-                  v-close-popover
-                >
-                  <i class="material-icons">favorite</i>
-                  <span>{{ $t("support_us") }}</span>
-                </button>
-              </div>
-              <div>
-                <button
-                  class="icon"
-                  onClick="window.open('https://twitter.com/share?text=👽 Postwoman • API request builder - Helps you create your requests faster, saving you precious time on your development&url=https://postwoman.io&hashtags=postwoman&via=liyasthomas');"
-                  v-close-popover
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
-                    />
-                  </svg>
-                  <span>{{ $t("tweet") }}</span>
-                </button>
-              </div>
-            </template>
-          </v-popover>
-        </span>
-      </div>
-    </header>
     <div class="content">
       <div class="columns">
         <aside class="nav-first">
@@ -312,65 +227,157 @@
             </nav>
           </div>
         </aside>
-        <nuxt id="main" class="main" />
+        <div class="main" id="main">
+          <header class="header">
+            <div class="flex-wrap">
+              <span class="slide-in">
+                <nuxt-link :to="localePath('index')">
+                  <h1 class="logo">Postwoman</h1>
+                </nuxt-link>
+              </span>
+              <span>
+                <a
+                  href="https://github.com/liyasthomas/postwoman"
+                  target="_blank"
+                  aria-label="GitHub"
+                  rel="noopener"
+                >
+                  <button class="icon" aria-label="GitHub" v-tooltip="'GitHub'">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      class="material-icons"
+                    >
+                      <path
+                        d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                      />
+                    </svg>
+                  </button>
+                </a>
+                <button
+                  class="icon"
+                  id="installPWA"
+                  @click.prevent="showInstallPrompt()"
+                  v-tooltip="$t('install_pwa')"
+                >
+                  <i class="material-icons">offline_bolt</i>
+                </button>
+                <v-popover>
+                  <button class="icon" v-tooltip="'More'">
+                    <i class="material-icons">more_vert</i>
+                  </button>
+                  <template slot="popover">
+                    <div>
+                      <button
+                        class="icon"
+                        @click="showShortcuts = true"
+                        v-close-popover
+                      >
+                        <i class="material-icons">keyboard</i>
+                        <span>{{ $t("shortcuts") }}</span>
+                      </button>
+                    </div>
+                    <div>
+                      <button
+                        class="icon"
+                        @click="showSupport = true"
+                        v-close-popover
+                      >
+                        <i class="material-icons">favorite</i>
+                        <span>{{ $t("support_us") }}</span>
+                      </button>
+                    </div>
+                    <div>
+                      <button
+                        class="icon"
+                        onClick="window.open('https://twitter.com/share?text=👽 Postwoman • API request builder - Helps you create your requests faster, saving you precious time on your development&url=https://postwoman.io&hashtags=postwoman&via=liyasthomas');"
+                        v-close-popover
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+                          />
+                        </svg>
+                        <span>{{ $t("tweet") }}</span>
+                      </button>
+                    </div>
+                  </template>
+                </v-popover>
+              </span>
+            </div>
+          </header>
+          <nuxt />
+          <footer class="footer">
+            <div class="flex-wrap">
+              <span v-if="version.name" class="mono">
+                <a
+                  href="https://liyasthomas.web.app"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <button class="icon" v-tooltip="'Liyas Thomas'">
+                    🦄
+                  </button>
+                </a>
+                <a
+                  v-bind:href="
+                    'https://github.com/liyasthomas/postwoman/releases/tag/' +
+                      version.name
+                  "
+                  target="_blank"
+                  rel="noopener"
+                  v-tooltip="'GitHub'"
+                >
+                  {{ version.name }}
+                </a>
+                <!-- <span v-if="version.hash">
+                  -
+                  <a
+                    v-bind:href="'https://github.com/liyasthomas/postwoman/commit/' + version.hash"
+                    target="_blank"
+                    rel="noopener"
+                  >{{version.hash}}</a>
+                </span> -->
+                <!-- <span v-if="version.variant">({{version.variant}})</span> -->
+              </span>
+              <span>
+                <a
+                  href="https://postwoman.launchaco.com"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <button class="icon" v-tooltip="$t('subscribe')">
+                    <i class="material-icons">email</i>
+                  </button>
+                </a>
+                <v-popover>
+                  <button class="icon" v-tooltip="$t('choose_language')">
+                    <i class="material-icons">translate</i>
+                  </button>
+                  <template slot="popover">
+                    <div v-for="locale in availableLocales" :key="locale.code">
+                      <nuxt-link :to="switchLocalePath(locale.code)">
+                        <button class="icon" v-close-popover>
+                          {{ locale.name }}
+                        </button>
+                      </nuxt-link>
+                    </div>
+                  </template>
+                </v-popover>
+              </span>
+            </div>
+          </footer>
+        </div>
         <aside class="nav-second"></aside>
       </div>
     </div>
-    <footer class="footer">
-      <div class="flex-wrap">
-        <span v-if="version.name" class="mono">
-          <a
-            v-bind:href="
-              'https://github.com/liyasthomas/postwoman/releases/tag/' +
-                version.name
-            "
-            target="_blank"
-            rel="noopener"
-          >
-            {{ version.name }}
-          </a>
-          <!-- <span v-if="version.hash">
-            -
-            <a
-              v-bind:href="'https://github.com/liyasthomas/postwoman/commit/' + version.hash"
-              target="_blank"
-              rel="noopener"
-            >{{version.hash}}</a>
-          </span> -->
-          <!-- <span v-if="version.variant">({{version.variant}})</span> -->
-        </span>
-        <span>
-          <a href="https://liyasthomas.web.app" target="_blank" rel="noopener">
-            <button class="icon" v-tooltip="'Liyas Thomas'">
-              🦄
-            </button>
-          </a>
-          <a
-            href="https://postwoman.launchaco.com"
-            target="_blank"
-            rel="noopener"
-          >
-            <button class="icon" v-tooltip="$t('subscribe')">
-              <i class="material-icons">email</i>
-            </button>
-          </a>
-          <v-popover>
-            <button class="icon" v-tooltip="$t('choose_language')">
-              <i class="material-icons">translate</i>
-            </button>
-            <template slot="popover">
-              <div v-for="locale in availableLocales" :key="locale.code">
-                <nuxt-link :to="switchLocalePath(locale.code)">
-                  <button class="icon" v-close-popover>
-                    {{ locale.name }}
-                  </button>
-                </nuxt-link>
-              </div>
-            </template>
-          </v-popover>
-        </span>
-      </div>
-    </footer>
     <modal v-if="showShortcuts" @close="showShortcuts = false">
       <div slot="header">
         <ul>

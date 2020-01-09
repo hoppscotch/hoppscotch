725fa31e7759990350a6422daef50b392af7971a
Liyas Thomas <liyasthomas@gmail.com>
2020-01-09 06:01:31 +0530

    :art: Improved structure and format of the code

--- components/modal.vue
+++ components/modal.vue
@@ -44,12 +44,17 @@
   display: flex;
   flex-grow: 1;
   flex-direction: column;
-  margin: 8px;
+  margin: 16px;
   padding: 16px;
   transition: all 0.2s ease;
   background-color: var(--bg-color);
   border-radius: 16px;
   box-shadow: 0px 16px 70px rgba(0, 0, 0, 0.5);
+  max-height: calc(100vh - 32px);
+}
+
+.modal-body {
+  overflow: auto;
 }
 
 /*

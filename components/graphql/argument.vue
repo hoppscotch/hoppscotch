faa420753f0d16c8d2e45d7457503d0ab6fb7cbc
Andrew Bastin <andrewbastin.k@gmail.com>
2020-01-09 13:31:30 -0500

    Added argument and typelink components for GraphQL

---
+++ components/graphql/argument.vue
@@ -0,0 +1,44 @@
+<template>
+  <span>
+    <span class="argumentName">
+      {{ argName }}
+    </span>
+    :
+    <typelink 
+      :type="argType"
+      :jumpTypeCallback="jumpCallback"
+    />
+  </span>
+</template>
+
+<style>
+</style>
+
+<script>
+import typelink from './typelink';
+
+export default {
+  components: {
+    typelink: typelink
+  },
+
+  props: {
+    gqlArg: Object
+  },
+
+  computed: {
+    argName() {
+      return this.gqlArg.name;
+    },
+    argType() {
+      return this.gqlArg.type;
+    }
+  },
+
+  methods: {
+    jumpCallback(typeName) {
+
+    }
+  }
+}
+</script>

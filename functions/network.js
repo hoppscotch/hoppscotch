3726f0a376c121c66115a65ce46d29aef39d0e05
Andrew Bastin <andrewbastin.k@gmail.com>
2020-01-13 23:47:28 -0500

    Added sendNetworkRequest to detect appropriate strategy and use it

---
+++ functions/network.js
@@ -0,0 +1,13 @@
+import AxiosStrategy from "./strategies/AxiosStrategy";
+import ProxyStrategy from "./strategies/ProxyStrategy";
+
+
+const sendNetworkRequest = (req, store) => {
+  if (store.state.postwoman.settings.PROXY_ENABLED) {
+    return ProxyStrategy(req, store);
+  } else {
+    return AxiosStrategy(req, store);
+  }
+}
+
+export { sendNetworkRequest };

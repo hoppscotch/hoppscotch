061f86fbfa3ee8a074c4e5270be0146507e92033
Liyas Thomas <liyasthomas@gmail.com>
2020-01-15 06:33:04 +0530

    :bug: Fixed Nuxt loader won't finish after response

--- functions/strategies/AxiosStrategy.js
+++ functions/strategies/AxiosStrategy.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosStrategy = async (req, _store) => {
   const res = await axios(req);
+  window.$nuxt.$loading.finish();
   return res;
-}
+};
 
 export default axiosStrategy;
--- functions/strategies/ProxyStrategy.js
+++ functions/strategies/ProxyStrategy.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const proxyStrategy = async (req, store) => {
   const { data } = await axios.post(
     store.state.postwoman.settings.PROXY_URL ||
-    "https://postwoman.apollotv.xyz/",
+      "https://postwoman.apollotv.xyz/",
     req
   );
-
+  window.$nuxt.$loading.finish();
   return data;
-}
+};
 
 export default proxyStrategy;

4b48d8a8c8a28ad69c21ae03056d0fc876c76755
Andrew Bastin <andrewbastin.k@gmail.com>
2020-01-16 01:25:43 -0500

    Refactor to bring the nuxt loading bar code out of the strategies

--- functions/network.js
+++ functions/network.js
@@ -1,12 +1,19 @@
 import AxiosStrategy from "./strategies/AxiosStrategy";
 import ProxyStrategy from "./strategies/ProxyStrategy";
+import FirefoxStrategy from "./strategies/FirefoxStrategy";
 
-const sendNetworkRequest = (req, store) => {
+
+const runAppropriateStrategy = (req, store) => {
+
   if (store.state.postwoman.settings.PROXY_ENABLED) {
     return ProxyStrategy(req, store);
   }
 
   return AxiosStrategy(req, store);
-};
+}
+
+const sendNetworkRequest = (req, store) => 
+  runAppropriateStrategy(req, store)
+    .finally(() => window.$nuxt.$loading.finish());
 
 export { sendNetworkRequest };

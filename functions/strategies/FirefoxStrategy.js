951266e52946c99a4acf7d8818795a5e5d510562
Andrew Bastin <andrewbastin.k@gmail.com>
2020-01-17 13:28:04 -0500

    Added proxy support to FirefoxStrategy

--- functions/strategies/FirefoxStrategy.js
+++ functions/strategies/FirefoxStrategy.js
@@ -1,5 +1,22 @@
-const firefoxStrategy = (req, _store) => new Promise((resolve, reject) => {
 
+const firefoxWithProxy = (req, store) => new Promise((resolve, reject) => {
+  const eventListener = (event) => {
+    window.removeEventListener("firefoxExtSendRequestComplete", event);
+
+    if (event.detail.error) reject(JSON.parse(event.detail.error));
+    else resolve(JSON.parse(event.detail.response));
+  };
+
+  window.addEventListener("firefoxExtSendRequestComplete", eventListener);
+
+  window.firefoxExtSendRequest({
+    method: "post",
+    url: store.state.postwoman.settings.PROXY_URL || "https://postwoman.apollotv.xyz/",
+    data: req
+  });
+});
+
+const firefoxWithoutProxy = (req, _store) => new Promise((resolve, reject) => {
   const eventListener = (event) => {
     window.removeEventListener("firefoxExtSendRequestComplete", eventListener);
 
@@ -11,5 +28,11 @@ const firefoxStrategy = (req, _store) => new Promise((resolve, reject) => {
 
   window.firefoxExtSendRequest(req);
 });
+
+const firefoxStrategy = (req, store) => {
+  if (store.state.postwoman.settings.PROXY_ENABLED)
+    return firefoxWithProxy(req, store);
+  else return firefoxWithoutProxy(req, store);
+}
 
 export default firefoxStrategy;

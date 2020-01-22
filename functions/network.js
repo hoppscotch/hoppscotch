2d6d1b2d348875390e356ca5d5fc28a53e9783ce
Andrew Bastin <andrewbastin.k@gmail.com>
2020-01-21 20:01:22 -0500

    Added Chrome Strategy as a valid network strategy

--- functions/network.js
+++ functions/network.js
@@ -1,8 +1,13 @@
 import AxiosStrategy from "./strategies/AxiosStrategy";
 import FirefoxStrategy from "./strategies/FirefoxStrategy";
-
+import ChromeStrategy, { hasChromeExtensionInstalled } from "~/functions/strategies/ChromeStrategy";
 
 const runAppropriateStrategy = (req, store) => {
+  // Chrome Provides a chrome object for scripts to access
+  // Check its availability to say whether you are in Google Chrome
+  if (window.chrome && hasChromeExtensionInstalled()) {
+      return ChromeStrategy(req, store);
+  }
   // The firefox plugin injects a function to send requests through it
   // If that is available, then we can use the FirefoxStrategy
   if (window.firefoxExtSendRequest) {

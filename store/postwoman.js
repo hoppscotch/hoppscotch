88fc45975f3d055b53c5f7bdd6b1d0b58262a1b7
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-02-03 17:38:07 +0530

    fix: account for letter casing while creating new collection

--- store/postwoman.js
+++ store/postwoman.js
@@ -112,7 +112,7 @@ export const mutations = {
 
   addNewCollection({ collections }, collection) {
     const { name } = collection;
-    const duplicateCollection = collections.some(item => item.name === name);
+    const duplicateCollection = collections.some(item => item.name.toLowerCase() === name.toLowerCase());
     if (duplicateCollection) {
       this.$toast.info("Duplicate collection");
       return;

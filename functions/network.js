89312347d1e25dfb550ce12167717713165556fd
Andrew Bastin <andrewbastin.k@gmail.com>
2020-02-04 13:13:20 -0500

    Updated strategy identification to consider extensions toggle

--- functions/network.js
+++ functions/network.js
@@ -2,16 +2,25 @@ import AxiosStrategy from "./strategies/AxiosStrategy";
 import FirefoxStrategy from "./strategies/FirefoxStrategy";
 import ChromeStrategy, { hasChromeExtensionInstalled } from "./strategies/ChromeStrategy";
 
+const isExtensionsAllowed = ({ state }) => {
+  console.log(typeof(state.postwoman.settings.EXTENSIONS_ENABLED) === 'undefined'
+    || state.postwoman.settings.EXTENSIONS_ENABLED);
+  return typeof(state.postwoman.settings.EXTENSIONS_ENABLED) === 'undefined'
+    || state.postwoman.settings.EXTENSIONS_ENABLED;
+}
+
 const runAppropriateStrategy = (req, store) => {
-  // Chrome Provides a chrome object for scripts to access
-  // Check its availability to say whether you are in Google Chrome
-  if (window.chrome && hasChromeExtensionInstalled()) {
-      return ChromeStrategy(req, store);
-  }
-  // The firefox plugin injects a function to send requests through it
-  // If that is available, then we can use the FirefoxStrategy
-  if (window.firefoxExtSendRequest) {
-    return FirefoxStrategy(req, store); 
+  if (isExtensionsAllowed(store)) {
+    // Chrome Provides a chrome object for scripts to access
+    // Check its availability to say whether you are in Google Chrome
+    if (window.chrome && hasChromeExtensionInstalled()) {
+        return ChromeStrategy(req, store);
+    }
+    // The firefox plugin injects a function to send requests through it
+    // If that is available, then we can use the FirefoxStrategy
+    if (window.firefoxExtSendRequest) {
+      return FirefoxStrategy(req, store); 
+    }
   }
 
   return AxiosStrategy(req, store);

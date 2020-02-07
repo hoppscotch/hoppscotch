6575d03f45a99ffc5b351b6a5d9c0882a64f824d
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-02-07 14:54:09 +0530

    refactor: use localizable string

--- components/collections/addCollection.vue
+++ components/collections/addCollection.vue
@@ -58,7 +58,7 @@ export default {
   methods: {
     addNewCollection() {
       if (!this.$data.name) {
-        this.$toast.info("Please provide a valid name for the collection");
+        this.$toast.info($t("invalid_collection_name"));
         return;
       }
       this.$store.commit("postwoman/addNewCollection", {

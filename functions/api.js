703f8f8a9989301984c570a5f60d69ea8fd22cc2
Liyas Thomas <liyascthomas@gmail.com>
2020-03-12 18:15:59 +0530

    `POST` method

--- functions/api.js
+++ functions/api.js
@@ -1,21 +1,31 @@
-import querystring from "querystring"
-
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
-  switch (event.httpMethod) {
+  switch (httpMethod) {
     case "GET":
-      const name = event.queryStringParameters.name || "World"
-      return {
-        statusCode: 200,
-        body: `Hello ${name}`,
+      try {
+        const name = event.queryStringParameters.name || "World"
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: `Hello ${name}` }),
+          // // more keys you can return:
+          // headers: { "headerName": "headerValue", ... },
+          // isBase64Encoded: true,
+        }
+      } catch (err) {
+        return { statusCode: 500, body: err.toString() }
       }
     case "POST":
-      // When the method is POST, the name will no longer be in the event’s
-      // queryStringParameters – it’ll be in the event body encoded as a query string
-      const params = querystring.parse(event.body)
-      const name = params.name || "World"
-      return {
-        statusCode: 200,
-        body: `Hello ${name}`,
+      try {
+        const name = event.body.name || "World"
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: `Hello ${name}` }),
+          // // more keys you can return:
+          // headers: { "headerName": "headerValue", ... },
+          // isBase64Encoded: true,
+        }
+      } catch (err) {
+        return { statusCode: 500, body: err.toString() }
       }
     default:
       return { statusCode: 405, body: "Method Not Allowed" }

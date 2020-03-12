a99b2b4bb91c90b618274cc69895394aab184436
Liyas Thomas <liyascthomas@gmail.com>
2020-03-12 17:33:12 +0530

    POST method

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,23 @@
-exports.handler = async (event, context) => {
-  const name = event.queryStringParameters.name || "World"
+import querystring from "querystring"
 
-  return {
-    statusCode: 200,
-    body: `Hello, ${name}`,
+exports.handler = async (event, context) => {
+  switch (event.httpMethod) {
+    case "GET":
+      const name = event.queryStringParameters.name || "World"
+      return {
+        statusCode: 200,
+        body: `Hello ${name}`,
+      }
+    case "POST":
+      // When the method is POST, the name will no longer be in the event’s
+      // queryStringParameters – it’ll be in the event body encoded as a query string
+      const params = querystring.parse(event.body)
+      const name = params.name || "World"
+      return {
+        statusCode: 200,
+        body: `Hello ${name}`,
+      }
+    default:
+      return { statusCode: 405, body: "Method Not Allowed" }
   }
 }

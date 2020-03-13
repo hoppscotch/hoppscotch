f52d68a1d705a138519c47f73a0418c0ce4cf464
Liyas Thomas <liyascthomas@gmail.com>
2020-03-13 08:19:51 +0530

    ✅ Updating tests

--- tests/e2e/integration/proxy.spec.js
+++ tests/e2e/integration/proxy.spec.js
@@ -3,9 +3,8 @@ describe("Proxy disabled - local request", () => {
     cy.seedAndVisit("catapi", "/?url=https://api.thecatapi.com&path=")
       .get("#url")
       .then(el => expect(el.val() === "https://api.thecatapi.com").to.equal(true))
-      .get("#response-details-wrapper")
+      .get("#response-details-wrapper", { timeout: 24000 })
       .should("be.visible")
-      .wait(2000)
       .should($wrapper => {
         expect($wrapper).to.contain("FAKE Cat API")
       })

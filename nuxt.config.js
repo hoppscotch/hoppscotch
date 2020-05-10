b598d846bbda9f13f575d11ba78ddccb4ec35618
Liyas Thomas <liyascthomas@gmail.com>
2020-05-10 12:36:40 +0530

    :zap: dotenv

--- nuxt.config.js
+++ nuxt.config.js
@@ -297,7 +297,11 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {},
+    extend(config, ctx) {
+      config.node = {
+        fs: "empty",
+      }
+    },
   },
   /*
    ** Generate configuration

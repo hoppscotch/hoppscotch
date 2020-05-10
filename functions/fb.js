6f034ad878c0e22ffc19c78aeb0481cadbf70070
Liyas Thomas <liyascthomas@gmail.com>
2020-05-10 13:18:40 +0530

    :zap: dotenv

--- functions/fb.js
+++ functions/fb.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/firestore"
 import "firebase/auth"
-require("dotenv").config()
 
 // Initialize Firebase, copied from cloud console
 const firebaseConfig = {

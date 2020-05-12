2ffb6d6a9dfad9ab60107a9cf8cd04458619a8cd
Liyas Thomas <liyascthomas@gmail.com>
2020-05-12 13:29:02 +0530

    dotenv

--- nuxt.config.js
+++ nuxt.config.js
@@ -214,6 +214,16 @@ export default {
     Allow: "/",
     Sitemap: "https://postwoman.io/sitemap.xml",
   },
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+  },
   i18n: {
     locales: [
       {

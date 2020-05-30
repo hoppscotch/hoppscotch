b2600d3ffdfe245e6f720d7ea1e31f3a51ed45f6
Andrew Bastin <andrewbastin.k@gmail.com>
2020-05-30 18:26:26 -0400

    Added cancellable requests for AxiosStrategy

--- functions/strategies/AxiosStrategy.js
+++ functions/strategies/AxiosStrategy.js
@@ -1,39 +1,69 @@
 import axios from "axios"
 
+let cancelSource = axios.CancelToken.source()
+
+export const cancelRunningAxiosRequest = () => {
+  cancelSource.cancel()
+
+  // Create a new cancel token
+  cancelSource = axios.CancelToken.source()
+}
+
 const axiosWithProxy = async (req, { state }) => {
-  const { data } = await axios.post(
-    state.postwoman.settings.PROXY_URL || "https://postwoman.apollosoftware.xyz/",
-    req
-  )
-  return data
+  try {
+    const { data } = await axios.post(
+      state.postwoman.settings.PROXY_URL || "https://postwoman.apollosoftware.xyz/",
+      req,
+      {
+        cancelToken: cancelSource.token,
+      }
+    )
+    return data
+  } catch (e) {
+    // Check if the throw is due to a cancellation
+    if (axios.isCancel(e)) {
+      throw "cancellation"
+    } else {
+      throw e
+    }
+  }
 }
 
 const axiosWithoutProxy = async (req, _store) => {
-  const res = await axios({
-    ...req,
-    transformResponse: [
-      (data, headers) => {
-        // If the response has a JSON content type, try parsing it
-        if (
-          headers["content-type"] &&
-          (headers["content-type"].startsWith("application/json") ||
-            headers["content-type"].startsWith("application/vnd.api+json") ||
-            headers["content-type"].startsWith("application/hal+json"))
-        ) {
-          try {
-            const jsonData = JSON.parse(data)
-            return jsonData
-          } catch (e) {
-            return data
+  try {
+    const res = await axios({
+      ...req,
+      cancelToken: cancelSource.token,
+      transformResponse: [
+        (data, headers) => {
+          // If the response has a JSON content type, try parsing it
+          if (
+            headers["content-type"] &&
+            (headers["content-type"].startsWith("application/json") ||
+              headers["content-type"].startsWith("application/vnd.api+json") ||
+              headers["content-type"].startsWith("application/hal+json"))
+          ) {
+            try {
+              const jsonData = JSON.parse(data)
+              return jsonData
+            } catch (e) {
+              return data
+            }
           }
-        }
 
-        // Else return the string itself without any transformations
-        return data
-      },
-    ],
-  })
-  return res
+          // Else return the string itself without any transformations
+          return data
+        },
+      ],
+    })
+    return res
+  } catch (e) {
+    if (axios.isCancel(e)) {
+      throw "cancellation"
+    } else {
+      throw e
+    }
+  }
 }
 
 const axiosStrategy = (req, store) => {

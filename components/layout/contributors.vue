df115bd7db6cea7c36f913a56939f1ff0949e524
Liyas Thomas <liyascthomas@gmail.com>
2020-06-09 14:32:08 +0530

    :seedling: Updated funding info

--- components/layout/contributors.vue
+++ components/layout/contributors.vue
@@ -9,6 +9,34 @@
     <div class="flex-wrap">
       <span>
         <a
+          href="https://github.com/sponsors/postwoman-io"
+          target="_blank"
+          rel="noopener"
+          v-tooltip.right="$t('recurring')"
+        >
+          <button class="icon">
+            <i class="material-icons">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                class="material-icons"
+              >
+                <path
+                  d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                />
+              </svg>
+            </i>
+            <span>GitHub</span>
+          </button>
+        </a>
+        🎉 NEW
+      </span>
+    </div>
+    <div class="flex-wrap">
+      <span>
+        <a
           href="https://opencollective.com/postwoman"
           target="_blank"
           rel="noopener"
@@ -47,34 +75,6 @@
           <span>{{ $t("patreon") }}</span>
         </button>
       </a>
-    </div>
-    <div class="flex-wrap">
-      <span>
-        <a
-          href="https://github.com/sponsors/postwoman-io"
-          target="_blank"
-          rel="noopener"
-          v-tooltip.right="$t('recurring')"
-        >
-          <button class="icon">
-            <i class="material-icons">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                class="material-icons"
-              >
-                <path
-                  d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-                />
-              </svg>
-            </i>
-            <span>GitHub</span>
-          </button>
-        </a>
-        🎉 NEW
-      </span>
     </div>
     <hr />
     <h3 class="title">Financial Contributors</h3>
@@ -136,6 +136,13 @@
       <a class="link" href="https://opencollective.com/postwoman/contribute">Contribute</a>.
     </p>
     <div class="contributors">
+      <a href="http://tom.preston-werner.com" target="_blank" rel="noopener">
+        <img
+          style="max-width: 64px; max-height: 64px; border-radius: 100%;"
+          src="https://github.com/mojombo.png?size=64"
+          alt="Tom Preston-Werner"
+        />
+      </a>
       <a href="https://opencollective.com/postwoman">
         <img src="https://opencollective.com/postwoman/individuals.svg" />
       </a>
@@ -155,6 +162,7 @@
 <style scoped lang="scss">
 .contributors {
   display: flex;
+  align-items: center;
   flex-flow: row nowrap;
   overflow: auto;
   padding: 8px 0;

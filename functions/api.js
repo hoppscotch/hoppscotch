b6c16c83c144040bc0e3fcf88e93ec6f8db2af7f
Liyas Thomas <liyascthomas@gmail.com>
2020-06-12 08:55:17 +0530

    :ambulance: Critical hotfix

--- functions/api.js
+++ functions/api.js
@@ -1,9 +1,9 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-export async function handler({ httpMethod, queryStringParameters }, context) {
-  switch (httpMethod) {
+exports.handler = async (event, context) => {
+  switch (event.httpMethod) {
     case "GET":
       try {
-        const name = queryStringParameters.name || "World"
+        const name = event.queryStringParameters.name || "World"
         return {
           statusCode: 200,
           headers: {

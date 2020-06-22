ac323c99d537a7ae4534e4ef5f9993f25fbc6d31
Liyas Thomas <liyascthomas@gmail.com>
2020-06-22 08:00:46 +0530

    Fixed activating multiple tabs on initial load

--- components/lenses/ResponseBodyRenderer.vue
+++ components/lenses/ResponseBodyRenderer.vue
@@ -2,11 +2,11 @@
   <div>
     <tabs>
       <tab
-        v-for="(lens, index) in validLenses"
+        v-for="lens in validLenses"
         :id="lens.lensName"
         :key="lens.lensName"
         :label="lens.lensName"
-        :selected="index === 0"
+        :selected="lens.lensName === 'Raw'"
       >
         <component :is="lens.renderer" :response="response" />
       </tab>
@@ -15,24 +15,21 @@
 </template>
 
 <script>
-import getSuitableLenses from "../../helpers/lenses/lenses"
+import getSuitableLenses from "~/helpers/lenses/lenses"
 
 export default {
   components: {
+    tabs: () => import("../ui/tabs"),
+    tab: () => import("../ui/tab"),
     // Lens Renderers
     raw: () => import("../lenses/renderers/RawLensRenderer"),
     json: () => import("../lenses/renderers/JSONLensRenderer"),
     imageres: () => import("../lenses/renderers/ImageLensRenderer"),
     htmlres: () => import("../lenses/renderers/HTMLLensRenderer"),
-
-    tabs: () => import("../ui/tabs"),
-    tab: () => import("../ui/tab"),
   },
-
   props: {
     response: {},
   },
-
   computed: {
     validLenses() {
       return getSuitableLenses(this.response)

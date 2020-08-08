e90a31948fddba8e8248905c28fafa715d24a0f8
Dym Sohin <re@dym.sh>
2020-08-08 12:58:50 +0200

    added icon.vue component

---
+++ components/ui/icon.vue
@@ -0,0 +1,11 @@
+<template>
+  <img class="material-icons" :src="'~assets/icons/' + $t('icon') + '.svg'" :alt="$t('icon')" />
+</template>
+
+<script>
+export default {
+  props: {
+    icon: { type: String },
+  },
+}
+</script>

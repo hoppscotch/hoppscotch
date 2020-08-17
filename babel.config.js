f57ac978ae9b8864e7025d95b6f9dcce52f64bcd
Liyas Thomas <liyascthomas@gmail.com>
2020-08-17 09:22:02 +0530

    Tests

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+function isBabelLoader(caller) {
+  return caller && caller.name === "babel-loader"
+}
+
+module.exports = function (api) {
+  if (api.env("test") && !api.caller(isBabelLoader)) {
+    return {
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              node: "current",
+            },
+          },
+        ],
+      ],
+    }
+  }
+  return {}
+}

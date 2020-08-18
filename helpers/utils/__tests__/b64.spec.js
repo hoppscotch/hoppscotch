9fe10fd9a250b569cfd2fe4be48f9ed4f0229471
Andrew Bastin <andrewbastin.k@gmail.com>
2020-08-18 14:01:04 -0400

    Added test spec for helpers/utils/b64.js

---
+++ helpers/utils/__tests__/b64.spec.js
@@ -0,0 +1,13 @@
+import { TextDecoder } from "util"
+import { decodeB64StringToArrayBuffer } from "../b64"
+
+describe("decodeB64StringToArrayBuffer", () => {
+  test("decodes content correctly", () => {
+    const decoder = new TextDecoder("utf-8")
+    expect(
+      decoder.decode(decodeB64StringToArrayBuffer("aG9wcHNjb3RjaCBpcyBhd2Vzb21lIQ=="))
+    ).toMatch("hoppscotch is awesome!")
+  })
+
+  // TODO : More tests for binary data ?
+})

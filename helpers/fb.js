465933d5facb27f2fe2ab3762e080e91d91938d0
Andrew Bastin <andrewbastin.k@gmail.com>
2020-08-30 13:43:55 -0400

    Refactor fb.js to help testing

--- helpers/fb.js
+++ helpers/fb.js
@@ -13,242 +13,258 @@ const firebaseConfig = {
   appId: process.env.APP_ID || "1:421993993223:web:ec0baa8ee8c02ffa1fc6a2",
   measurementId: process.env.MEASUREMENT_ID || "G-ERJ6025CEB",
 }
-firebase.initializeApp(firebaseConfig)
 
-// a reference to the users collection
-const usersCollection = firebase.firestore().collection("users")
+export class FirebaseInstance {
+  constructor(fbapp) {
+    this.app = fbapp
 
-// the shared state object that any vue component
-// can get access to
-export const fb = {
-  currentUser: null,
-  currentFeeds: [],
-  currentSettings: [],
-  currentHistory: [],
-  currentCollections: [],
-  currentEnvironments: [],
-  currentTeams: [],
-  writeFeeds: async (message, label) => {
+    this.usersCollection = this.app.firestore().collection("users")
+
+    this.currentUser = null
+    this.currentFeeds = []
+    this.currentSettings = []
+    this.currentHistory = []
+    this.currentCollections = []
+    this.currentEnvironments = []
+    this.currentTeams = []
+
+    this.app.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.currentUser = user
+        this.currentUser.providerData.forEach((profile) => {
+          let us = {
+            updatedOn: new Date(),
+            provider: profile.providerId,
+            name: profile.displayName,
+            email: profile.email,
+            photoUrl: profile.photoURL,
+            uid: profile.uid,
+          }
+          this.usersCollection
+            .doc(this.currentUser.uid)
+            .set(us)
+            .catch((e) => console.error("error updating", us, e))
+        })
+
+        this.usersCollection
+          .doc(this.currentUser.uid)
+          .collection("feeds")
+          .orderBy("createdOn", "desc")
+          .onSnapshot((feedsRef) => {
+            const feeds = []
+            feedsRef.forEach((doc) => {
+              const feed = doc.data()
+              feed.id = doc.id
+              feeds.push(feed)
+            })
+            this.currentFeeds = feeds
+          })
+
+        this.usersCollection
+          .doc(this.currentUser.uid)
+          .collection("settings")
+          .onSnapshot((settingsRef) => {
+            const settings = []
+            settingsRef.forEach((doc) => {
+              const setting = doc.data()
+              setting.id = doc.id
+              settings.push(setting)
+            })
+            this.currentSettings = settings
+          })
+
+        this.usersCollection
+          .doc(this.currentUser.uid)
+          .collection("history")
+          .onSnapshot((historyRef) => {
+            const history = []
+            historyRef.forEach((doc) => {
+              const entry = doc.data()
+              entry.id = doc.id
+              history.push(entry)
+            })
+            this.currentHistory = history
+          })
+
+        this.usersCollection
+          .doc(this.currentUser.uid)
+          .collection("collections")
+          .onSnapshot((collectionsRef) => {
+            const collections = []
+            collectionsRef.forEach((doc) => {
+              const collection = doc.data()
+              collection.id = doc.id
+              collections.push(collection)
+            })
+            if (collections.length > 0) {
+              this.currentCollections = collections[0].collection
+            }
+          })
+
+        this.usersCollection
+          .doc(this.currentUser.uid)
+          .collection("environments")
+          .onSnapshot((environmentsRef) => {
+            const environments = []
+            environmentsRef.forEach((doc) => {
+              const environment = doc.data()
+              environment.id = doc.id
+              environments.push(environment)
+            })
+            if (environments.length > 0) {
+              this.currentEnvironments = environments[0].environment
+            }
+          })
+
+        this.usersCollection
+          .doc(this.currentUser.uid)
+          .collection("teams")
+          .onSnapshot((teamsRef) => {
+            const teams = []
+            teamsRef.forEach((doc) => {
+              const team = doc.data()
+              team.id = doc.id
+              teams.push(team)
+            })
+            this.currentTeams = teams[0].team
+          })
+      } else {
+        this.currentUser = null
+      }
+    })
+  }
+
+  async writeFeeds(message, label) {
     const dt = {
       createdOn: new Date(),
-      author: fb.currentUser.uid,
-      author_name: fb.currentUser.displayName,
-      author_image: fb.currentUser.photoURL,
+      author: this.currentUser.uid,
+      author_name: this.currentUser.displayName,
+      author_image: this.currentUser.photoURL,
       message,
       label,
     }
-    usersCollection
-      .doc(fb.currentUser.uid)
+
+    this.usersCollection
+      .doc(this.currentUser.uid)
       .collection("feeds")
       .add(dt)
       .catch((e) => console.error("error inserting", dt, e))
-  },
-  deleteFeed: (id) => {
-    usersCollection
-      .doc(fb.currentUser.uid)
+  }
+
+  deleteFeed(id) {
+    this.usersCollection
+      .doc(this.currentUser.uid)
       .collection("feeds")
       .doc(id)
       .delete()
       .catch((e) => console.error("error deleting", id, e))
-  },
-  writeSettings: async (setting, value) => {
+  }
+
+  async writeSettings(setting, value) {
     const st = {
       updatedOn: new Date(),
-      author: fb.currentUser.uid,
-      author_name: fb.currentUser.displayName,
-      author_image: fb.currentUser.photoURL,
+      author: this.currentUser.uid,
+      author_name: this.currentUser.displayName,
+      author_image: this.currentUser.photoURL,
       name: setting,
       value,
     }
-    usersCollection
-      .doc(fb.currentUser.uid)
+
+    this.usersCollection
+      .doc(this.currentUser.uid)
       .collection("settings")
       .doc(setting)
       .set(st)
       .catch((e) => console.error("error updating", st, e))
-  },
-  writeHistory: async (entry) => {
+  }
+
+  async writeHistory(entry) {
     const hs = entry
-    usersCollection
-      .doc(fb.currentUser.uid)
+    this.usersCollection
+      .doc(this.currentUser.uid)
       .collection("history")
       .add(hs)
       .catch((e) => console.error("error inserting", hs, e))
-  },
-  deleteHistory: (entry) => {
-    usersCollection
-      .doc(fb.currentUser.uid)
+  }
+
+  deleteHistory(entry) {
+    this.usersCollection
+      .doc(this.currentUser.uid)
       .collection("history")
       .doc(entry.id)
       .delete()
       .catch((e) => console.error("error deleting", entry, e))
-  },
-  clearHistory: () => {
-    usersCollection
-      .doc(fb.currentUser.uid)
+  }
+
+  clearHistory() {
+    this.usersCollection
+      .doc(this.currentUser.uid)
       .collection("history")
       .get()
       .then(({ docs }) => {
-        docs.forEach((e) => fb.deleteHistory(e))
+        docs.forEach((e) => this.deleteHistory(e))
       })
-  },
-  toggleStar: (entry, value) => {
-    usersCollection
-      .doc(fb.currentUser.uid)
+  }
+
+  toggleStar(entry, value) {
+    this.usersCollection
+      .doc(this.currentUser.uid)
       .collection("history")
       .doc(entry.id)
       .update({ star: value })
       .catch((e) => console.error("error deleting", entry, e))
-  },
-  writeCollections: async (collection) => {
+  }
+
+  async writeCollections(collection) {
     const cl = {
       updatedOn: new Date(),
-      author: fb.currentUser.uid,
-      author_name: fb.currentUser.displayName,
-      author_image: fb.currentUser.photoURL,
+      author: this.currentUser.uid,
+      author_name: this.currentUser.displayName,
+      author_image: this.currentUser.photoURL,
       collection,
     }
-    usersCollection
-      .doc(fb.currentUser.uid)
+
+    this.usersCollection
+      .doc(this.currentUser.uid)
       .collection("collections")
       .doc("sync")
       .set(cl)
       .catch((e) => console.error("error updating", cl, e))
-  },
-  writeEnvironments: async (environment) => {
+  }
+
+  writeEnvironments(environment) {
     const ev = {
       updatedOn: new Date(),
-      author: fb.currentUser.uid,
-      author_name: fb.currentUser.displayName,
-      author_image: fb.currentUser.photoURL,
+      author: this.currentUser.uid,
+      author_name: this.currentUser.displayName,
+      author_image: this.currentUser.photoURL,
       environment,
     }
-    usersCollection
-      .doc(fb.currentUser.uid)
+
+    this.usersCollection
+      .doc(this.currentUser.uid)
       .collection("environments")
       .doc("sync")
       .set(ev)
       .catch((e) => console.error("error updating", ev, e))
-  },
-  writeTeams: async (team) => {
+  }
+
+  async writeTeams(team) {
     const ev = {
       updatedOn: new Date(),
-      author: fb.currentUser.uid,
-      author_name: fb.currentUser.displayName,
-      author_image: fb.currentUser.photoURL,
+      author: this.currentUser.uid,
+      author_name: this.currentUser.displayName,
+      author_image: this.currentUser.photoURL,
       team: team,
     }
-    usersCollection
-      .doc(fb.currentUser.uid)
+
+    this.usersCollection
+      .doc(this.currentUser.uid)
       .collection("teams")
       .doc("sync")
       .set(ev)
       .catch((e) => console.error("error updating", ev, e))
-  },
+  }
 }
 
-// When a user logs in or out, save that in the store
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    fb.currentUser = user
-    fb.currentUser.providerData.forEach((profile) => {
-      let us = {
-        updatedOn: new Date(),
-        provider: profile.providerId,
-        name: profile.displayName,
-        email: profile.email,
-        photoUrl: profile.photoURL,
-        uid: profile.uid,
-      }
-      usersCollection
-        .doc(fb.currentUser.uid)
-        .set(us)
-        .catch((e) => console.error("error updating", us, e))
-    })
-
-    usersCollection
-      .doc(fb.currentUser.uid)
-      .collection("feeds")
-      .orderBy("createdOn", "desc")
-      .onSnapshot((feedsRef) => {
-        const feeds = []
-        feedsRef.forEach((doc) => {
-          const feed = doc.data()
-          feed.id = doc.id
-          feeds.push(feed)
-        })
-        fb.currentFeeds = feeds
-      })
-
-    usersCollection
-      .doc(fb.currentUser.uid)
-      .collection("settings")
-      .onSnapshot((settingsRef) => {
-        const settings = []
-        settingsRef.forEach((doc) => {
-          const setting = doc.data()
-          setting.id = doc.id
-          settings.push(setting)
-        })
-        fb.currentSettings = settings
-      })
-
-    usersCollection
-      .doc(fb.currentUser.uid)
-      .collection("history")
-      .onSnapshot((historyRef) => {
-        const history = []
-        historyRef.forEach((doc) => {
-          const entry = doc.data()
-          entry.id = doc.id
-          history.push(entry)
-        })
-        fb.currentHistory = history
-      })
-
-    usersCollection
-      .doc(fb.currentUser.uid)
-      .collection("collections")
-      .onSnapshot((collectionsRef) => {
-        const collections = []
-        collectionsRef.forEach((doc) => {
-          const collection = doc.data()
-          collection.id = doc.id
-          collections.push(collection)
-        })
-        if (collections.length > 0) {
-          fb.currentCollections = collections[0].collection
-        }
-      })
-
-    usersCollection
-      .doc(fb.currentUser.uid)
-      .collection("environments")
-      .onSnapshot((environmentsRef) => {
-        const environments = []
-        environmentsRef.forEach((doc) => {
-          const environment = doc.data()
-          environment.id = doc.id
-          environments.push(environment)
-        })
-        if (environments.length > 0) {
-          fb.currentEnvironments = environments[0].environment
-        }
-      })
-
-    usersCollection
-      .doc(fb.currentUser.uid)
-      .collection("teams")
-      .onSnapshot((teamsRef) => {
-        const teams = []
-        teamsRef.forEach((doc) => {
-          const team = doc.data()
-          team.id = doc.id
-          teams.push(team)
-        })
-        fb.currentTeams = teams[0].team
-      })
-  } else {
-    fb.currentUser = null
-  }
-})
+export const fb = new FirebaseInstance(firebase.initializeApp(firebaseConfig))

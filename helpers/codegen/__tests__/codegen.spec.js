57dc9d99a1a60bc28759a5f2f71e88bbbd10b011
Qing Ye <ye.qing@go-jek.com>
2020-10-09 16:20:23 +0800

    table testing

--- helpers/codegen/__tests__/codegen.spec.js
+++ helpers/codegen/__tests__/codegen.spec.js
@@ -14,68 +14,72 @@ import {
 
 codegens.forEach((codegen) => {
   describe(`generate request for ${codegen.name}`, () => {
-    test("generate GET request", () => {
-      const result = codegen.generator({
-        url: TEST_URL,
-        pathName: TEST_PATH_NAME,
-        queryString: TEST_QUERY_STRING,
-        auth: "Basic Auth",
-        httpUser: TEST_HTTP_USER,
-        httpPassword: TEST_HTTP_PASSWORD,
-        method: "GET",
-        rawInput: false,
-        rawParams: "",
-        rawRequestBody: "",
-        headers: TEST_HEADERS,
-      })
-      expect(result).toMatchSnapshot()
-    })
+    const testCases = [
+      [
+        "generate GET request",
+        {
+          url: TEST_URL,
+          pathName: TEST_PATH_NAME,
+          queryString: TEST_QUERY_STRING,
+          auth: "Basic Auth",
+          httpUser: TEST_HTTP_USER,
+          httpPassword: TEST_HTTP_PASSWORD,
+          method: "GET",
+          rawInput: false,
+          rawParams: "",
+          rawRequestBody: "",
+          headers: TEST_HEADERS,
+        },
+      ],
+      [
+        "generate POST request for JSON",
+        {
+          url: TEST_URL,
+          pathName: TEST_PATH_NAME,
+          queryString: TEST_QUERY_STRING,
+          auth: "Bearer Token",
+          bearerToken: TEST_BEARER_TOKEN,
+          method: "POST",
+          rawInput: true,
+          rawParams: TEST_RAW_PARAMS_JSON,
+          rawRequestBody: "",
+          contentType: "application/json",
+          headers: TEST_HEADERS,
+        },
+      ],
+      [
+        "generate POST request for XML",
+        {
+          url: TEST_URL,
+          pathName: TEST_PATH_NAME,
+          queryString: TEST_QUERY_STRING,
+          auth: "OAuth 2.0",
+          bearerToken: TEST_BEARER_TOKEN,
+          method: "POST",
+          rawInput: true,
+          rawParams: TEST_RAW_PARAMS_XML,
+          rawRequestBody: "",
+          contentType: "application/xml",
+          headers: TEST_HEADERS,
+        },
+      ],
+      [
+        "generate PUT request for www-form-urlencoded",
+        {
+          url: TEST_URL,
+          pathName: TEST_PATH_NAME,
+          queryString: TEST_QUERY_STRING,
+          method: "PUT",
+          rawInput: false,
+          rawRequestBody: TEST_RAW_REQUEST_BODY,
+          contentType: "application/x-www-form-urlencoded",
+          headers: [],
+        },
+      ],
+    ]
 
-    test("generate POST request for JSON", () => {
-      const result = codegen.generator({
-        url: TEST_URL,
-        pathName: TEST_PATH_NAME,
-        queryString: TEST_QUERY_STRING,
-        auth: "Bearer Token",
-        bearerToken: TEST_BEARER_TOKEN,
-        method: "POST",
-        rawInput: true,
-        rawParams: TEST_RAW_PARAMS_JSON,
-        rawRequestBody: "",
-        contentType: "application/json",
-        headers: TEST_HEADERS,
-      })
-      expect(result).toMatchSnapshot()
-    })
-
-    test("generate POST request for XML", () => {
-      const result = codegen.generator({
-        url: TEST_URL,
-        pathName: TEST_PATH_NAME,
-        queryString: TEST_QUERY_STRING,
-        auth: "OAuth 2.0",
-        bearerToken: TEST_BEARER_TOKEN,
-        method: "POST",
-        rawInput: true,
-        rawParams: TEST_RAW_PARAMS_XML,
-        rawRequestBody: "",
-        contentType: "application/xml",
-        headers: TEST_HEADERS,
-      })
-      expect(result).toMatchSnapshot()
-    })
-
-    test("generate PUT request for www-form-urlencoded", () => {
-      const result = codegen.generator({
-        url: TEST_URL,
-        pathName: TEST_PATH_NAME,
-        queryString: TEST_QUERY_STRING,
-        method: "PUT",
-        rawInput: false,
-        rawRequestBody: TEST_RAW_REQUEST_BODY,
-        contentType: "application/x-www-form-urlencoded",
-        headers: [],
-      })
+    test.each(testCases)("%s", (_, context) => {
+      const result = codegen.generator(context)
       expect(result).toMatchSnapshot()
     })
   })

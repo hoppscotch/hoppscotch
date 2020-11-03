f20876a02817e46abd81f2e8a8f80624585fc846
Liyas Thomas <liyascthomas@gmail.com>
2020-11-03 07:17:17 +0530

    chore(deps): bump

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-// const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });

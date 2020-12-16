29a4dee91b756d874cf4c77f0b1ece2d6fd15422
Liyas Thomas <liyascthomas@gmail.com>
2020-12-16 23:53:08 +0530

    chore(deps): bump + refactor: removed absolute code

--- pages/index.vue
+++ pages/index.vue
@@ -1407,12 +1407,6 @@ export default {
       this.testsEnabled = entry.usesPostScripts
       if (this.settings.SCROLL_INTO_ENABLED) this.scrollInto("request")
     },
-    getVariablesFromPreRequestScript() {
-      if (!this.preRequestScript) {
-        return {}
-      }
-      return getEnvironmentVariablesFromScript(this.preRequestScript)
-    },
     async makeRequest(auth, headers, requestBody, preRequestScript) {
       const requestOptions = {
         method: this.method,

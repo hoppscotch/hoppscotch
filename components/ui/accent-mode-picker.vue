9e166774efa028fbfbc113ed334e57520ce54d3b
Liyas Thomas <liyascthomas@gmail.com>
2020-12-16 06:05:58 +0530

    refactor(ui): faster animations + chore(deps): bump

--- components/ui/accent-mode-picker.vue
+++ components/ui/accent-mode-picker.vue
@@ -6,7 +6,7 @@
         v-for="(color, index) of colors"
         :key="`color-${index}`"
         v-tooltip="`${color.charAt(0).toUpperCase()}${color.slice(1)}`"
-        class="inline-flex items-center justify-center p-3 m-2 transition duration-200 ease-in-out bg-transparent rounded-full cursor-pointer border-collapseer-2 hover:shadow-none"
+        class="inline-flex items-center justify-center p-3 m-2 transition duration-150 ease-in-out bg-transparent rounded-full cursor-pointer border-collapseer-2 hover:shadow-none"
         :class="[{ 'bg-bgDarkColor': color === active }, `text-${color}-400`]"
         @click="setActiveColor(color)"
       >

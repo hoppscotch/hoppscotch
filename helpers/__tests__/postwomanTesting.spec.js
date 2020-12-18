97eac6ca0ccff52b07019ab64bbc87f7ea81aa4d
Karel De Smet <karel.de.smet@outlook.com>
2020-12-18 07:35:36 +0100

    Use getTestResult and getErrors to avoid duplication of code

--- helpers/__tests__/postwomanTesting.spec.js
+++ helpers/__tests__/postwomanTesting.spec.js
@@ -16,35 +16,27 @@ describe("Error handling", () => {
     }).toThrow()
   })
   test("errors array is empty on a successful test", () => {
-    expect(runTestScriptWithVariables("pw.expect(1).toBe(1)").errors).toStrictEqual([])
+    expect(getErrors("pw.expect(1).toBe(1)")).toStrictEqual([])
   })
 })
 
 describe("toBe", () => {
   test("test for numbers", () => {
-    expect(runTestScriptWithVariables("pw.expect(1).toBe(2)").testResults[0].result).toBe(FAIL)
+    expect(getTestResult("pw.expect(1).toBe(2)", 0)).toBe(FAIL)
 
-    expect(runTestScriptWithVariables("pw.expect(1).toBe(1)").testResults[0].result).toBe(PASS)
+    expect(getTestResult("pw.expect(1).toBe(1)", 0)).toBe(PASS)
   })
 
   test("test for strings", () => {
-    expect(
-      runTestScriptWithVariables("pw.expect('hello').toBe('bonjour')").testResults[0].result
-    ).toBe(FAIL)
-    expect(runTestScriptWithVariables("pw.expect('hi').toBe('hi')").testResults[0].result).toBe(
-      PASS
-    )
+    expect(getTestResult("pw.expect('hello').toBe('bonjour')", 0)).toBe(FAIL)
+    expect(getTestResult("pw.expect('hi').toBe('hi')", 0)).toBe(PASS)
   })
 
   test("test for negative assertion (.not.toBe)", () => {
-    expect(runTestScriptWithVariables("pw.expect(1).not.toBe(1)").testResults[0].result).toBe(FAIL)
-    expect(runTestScriptWithVariables("pw.expect(1).not.toBe(2)").testResults[0].result).toBe(PASS)
-    expect(
-      runTestScriptWithVariables("pw.expect('world').not.toBe('planet')").testResults[0].result
-    ).toBe(PASS)
-    expect(
-      runTestScriptWithVariables("pw.expect('world').not.toBe('world')").testResults[0].result
-    ).toBe(FAIL)
+    expect(getTestResult("pw.expect(1).not.toBe(1)", 0)).toBe(FAIL)
+    expect(getTestResult("pw.expect(1).not.toBe(2)", 0)).toBe(PASS)
+    expect(getTestResult("pw.expect('world').not.toBe('planet')", 0)).toBe(PASS)
+    expect(getTestResult("pw.expect('world').not.toBe('world')", 0)).toBe(FAIL)
   })
 })
 
@@ -56,23 +48,16 @@ describe("toHaveProperty", () => {
 
   test("test for positive assertion (.toHaveProperty)", () => {
     expect(
-      runTestScriptWithVariables(`pw.expect(${JSON.stringify(dummyResponse)}).toHaveProperty("id")`)
-        .testResults[0].result
+      getTestResult(`pw.expect(${JSON.stringify(dummyResponse)}).toHaveProperty("id")`, 0)
     ).toBe(PASS)
-    expect(
-      runTestScriptWithVariables(`pw.expect(${dummyResponse.id}).toBe(843)`).testResults[0].result
-    ).toBe(PASS)
+    expect(getTestResult(`pw.expect(${dummyResponse.id}).toBe(843)`, 0)).toBe(PASS)
   })
   test("test for negative assertion (.not.toHaveProperty)", () => {
     expect(
-      runTestScriptWithVariables(
-        `pw.expect(${JSON.stringify(dummyResponse)}).not.toHaveProperty("type")`
-      ).testResults[0].result
+      getTestResult(`pw.expect(${JSON.stringify(dummyResponse)}).not.toHaveProperty("type")`, 0)
     ).toBe(PASS)
     expect(
-      runTestScriptWithVariables(
-        `pw.expect(${JSON.stringify(dummyResponse)}).toHaveProperty("type")`
-      ).testResults[0].result
+      getTestResult(`pw.expect(${JSON.stringify(dummyResponse)}).toHaveProperty("type")`, 0)
     ).toBe(FAIL)
   })
 })

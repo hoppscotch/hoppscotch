33dd854ece4d21643e329774c0a62cf65e86bca2
Earl Brown <github@theGleep.com>
2021-02-08 13:36:24 -0700

    workspace popover ui behaving but no implementation

---
+++ components/workspace.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <button class="icon" @click="openWorkspace" v-close-popover>
+        <i class="material-icons"></i>
+        <span>Open Workspace</span>
+      </button>
+    </div>
+    <div>
+      <button class="icon" @click="saveWorkspace" v-close-popover>
+        <i class="material-icons">save</i>
+        <span>Save Workspace</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // data() {
+  //   return {}
+  // },
+  methods: {
+    openWorkspace() {},
+    saveWorkspace() {},
+  },
+}
+</script>

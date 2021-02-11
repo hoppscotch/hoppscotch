84dc31d636c94a76ecad9b5510b28189828b7566
raghav0307 <raghav.gupta0307@gmail.com>
2021-02-12 00:27:06 +0530

    chore: disable new collection button for VIEWER

--- components/collections/index.vue
+++ components/collections/index.vue
@@ -13,7 +13,10 @@
         class="rounded-t-lg"
       />
     </div>
-    <add-collection :show="showModalAdd" @hide-modal="displayModalAdd(false)" />
+    <add-collection 
+      :collectionsType="collectionsType" 
+      :show="showModalAdd" 
+      @hide-modal="displayModalAdd(false)" />
     <edit-collection
       :show="showModalEdit"
       :editing-collection="editingCollection"
@@ -48,7 +51,13 @@
       @hide-modal="displayModalImportExport(false)"
     />
     <div class="border-b row-wrapper border-brdColor">
-      <button class="icon" @click="displayModalAdd(true)">
+      <button v-if="collectionsType.type=='team-collections' && collectionsType.selectedTeam.myRole == 'VIEWER'" class="icon" @click="displayModalAdd(true)" disabled>
+        <i class="material-icons">add</i>
+        <div  v-tooltip.left="$t('disable_new_collection')">
+          <span>{{ $t("new") }}</span>
+        </div>
+      </button>
+      <button v-else class="icon" @click="displayModalAdd(true)">
         <i class="material-icons">add</i>
         <span>{{ $t("new") }}</span>
       </button>
@@ -130,6 +139,7 @@ export default {
                 myTeams {
                     id
                     name
+                    myRole
                 }
             }
         `,

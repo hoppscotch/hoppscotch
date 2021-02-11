58096c872574f8a15c0d6c5e0ee1bc9b316df452
raghav0307 <raghav.gupta0307@gmail.com>
2021-02-12 00:27:49 +0530

    chore: add myRole in GraphQl query

--- components/collections/choose-collection-type.vue
+++ components/collections/choose-collection-type.vue
@@ -68,6 +68,7 @@ export default {
                     myTeams {
                         id
                         name
+                        myRole
                     }
                 }
             `,

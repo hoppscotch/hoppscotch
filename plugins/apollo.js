aee1d1970f6b83a16046d541247f7fe84e13605c
Andrew Bastin <andrewbastin.k@gmail.com>
2021-03-18 10:28:17 -0400

    Add websocket endpoint to apollo

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -4,5 +4,9 @@ export default () => {
       process.env.CONTEXT === "production"
         ? "https://api.hoppscotch.io/graphql"
         : "https://api.hoppscotch.io/graphql",
+    wsEndpoint:
+      process.env.CONTEXT === "production"
+        ? "wss://api.hoppscotch.io/graphql"
+        : "wss://api.hoppscotch.io/graphql",
   }
 }

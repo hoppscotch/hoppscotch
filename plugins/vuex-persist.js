92a42269e7cb6561d14f24f359b234b70c42e33f
Andrew Bastin <andrewbastin.k@gmail.com>
2021-08-14 21:27:22 +0530

    refactor: remove vuex dependency

--- plugins/vuex-persist.js
+++
@@ -1,7 +0,0 @@
-import VuexPersistence from "vuex-persist"
-
-export default ({ store }) => {
-  new VuexPersistence({
-    filter: (mutation) => mutation.type !== "setFilesBodyParams",
-  }).plugin(store)
-}

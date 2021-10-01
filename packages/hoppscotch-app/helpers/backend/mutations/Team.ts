539034df2a3453a5fd907c2b3a9584c08d8457e3
Andrew Bastin <andrewbastin.k@gmail.com>
2021-10-01 22:41:30 +0530

    refactor: refactor create team modal to new system

--- packages/hoppscotch-app/helpers/backend/mutations/Team.ts
+++ packages/hoppscotch-app/helpers/backend/mutations/Team.ts
@@ -1,12 +1,47 @@
 import gql from "graphql-tag"
+import { pipe } from "fp-ts/function"
+import * as TE from "fp-ts/TaskEither"
 import { runMutation } from "../GQLClient"
+import { TeamName } from "../types/TeamName"
 
 type DeleteTeamErrors =
   | "team/not_required_role"
   | "team/invalid_id"
   | "team/member_not_found"
+  | "ea/not_invite_or_admin"
 
-type ExitTeamErrors = "team/invalid_id" | "team/member_not_found"
+type ExitTeamErrors =
+  | "team/invalid_id"
+  | "team/member_not_found"
+  | "ea/not_invite_or_admin"
+
+type CreateTeamErrors = "team/name_invalid" | "ea/not_invite_or_admin"
+
+export const createTeam = (name: TeamName) =>
+  pipe(
+    runMutation<
+      {
+        createTeam: {
+          id: string
+          name: string
+        }
+      },
+      CreateTeamErrors
+    >(
+      gql`
+        mutation CreateTeam($name: String!) {
+          createTeam(name: $name) {
+            id
+            name
+          }
+        }
+      `,
+      {
+        name,
+      }
+    ),
+    TE.map(({ createTeam }) => createTeam)
+  )
 
 export const deleteTeam = (teamID: string) =>
   runMutation<void, DeleteTeamErrors>(

49bdf9f2031f2a3677561e87b8ab290b33e5cc66
liyasthomas <liyascthomas@gmail.com>
2021-10-13 16:12:02 +0530

    feat: init teams invite page

---
+++ packages/hoppscotch-app/pages/teams-invite.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex flex-col min-h-screen items-center justify-center">
+    <h1 class="heading">{{ $t("team.join_team", { team: "Tesla" }) }}</h1>
+    <p class="text-secondaryLight mt-2">
+      {{ $t("team.invited_to_team", { owner: "Elon Musk", team: "Tesla" }) }}
+    </p>
+    <div v-if="currentUser === null" class="mt-8">
+      <ButtonPrimary
+        :label="$t('auth.login_to_hoppscotch')"
+        @click.native="showLogin = true"
+      />
+    </div>
+    <div v-else class="mt-8">
+      <ButtonPrimary
+        :label="$t('team.join_team', { team: 'Tesla' })"
+        @click.native="joinTeam"
+      />
+    </div>
+    <!-- <SmartSpinner v-if="loading" />
+    <SmartLoadingIndicator v-else />
+    <pre v-if="error">{{ error }}</pre> -->
+    <FirebaseLogin :show="showLogin" @hide-modal="showLogin = false" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "@nuxtjs/composition-api"
+import { initializeFirebase } from "~/helpers/fb"
+import { currentUser$ } from "~/helpers/fb/auth"
+import { useReadonlyStream } from "~/helpers/utils/composables"
+initializeFirebase()
+
+const currentUser = useReadonlyStream(currentUser$, null)
+
+// const loading = ref(false)
+// const error = ref(null)
+const showLogin = ref(false)
+
+const joinTeam = () => {}
+</script>

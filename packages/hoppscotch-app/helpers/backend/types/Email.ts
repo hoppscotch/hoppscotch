dea3a34e3dd99d470cf9065fb416a30ea0e508b0
Andrew Bastin <andrewbastin.k@gmail.com>
2021-10-15 20:55:52 +0530

    feat: team invitation mutations

---
+++ packages/hoppscotch-app/helpers/backend/types/Email.ts
@@ -0,0 +1,15 @@
+import * as t from "io-ts"
+
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ 
+
+interface EmailBrand {
+  readonly Email: unique symbol
+}
+
+export const EmailCodec = t.brand(
+  t.string,
+  (x): x is t.Branded<string, EmailBrand> => emailRegex.test(x),
+  "Email"
+)
+
+export type Email = t.TypeOf<typeof EmailCodec>

245b8a6e3c80510987e5dca99c854fc6bf4efb76
Andrew Bastin <andrewbastin.k@gmail.com>
2021-11-10 14:31:00 +0530

    fix: compiling issues for codemirror-lang-graphql

--- packages/codemirror-lang-graphql/rollup.config.js
+++ packages/codemirror-lang-graphql/rollup.config.js
@@ -2,7 +2,7 @@ import typescript from "rollup-plugin-ts"
 import {lezer} from "@lezer/generator/rollup"
 
 export default {
-  input: "src/index.ts",
+  input: "src/index.js",
   external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
   output: [
     {file: "dist/index.cjs", format: "cjs"},

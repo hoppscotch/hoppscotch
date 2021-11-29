a619cabf4cb551ca191c368c6a575b5e03c2be1e
devblin <dhruwxl@gmail.com>
2021-11-29 13:26:08 +0530

    fix: updated javascript-jquery codegen

--- packages/hoppscotch-app/helpers/codegen/generators/javascript-jquery.js
+++ packages/hoppscotch-app/helpers/codegen/generators/javascript-jquery.js
@@ -14,7 +14,6 @@ export const JavascriptJqueryCodegen = {
     rawInput,
     rawParams,
     rawRequestBody,
-    contentType,
     headers,
   }) => {
     const requestString = []
@@ -24,20 +23,16 @@ export const JavascriptJqueryCodegen = {
       `jQuery.ajax({\n  url: "${url}${pathName}?${queryString}"`
     )
     requestString.push(`,\n  method: "${method.toUpperCase()}"`)
-    const requestBody = rawInput ? rawParams : rawRequestBody
+    let requestBody = rawInput ? rawParams : rawRequestBody
 
-    if (requestBody.length !== 0) {
-      requestString.push(`,\n  body: ${requestBody}`)
+    if (requestBody && requestBody.length !== 0) {
+      requestBody = requestBody.replace("}", "  }")
+      requestString.push(`,\n  data: ${requestBody}`)
     }
     if (headers) {
       headers.forEach(({ key, value }) => {
         if (key) genHeaders.push(`    "${key}": "${value}",\n`)
       })
-    }
-
-    if (contentType) {
-      genHeaders.push(`    "Content-Type": "${contentType}; charset=utf-8",\n`)
-      requestString.push(`,\n  contentType: "${contentType}; charset=utf-8"`)
     }
 
     if (auth === "Basic Auth") {
@@ -50,10 +45,12 @@ export const JavascriptJqueryCodegen = {
     } else if (auth === "Bearer Token" || auth === "OAuth 2.0") {
       genHeaders.push(`    "Authorization": "Bearer ${bearerToken}",\n`)
     }
-    requestString.push(
-      `,\n  headers: {\n${genHeaders.join("").slice(0, -2)}\n  }\n})`
-    )
-    requestString.push(".then(response => {\n")
+    if (genHeaders.length > 0) {
+      requestString.push(
+        `,\n  headers: {\n${genHeaders.join("").slice(0, -2)}\n  }`
+      )
+    }
+    requestString.push("\n}).then(response => {\n")
     requestString.push("    console.log(response);\n")
     requestString.push("})")
     requestString.push(".catch(e => {\n")

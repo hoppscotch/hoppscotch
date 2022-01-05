ffd1acdfae22f4b82a1a09e7ccf9a7afc2a06d5a
liyasthomas <liyascthomas@gmail.com>
2022-01-05 10:17:23 +0530

    chore: lint

--- packages/codemirror-lang-graphql/rollup.config.js
+++ packages/codemirror-lang-graphql/rollup.config.js
@@ -1,12 +1,12 @@
 import typescript from "rollup-plugin-ts"
-import {lezer} from "@lezer/generator/rollup"
+import { lezer } from "@lezer/generator/rollup"
 
 export default {
   input: "src/index.js",
-  external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
+  external: (id) => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
   output: [
-    {file: "dist/index.cjs", format: "cjs"},
-    {dir: "./dist", format: "es"}
+    { file: "dist/index.cjs", format: "cjs" },
+    { dir: "./dist", format: "es" },
   ],
-  plugins: [lezer(), typescript()]
+  plugins: [lezer(), typescript()],
 }

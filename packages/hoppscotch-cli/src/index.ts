44881e0b139a7626568af6d65d2071b8499f4fde
Andrew Bastin <andrewbastin.k@gmail.com>
2022-03-17 21:02:01 +0530

    refactor: update help screen to be more colorful

--- packages/hoppscotch-cli/src/index.ts
+++ packages/hoppscotch-cli/src/index.ts
@@ -1,5 +1,8 @@
+import chalk from "chalk";
 import { program } from "commander";
 import * as E from "fp-ts/Either";
+import * as A from "fp-ts/Array";
+import { pipe } from "fp-ts/function";
 import { version } from "../package.json";
 import { run } from "./commands";
 import { handleError } from "./handlers/error";
@@ -8,9 +11,26 @@ import { handleError } from "./handlers/error";
  * * Program Default Configuration
  */
 program
-  .name("hopp-cli")
+  .name("hopp")
   .version(version, "-v, --ver", "see the current version of hopp-cli")
-  .usage("[options or commands] arguments");
+  .usage("[options or commands] arguments")
+  .addHelpText("beforeAll", `hopp: The ${chalk.greenBright("Hoppscotch")} CLI - Version ${version} (${chalk.greenBright("https://hoppscotch.io")})\n`)
+  .addHelpText("afterAll", `\nFor more help, head on to ${chalk.greenBright("https://docs.hoppscotch.io/")}`)
+  .configureHelp({
+    optionTerm: (option) =>
+      chalk.greenBright(
+        option.flags
+      ),
+    subcommandTerm: (cmd) =>
+      chalk.greenBright(
+        cmd.name(),
+        cmd.usage()
+      ),
+    argumentTerm: (arg) =>
+      chalk.greenBright(
+        arg.name()
+      )
+  });
 
 program.exitOverride().configureOutput({
   writeErr: (str) => {},

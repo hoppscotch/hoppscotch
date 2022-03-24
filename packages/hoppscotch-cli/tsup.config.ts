eec3a71a1c70ac49835d02ffd06ec24ff98b9c43
Andrew Bastin <andrewbastin.k@gmail.com>
2022-03-25 02:14:58 +0530

    fix: update cli deps and bundling to be npm friendly

--- packages/hoppscotch-cli/tsup.config.ts
+++ packages/hoppscotch-cli/tsup.config.ts
@@ -3,7 +3,17 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: [ "./src/index.ts" ],
   outDir: "./dist/",
-  format: ["esm", "cjs"],
+  format: ["cjs"],
+  platform: "node",
   sourcemap: true,
+  bundle: true,
+  target: "node12",
+  skipNodeModulesBundle: false,
+  esbuildOptions(options) {
+    options.bundle = true
+  },
+  noExternal:  [
+    /\w+/
+  ],
   clean: true,
 });

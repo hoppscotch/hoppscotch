108f30734e05ed0dc88cce5a53b3eeff0e183610
Anwarul Islam <anwaarulislaam@gmail.com>
2022-04-08 14:56:23 +0600

    refactor: migrate from options api to script setup with ts

--- packages/hoppscotch-app/components/realtime/Socketio.vue
+++ packages/hoppscotch-app/components/realtime/Socketio.vue
@@ -45,7 +45,7 @@
               spellcheck="false"
               :class="{ error: !urlValid }"
               class="flex flex-1 w-full px-4 py-2 border bg-primaryLight border-divider text-secondaryDark"
-              :placeholder="$t('socketio.url')"
+              :placeholder="`${t('socketio.url')}`"
               :disabled="connectionState"
               @keyup.enter="urlValid ? toggleConnection() : null"
             />
@@ -64,7 +64,7 @@
             name="connect"
             class="w-32"
             :label="
-              !connectionState ? $t('action.connect') : $t('action.disconnect')
+              !connectionState ? t('action.connect') : t('action.disconnect')
             "
             :loading="connectingState"
             @click.native="toggleConnection"
@@ -76,7 +76,7 @@
       >
         <span class="flex items-center">
           <label class="font-semibold text-secondaryLight">
-            {{ $t("authorization.type") }}
+            {{ t("authorization.type") }}
           </label>
           <tippy
             ref="authTypeOptions"
@@ -133,18 +133,18 @@
             class="px-2"
             @change="authActive = !authActive"
           >
-            {{ $t("state.enabled") }}
+            {{ t("state.enabled") }}
           </SmartCheckbox>
           <ButtonSecondary
             v-tippy="{ theme: 'tooltip' }"
             to="https://docs.hoppscotch.io/features/authorization"
             blank
-            :title="$t('app.wiki')"
+            :title="t('app.wiki')"
             svg="help-circle"
           />
           <ButtonSecondary
             v-tippy="{ theme: 'tooltip' }"
-            :title="$t('action.clear')"
+            :title="t('action.clear')"
             svg="trash-2"
             @click.native="clearContent"
           />
@@ -158,14 +158,14 @@
           :src="`/images/states/${$colorMode.value}/login.svg`"
           loading="lazy"
           class="inline-flex flex-col object-contain object-center w-16 h-16 my-4"
-          :alt="$t('empty.authorization')"
+          :alt="`${t('empty.authorization')}`"
         />
         <span class="pb-4 text-center">
           This SocketIO connection does not use any authentication.
         </span>
         <ButtonSecondary
           outline
-          :label="$t('app.documentation')"
+          :label="t('app.documentation')"
           to="https://docs.hoppscotch.io/features/authorization"
           blank
           svg="external-link"
@@ -187,11 +187,11 @@
         >
           <div class="p-2">
             <div class="pb-2 text-secondaryLight">
-              {{ $t("helpers.authorization") }}
+              {{ t("helpers.authorization") }}
             </div>
             <SmartAnchor
               class="link"
-              :label="`${$t('authorization.learn')} \xA0 →`"
+              :label="`${t('authorization.learn')} \xA0 →`"
               to="https://docs.hoppscotch.io/features/authorization"
               blank
             />
@@ -200,12 +200,12 @@
       </div>
     </template>
     <template #secondary>
-      <RealtimeLog :title="$t('socketio.log')" :log="log" />
+      <RealtimeLog :title="t('socketio.log')" :log="log" />
     </template>
     <template #sidebar>
       <div class="flex items-center justify-between p-4">
         <label for="events" class="font-semibold text-secondaryLight">
-          {{ $t("socketio.events") }}
+          {{ t("socketio.events") }}
         </label>
       </div>
       <div class="flex px-4">
@@ -214,7 +214,7 @@
           v-model="communication.eventName"
           class="input"
           name="event_name"
-          :placeholder="$t('socketio.event_name')"
+          :placeholder="`${t('socketio.event_name')}`"
           type="text"
           autocomplete="off"
           :disabled="!connectionState"
@@ -222,12 +222,12 @@
       </div>
       <div class="flex items-center justify-between p-4">
         <label class="font-semibold text-secondaryLight">
-          {{ $t("socketio.communication") }}
+          {{ t("socketio.communication") }}
         </label>
         <div class="flex">
           <ButtonSecondary
             v-tippy="{ theme: 'tooltip' }"
-            :title="$t('add.new')"
+            :title="t('add.new')"
             svg="plus"
             @click.native="addCommunicationInput"
           />
@@ -243,7 +243,7 @@
               v-model="communication.inputs[index]"
               class="input"
               name="message"
-              :placeholder="$t('count.message', { count: index + 1 })"
+              :placeholder="`${t('count.message', { count: index + 1 })}`"
               type="text"
               autocomplete="off"
               :disabled="!connectionState"
@@ -252,18 +252,18 @@
             <ButtonSecondary
               v-if="index + 1 !== communication.inputs.length"
               v-tippy="{ theme: 'tooltip' }"
-              :title="$t('action.remove')"
+              :title="t('action.remove')"
               svg="trash"
               color="red"
               outline
-              @click.native="removeCommunicationInput({ index })"
+              @click.native="removeCommunicationInput(index)"
             />
             <ButtonPrimary
               v-if="index + 1 === communication.inputs.length"
               id="send"
               name="send"
               :disabled="!connectionState"
-              :label="$t('action.send')"
+              :label="t('action.send')"
               @click.native="sendMessage"
             />
           </div>
@@ -273,8 +273,14 @@
   </AppPaneLayout>
 </template>
 
-<script>
-import { defineComponent, ref } from "@nuxtjs/composition-api"
+<script setup lang="ts">
+import {
+  ref,
+  onUnmounted,
+  computed,
+  reactive,
+  watch,
+} from "@nuxtjs/composition-api"
 // All Socket.IO client version imports
 import ClientV2 from "socket.io-client-v2"
 import { io as ClientV3 } from "socket.io-client-v3"
@@ -299,7 +305,16 @@ import {
   setSIOLog,
   addSIOLogLine,
 } from "~/newstore/SocketIOSession"
-import { useStream } from "~/helpers/utils/composables"
+import {
+  useStream,
+  useI18n,
+  useToast,
+  useNuxt,
+} from "~/helpers/utils/composables"
+
+const t = useI18n()
+const toast = useToast()
+const nuxt = useNuxt()
 
 const socketIoClients = {
   v4: ClientV4,
@@ -307,215 +322,207 @@ const socketIoClients = {
   v2: ClientV2,
 }
 
-export default defineComponent({
-  setup() {
-    return {
-      socketIoClients,
-      url: useStream(SIOEndpoint$, "", setSIOEndpoint),
-      clientVersion: useStream(SIOVersion$, "", setSIOVersion),
-      path: useStream(SIOPath$, "", setSIOPath),
-      connectingState: useStream(
-        SIOConnectingState$,
-        false,
-        setSIOConnectingState
-      ),
-      connectionState: useStream(
-        SIOConnectionState$,
-        false,
-        setSIOConnectionState
-      ),
-      io: useStream(SIOSocket$, null, setSIOSocket),
-      log: useStream(SIOLog$, [], setSIOLog),
-      authTypeOptions: ref(null),
+const url = useStream(SIOEndpoint$, "", setSIOEndpoint)
+const clientVersion = useStream(SIOVersion$, "", setSIOVersion)
+const path = useStream(SIOPath$, "", setSIOPath)
+const connectingState = useStream(
+  SIOConnectingState$,
+  false,
+  setSIOConnectingState
+)
+const connectionState = useStream(
+  SIOConnectionState$,
+  false,
+  setSIOConnectionState
+)
+const io = useStream(SIOSocket$, null, setSIOSocket)
+const log = useStream(SIOLog$, [], setSIOLog)
+const authTypeOptions = ref<any>(null)
+const versionOptions = ref<any>(null)
+
+const isUrlValid = ref(true)
+const communication = reactive({
+  eventName: "",
+  inputs: [""],
+})
+const authType = ref("None")
+const bearerToken = ref("")
+const authActive = ref(true)
+
+const urlValid = computed(() => isUrlValid.value)
+
+let worker: Worker
+
+const workerResponseHandler = ({ data }) => {
+  if (data.url === url.value) isUrlValid.value = data.result
+}
+
+if (process.browser) {
+  worker = nuxt.value.$worker.createRejexWorker()
+  worker.addEventListener("message", workerResponseHandler)
+}
+
+watch(url, (newUrl) => {
+  if (newUrl) debouncer()
+})
+
+watch(connectionState, (connected) => {
+  if (connected) versionOptions.tippy().disable()
+  else versionOptions.tippy().enable()
+})
+
+onUnmounted(() => {
+  worker.terminate()
+})
+
+const debouncer = debounce(function () {
+  worker.postMessage({ type: "socketio", url: url.value })
+}, 1000)
+
+const removeCommunicationInput = (index: number) => {
+  nuxt.value.$delete(communication.inputs, index)
+}
+const addCommunicationInput = () => {
+  communication.inputs.push("")
+}
+const toggleConnection = () => {
+  // If it is connecting:
+  if (!connectionState.value) return connect()
+  // Otherwise, it's disconnecting.
+  else return disconnect()
+}
+const connect = () => {
+  connectingState.value = true
+  log.value = [
+    {
+      payload: `${t("state.connecting_to", { name: url.value })}`,
+      source: "info",
+      color: "var(--accent-color)",
+      ts: "",
+    },
+  ]
+
+  try {
+    if (!path.value) {
+      path.value = "/socket.io"
     }
-  },
-  data() {
-    return {
-      isUrlValid: true,
-      communication: {
-        eventName: "",
-        inputs: [""],
-      },
-      authType: "None",
-      bearerToken: "",
-      authActive: true,
+    const Client = socketIoClients[clientVersion.value]
+    if (authActive.value && authType.value === "Bearer") {
+      io.value = new Client(url.value, {
+        path: path.value,
+        auth: {
+          token: bearerToken.value,
+        },
+      })
+    } else {
+      io.value = new Client(url.value, { path: path.value })
     }
-  },
-  computed: {
-    urlValid() {
-      return this.isUrlValid
-    },
-  },
-  watch: {
-    url() {
-      this.debouncer()
-    },
-    connectionState(connected) {
-      if (connected) this.$refs.versionOptions.tippy().disable()
-      else this.$refs.versionOptions.tippy().enable()
-    },
-  },
-  created() {
-    if (process.browser) {
-      this.worker = this.$worker.createRejexWorker()
-      this.worker.addEventListener("message", this.workerResponseHandler)
-    }
-  },
-  destroyed() {
-    this.worker.terminate()
-  },
-  methods: {
-    debouncer: debounce(function () {
-      this.worker.postMessage({ type: "socketio", url: this.url })
-    }, 1000),
-    workerResponseHandler({ data }) {
-      if (data.url === this.url) this.isUrlValid = data.result
-    },
-    removeCommunicationInput({ index }) {
-      this.$delete(this.communication.inputs, index)
-    },
-    addCommunicationInput() {
-      this.communication.inputs.push("")
-    },
-    toggleConnection() {
-      // If it is connecting:
-      if (!this.connectionState) return this.connect()
-      // Otherwise, it's disconnecting.
-      else return this.disconnect()
-    },
-    connect() {
-      this.connectingState = true
-      this.log = [
+
+    // Add ability to listen to all events
+    wildcard(Client.Manager)(io.value)
+    io.value.on("connect", () => {
+      connectingState.value = false
+      connectionState.value = true
+      log.value = [
         {
-          payload: this.$t("state.connecting_to", { name: this.url }),
+          payload: `${t("state.connected_to", { name: url.value })}`,
           source: "info",
           color: "var(--accent-color)",
+          ts: new Date().toLocaleTimeString(),
         },
       ]
-
-      try {
-        if (!this.path) {
-          this.path = "/socket.io"
-        }
-        const Client = socketIoClients[this.clientVersion]
-        if (this.authActive && this.authType === "Bearer") {
-          this.io = new Client(this.url, {
-            path: this.path,
-            auth: {
-              token: this.bearerToken,
-            },
-          })
-        } else {
-          this.io = new Client(this.url, { path: this.path })
-        }
-
-        // Add ability to listen to all events
-        wildcard(Client.Manager)(this.io)
-        this.io.on("connect", () => {
-          this.connectingState = false
-          this.connectionState = true
-          this.log = [
-            {
-              payload: this.$t("state.connected_to", { name: this.url }),
-              source: "info",
-              color: "var(--accent-color)",
-              ts: new Date().toLocaleTimeString(),
-            },
-          ]
-          this.$toast.success(this.$t("state.connected"))
-        })
-        this.io.on("*", ({ data }) => {
-          const [eventName, message] = data
-          addSIOLogLine({
-            payload: `[${eventName}] ${message ? JSON.stringify(message) : ""}`,
-            source: "server",
-            ts: new Date().toLocaleTimeString(),
-          })
-        })
-        this.io.on("connect_error", (error) => {
-          this.handleError(error)
-        })
-        this.io.on("reconnect_error", (error) => {
-          this.handleError(error)
-        })
-        this.io.on("error", () => {
-          this.handleError()
-        })
-        this.io.on("disconnect", () => {
-          this.connectingState = false
-          this.connectionState = false
-          addSIOLogLine({
-            payload: this.$t("state.disconnected_from", { name: this.url }),
-            source: "info",
-            color: "#ff5555",
-            ts: new Date().toLocaleTimeString(),
-          })
-          this.$toast.error(this.$t("state.disconnected"))
-        })
-      } catch (e) {
-        this.handleError(e)
-        this.$toast.error(this.$t("error.something_went_wrong"))
-      }
-
-      logHoppRequestRunToAnalytics({
-        platform: "socketio",
-      })
-    },
-    disconnect() {
-      this.io.close()
-    },
-    handleError(error) {
-      this.disconnect()
-      this.connectingState = false
-      this.connectionState = false
+      toast.success(`${t("state.connected")}`)
+    })
+    io.value.on("*", ({ data }) => {
+      const [eventName, message] = data
       addSIOLogLine({
-        payload: this.$t("error.something_went_wrong"),
+        payload: `[${eventName}] ${message ? JSON.stringify(message) : ""}`,
+        source: "server",
+        ts: new Date().toLocaleTimeString(),
+      })
+    })
+    io.value.on("connect_error", (error: any) => {
+      handleError(error)
+    })
+    io.value.on("reconnect_error", (error: any) => {
+      handleError(error)
+    })
+    io.value.on("error", (error: any) => {
+      handleError(error)
+    })
+    io.value.on("disconnect", () => {
+      connectingState.value = false
+      connectionState.value = false
+      addSIOLogLine({
+        payload: `${t("state.disconnected_from", { name: url.value })}`,
         source: "info",
         color: "#ff5555",
         ts: new Date().toLocaleTimeString(),
       })
-      if (error !== null)
-        addSIOLogLine({
-          payload: error,
-          source: "info",
-          color: "#ff5555",
-          ts: new Date().toLocaleTimeString(),
-        })
-    },
-    sendMessage() {
-      const eventName = this.communication.eventName
-      const messages = (this.communication.inputs || [])
-        .map((input) => {
-          try {
-            return JSON.parse(input)
-          } catch (e) {
-            return input
-          }
-        })
-        .filter((message) => !!message)
+      toast.error(`${t("state.disconnected")}`)
+    })
+  } catch (e) {
+    handleError(e)
+    toast.error(`${t("error.something_went_wrong")}`)
+  }
 
-      if (this.io) {
-        this.io.emit(eventName, ...messages, (data) => {
-          // receive response from server
-          addSIOLogLine({
-            payload: `[${eventName}] ${JSON.stringify(data)}`,
-            source: "server",
-            ts: new Date().toLocaleTimeString(),
-          })
-        })
-
-        addSIOLogLine({
-          payload: `[${eventName}] ${JSON.stringify(messages)}`,
-          source: "client",
-          ts: new Date().toLocaleTimeString(),
-        })
-        this.communication.inputs = [""]
+  logHoppRequestRunToAnalytics({
+    platform: "socketio",
+  })
+}
+const disconnect = () => {
+  io.value.close()
+}
+const handleError = (error: any) => {
+  disconnect()
+  connectingState.value = false
+  connectionState.value = false
+  addSIOLogLine({
+    payload: `${t("error.something_went_wrong")}`,
+    source: "info",
+    color: "#ff5555",
+    ts: new Date().toLocaleTimeString(),
+  })
+  if (error !== null)
+    addSIOLogLine({
+      payload: error,
+      source: "info",
+      color: "#ff5555",
+      ts: new Date().toLocaleTimeString(),
+    })
+}
+const sendMessage = () => {
+  const eventName = communication.eventName
+  const messages = (communication.inputs || [])
+    .map((input) => {
+      try {
+        return JSON.parse(input)
+      } catch (e) {
+        return input
       }
-    },
-    onSelectVersion(version) {
-      this.clientVersion = version
-      this.$refs.versionOptions.tippy().hide()
-    },
-  },
-})
+    })
+    .filter((message) => !!message)
+
+  if (io.value) {
+    io.value.emit(eventName, ...messages, (data: object) => {
+      // receive response from server
+      addSIOLogLine({
+        payload: `[${eventName}] ${JSON.stringify(data)}`,
+        source: "server",
+        ts: new Date().toLocaleTimeString(),
+      })
+    })
+
+    addSIOLogLine({
+      payload: `[${eventName}] ${JSON.stringify(messages)}`,
+      source: "client",
+      ts: new Date().toLocaleTimeString(),
+    })
+    communication.inputs = [""]
+  }
+}
+const onSelectVersion = (version: string) => {
+  clientVersion.value = version
+  versionOptions.tippy().hide()
+}
 </script>

1ab5f783850bab1166d8cb26378b123088294f9b
amk-dev <akash.k.mohan98@gmail.com>
2022-04-13 16:36:43 +0530

    feat: define importer for url import

---
+++ packages/hoppscotch-app/helpers/import-export/import/url.ts
@@ -0,0 +1,34 @@
+import { pipe } from "fp-ts/function"
+import * as TE from "fp-ts/TaskEither"
+import * as TO from "fp-ts/TaskOption"
+import axios from "axios"
+import { HoppRESTRequest, HoppCollection } from "@hoppscotch/data"
+import { step } from "../steps"
+import { defineImporter, IMPORTER_INVALID_FILE_FORMAT } from "."
+
+const fetchHoppCollectionFromUrl = (
+  url: string
+): TO.TaskOption<HoppCollection<HoppRESTRequest>> =>
+  pipe(
+    TO.tryCatch(() => axios.get(url)),
+    TO.chain((res) => pipe(res.data, TO.some))
+  )
+
+export default defineImporter({
+  name: "Import Hoppscotch Collection From URL",
+  icon: "github",
+  steps: [
+    step({
+      stepName: "URL_IMPORT",
+      metadata: {
+        caption: "Import collection from url",
+        placeholder: "Enter url",
+      },
+    }),
+  ],
+  importer: ([url]) =>
+    pipe(
+      fetchHoppCollectionFromUrl(url),
+      TE.fromTaskOption(() => IMPORTER_INVALID_FILE_FORMAT)
+    ),
+})

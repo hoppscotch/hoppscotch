f2046a8f6ef225cd9fbe05b0e3fac3d291922260
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2022-04-20 17:49:33 +0530

    refactor: added codemirror instance for json responses

--- packages/hoppscotch-app/components/realtime/LogEntry.vue
+++ packages/hoppscotch-app/components/realtime/LogEntry.vue
@@ -1,54 +1,192 @@
 <template>
-  <div
-    v-if="entry"
-    :style="{ color: entryColor }"
-    class="font-uniform realtime-log"
-    @mouseover="invertHover()"
-    @mouseout="invertHover()"
-  >
-    <div class="border-y divide-y divide-dividerLight border-dividerLight">
-      <div class="flex divide-x divide-dividerLight">
-        <div class="inline-flex items-center">
+  <div v-if="entry">
+    <div
+      :style="{ color: entryColor }"
+      class="font-uniform realtime-log"
+      @mouseover="invertHover()"
+      @mouseout="invertHover()"
+    >
+      <div class="border-y divide-y divide-dividerLight border-dividerLight">
+        <div class="flex divide-x divide-dividerLight">
+          <div class="inline-flex items-center">
+            <ButtonSecondary
+              v-tippy="{ theme: 'tooltip' }"
+              :svg="`${source(entry.source)}`"
+              @click.native="copyQuery(entry.payload)"
+            />
+          </div>
+          <div class="hidden sm:inline-flex items-center px-1 w-16">
+            <span
+              ref="timestampEl"
+              class="ts-font text-secondaryLight hover:text-secondary mx-auto"
+            >
+              {{
+                timestampHovered
+                  ? relativeTime
+                  : new Date(entry.ts).toLocaleTimeString()
+              }}
+            </span>
+          </div>
+
+          <div class="flex-1 inline-flex items-center px-1.5 truncate">
+            <div @click="toggleExpandPayload()">{{ entry.payload }}</div>
+          </div>
+          <div :class="expandCopy" class="ml-1 items-center">
+            <ButtonSecondary
+              v-tippy="{ theme: 'tooltip' }"
+              :title="t('action.copy')"
+              :svg="`${copyQueryIcon}`"
+              @click.native="copyQuery(entry.payload)"
+            />
+          </div>
+          <div class="inline-flex items-center">
+            <ButtonSecondary
+              svg="chevron-down"
+              class="transform"
+              :class="{ 'rotate-180': !minimized }"
+              @click.native="toggleExpandPayload()"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="!minimized && isJSON(entry.payload)">
+      <div
+        class="z-10 flex items-center justify-between pl-4 border-b bg-primary border-dividerLight top-lowerSecondaryStickyFold"
+      >
+        <label class="font-semibold text-secondaryLight">{{
+          t("response.body")
+        }}</label>
+        <div class="flex">
           <ButtonSecondary
             v-tippy="{ theme: 'tooltip' }"
-            :svg="`${source(entry.source)}`"
-            @click.native="copyQuery(entry.payload)"
+            :title="t('state.linewrap')"
+            :class="{ '!text-accent': linewrapEnabled }"
+            svg="wrap-text"
+            @click.native.prevent="linewrapEnabled = !linewrapEnabled"
           />
-        </div>
-        <div class="hidden sm:inline-flex items-center px-1.1 w-18">
-          <span
-            ref="timestampEl"
-            class="ts-font text-secondaryLight hover:text-secondary"
-          >
-            {{
-              timestampHovered
-                ? relativeTime
-                : new Date(entry.ts).toLocaleTimeString()
-            }}
-          </span>
-        </div>
-
-        <div
-          :class="expandPayload"
-          class="flex-1 inline-flex items-center px-1.5"
-        >
-          <span @click="invertPayload()">{{ entry.payload }}</span>
-        </div>
-        <div :class="expandCopy" class="ml-1 items-center">
           <ButtonSecondary
+            ref="downloadResponse"
+            v-tippy="{ theme: 'tooltip' }"
+            :title="t('action.download_file')"
+            :svg="downloadIcon"
+            @click.native="downloadResponse"
+          />
+          <ButtonSecondary
+            ref="copyResponse"
             v-tippy="{ theme: 'tooltip' }"
             :title="t('action.copy')"
-            :svg="`${copyQueryIcon}`"
-            @click.native="copyQuery(entry.payload)"
+            :svg="copyIcon"
+            @click.native="copyResponse"
           />
         </div>
-        <div class="inline-flex items-center">
-          <ButtonSecondary
-            svg="chevron-down"
-            class="transform"
-            :class="{ 'rotate-180': !payload }"
-            @click.native="invertPayload()"
-          />
+      </div>
+
+      <div ref="jsonResponse"></div>
+      <div
+        v-if="outlinePath"
+        class="sticky bottom-0 z-10 flex px-2 overflow-auto border-t bg-primaryLight border-dividerLight flex-nowrap hide-scrollbar"
+      >
+        <div
+          v-for="(item, index) in outlinePath"
+          :key="`item-${index}`"
+          class="flex items-center"
+        >
+          <tippy
+            ref="outlineOptions"
+            interactive
+            trigger="click"
+            theme="popover"
+            arrow
+          >
+            <template #trigger>
+              <div v-if="item.kind === 'RootObject'" class="outline-item">
+                {}
+              </div>
+              <div v-if="item.kind === 'RootArray'" class="outline-item">
+                []
+              </div>
+              <div v-if="item.kind === 'ArrayMember'" class="outline-item">
+                {{ item.index }}
+              </div>
+              <div v-if="item.kind === 'ObjectMember'" class="outline-item">
+                {{ item.name }}
+              </div>
+            </template>
+            <div
+              v-if="item.kind === 'ArrayMember' || item.kind === 'ObjectMember'"
+            >
+              <div
+                v-if="item.kind === 'ArrayMember'"
+                class="flex flex-col"
+                role="menu"
+              >
+                <SmartItem
+                  v-for="(arrayMember, astIndex) in item.astParent.values"
+                  :key="`ast-${astIndex}`"
+                  :label="`${astIndex}`"
+                  @click.native="
+                    () => {
+                      jumpCursor(arrayMember)
+                      outlineOptions[index].tippy().hide()
+                    }
+                  "
+                />
+              </div>
+              <div
+                v-if="item.kind === 'ObjectMember'"
+                class="flex flex-col"
+                role="menu"
+              >
+                <SmartItem
+                  v-for="(objectMember, astIndex) in item.astParent.members"
+                  :key="`ast-${astIndex}`"
+                  :label="objectMember.key.value"
+                  @click.native="
+                    () => {
+                      jumpCursor(objectMember)
+                      outlineOptions[index].tippy().hide()
+                    }
+                  "
+                />
+              </div>
+            </div>
+            <div
+              v-if="item.kind === 'RootObject'"
+              class="flex flex-col"
+              role="menu"
+            >
+              <SmartItem
+                label="{}"
+                @click.native="
+                  () => {
+                    jumpCursor(item.astValue)
+                    outlineOptions[index].tippy().hide()
+                  }
+                "
+              />
+            </div>
+            <div
+              v-if="item.kind === 'RootArray'"
+              class="flex flex-col"
+              role="menu"
+            >
+              <SmartItem
+                label="[]"
+                @click.native="
+                  () => {
+                    jumpCursor(item.astValue)
+                    outlineOptions[index].tippy().hide()
+                  }
+                "
+              />
+            </div>
+          </tippy>
+          <i
+            v-if="index + 1 !== outlinePath.length"
+            class="opacity-50 text-secondaryLight material-icons"
+            >chevron_right</i
+          >
         </div>
       </div>
     </div>
@@ -57,27 +195,100 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "@nuxtjs/composition-api"
+import * as LJSON from "lossless-json"
+import * as O from "fp-ts/Option"
+import { pipe } from "fp-ts/function"
+import { ref, computed, reactive } from "@nuxtjs/composition-api"
 import { useElementHover, useTimeAgo } from "@vueuse/core"
 import { LogEntryData } from "./Log.vue"
 import { useI18n } from "~/helpers/utils/composables"
 import { copyToClipboard } from "~/helpers/utils/clipboard"
+import { isJSON } from "~/helpers/functional/json"
+import useCopyResponse from "~/helpers/lenses/composables/useCopyResponse"
+import useDownloadResponse from "~/helpers/lenses/composables/useDownloadResponse"
+import { useCodemirror } from "~/helpers/editor/codemirror"
+import jsonParse, { JSONObjectMember, JSONValue } from "~/helpers/jsonParse"
+import { getJSONOutlineAtPos } from "~/helpers/newOutline"
+import {
+  convertIndexToLineCh,
+  convertLineChToIndex,
+} from "~/helpers/editor/utils"
 
 const t = useI18n()
 
-const payload = ref(true)
-const hover = ref(false)
+const props = defineProps<{ entry: LogEntryData }>()
 
-const relativeTime = useTimeAgo(
-  computed(() => {
-    return props.entry.ts
+const outlineOptions = ref<any | null>(null)
+const jsonResponse = ref<any | null>(null)
+const linewrapEnabled = ref(true)
+const logPayload = computed(() => props.entry.payload)
+
+const jsonBodyText = computed(() =>
+  pipe(
+    logPayload.value,
+    O.tryCatchK(LJSON.parse),
+    O.map((val) => LJSON.stringify(val, undefined, 2)),
+    O.getOrElse(() => logPayload.value)
+  )
+)
+
+const { copyIcon, copyResponse } = useCopyResponse(logPayload)
+
+const { downloadIcon, downloadResponse } = useDownloadResponse(
+  "application/json",
+  logPayload
+)
+
+const ast = computed(() =>
+  pipe(
+    jsonBodyText.value,
+    O.tryCatchK(jsonParse),
+    O.getOrElseW(() => null)
+  )
+)
+
+const { cursor } = useCodemirror(
+  jsonResponse,
+  jsonBodyText,
+  reactive({
+    extendedEditorConfig: {
+      mode: "application/ld+json",
+      readOnly: true,
+      lineWrapping: linewrapEnabled,
+    },
+    linter: null,
+    completer: null,
+    environmentHighlights: true,
   })
 )
 
+const jumpCursor = (ast: JSONValue | JSONObjectMember) => {
+  const pos = convertIndexToLineCh(jsonBodyText.value, ast.start)
+  pos.line--
+  cursor.value = pos
+}
+
+const outlinePath = computed(() =>
+  pipe(
+    ast.value,
+    O.fromNullable,
+    O.map((ast) =>
+      getJSONOutlineAtPos(
+        ast,
+        convertLineChToIndex(jsonBodyText.value, cursor.value)
+      )
+    ),
+    O.getOrElseW(() => null)
+  )
+)
+
+const minimized = ref(true)
+const hover = ref(false)
+
+const relativeTime = useTimeAgo(computed(() => props.entry.ts))
+
 const timestampEl = ref()
 const timestampHovered = useElementHover(timestampEl)
-
-const props = defineProps<{ entry: LogEntryData }>()
 
 const entryColor = computed(() => {
   switch (props.entry.event) {
@@ -92,11 +303,6 @@ const entryColor = computed(() => {
   }
 })
 
-const expandPayload = computed(() => {
-  if (payload.value) return "truncate"
-  return "max-w-68 sm:max-w-none overflow-auto"
-})
-
 const expandCopy = computed(() => {
   if (hover.value) return "inline-flex"
   return "hidden"
@@ -109,8 +315,8 @@ const copyQuery = (entry: string) => {
   setTimeout(() => (copyQueryIcon.value = "copy"), 1000)
 }
 
-const invertPayload = () => {
-  payload.value = !payload.value
+const toggleExpandPayload = () => {
+  minimized.value = !minimized.value
 }
 
 const invertHover = () => {
@@ -148,9 +354,21 @@ const source = (source: keyof typeof sourceIcons) => {
     @apply block;
     @apply break-words break-all;
   }
+}
 
-  .ts-font {
-    font-size: 0.62rem;
-  }
+.outline-item {
+  @apply cursor-pointer;
+  @apply flex-grow-0 flex-shrink-0;
+  @apply text-secondaryLight;
+  @apply inline-flex;
+  @apply items-center;
+  @apply px-2;
+  @apply py-1;
+  @apply transition;
+  @apply hover: text-secondary;
+}
+
+.ts-font {
+  font-size: 0.6rem;
 }
 </style>

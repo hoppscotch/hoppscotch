588e12026d5a545025a4bc9a5e702e57524babb1
Nivedin <nivedinp@gmail.com>
2022-05-13 11:11:49 +0000

    fix: rebase error

--- packages/hoppscotch-app/components/profile/Shortcode.vue
+++ packages/hoppscotch-app/components/profile/Shortcode.vue
@@ -1,30 +1,24 @@
 <template>
   <div class="block my-10 lg:my-0 lg:table-row lg:p-2">
     <div class="table-col" :data-label="t('shortcodes.short_code')">
-      {{ shortCode.id }}
+      {{ shortcode.id }}
     </div>
     <div
       class="table-col"
-      :class="
-        getRequestLabelColor(parseShortcodeRequest(shortCode.request).method)
-      "
+      :class="getRequestLabelColor"
       :data-label="t('shortcodes.method')"
     >
-      {{ parseShortcodeRequest(shortCode.request).method }}
+      {{ parseShortcodeRequest.method }}
     </div>
     <div class="table-col" :data-label="t('shortcodes.url')">
-      {{ parseShortcodeRequest(shortCode.request).endpoint }}
+      {{ parseShortcodeRequest.endpoint }}
     </div>
     <div
       ref="timeStampRef"
       class="table-col"
       :data-label="t('shortcodes.created_on')"
     >
-      {{
-        timestampHovered
-          ? new Date(shortCode.createdOn).toLocaleTimeString()
-          : new Date(shortCode.createdOn).toLocaleDateString()
-      }}
+      {{ timestampHovered ? timeStamp : dateStamp }}
     </div>
     <div
       class="flex items-center justify-center border divide-x lg:table-cell border-dividerLight divide-dividerLight"
@@ -33,7 +27,7 @@
       <SmartAnchor
         v-tippy="{ theme: 'tooltip' }"
         :title="t('action.open_workspace')"
-        :to="`https://hopp.sh/r/${shortCode.id}`"
+        :to="`https://hopp.sh/r/${shortcode.id}`"
         blank
         svg="external-link"
         color="blue"
@@ -44,30 +38,34 @@
         :title="t('action.copy')"
         color="green"
         :svg="copyIconRefs"
-        @click.native="copyShortCode(shortCode.id)"
+        @click.native="copyShortCode(shortcode.id)"
       />
       <ButtonSecondary
         v-tippy="{ theme: 'tooltip' }"
         :title="t('action.delete')"
         svg="trash"
         color="red"
-        @click.native="deleteShortCode(shortCode.id)"
+        @click.native="deleteShortcode(shortcode.id)"
       />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "@nuxtjs/composition-api"
+import { ref, computed } from "@nuxtjs/composition-api"
 import { useElementHover } from "@vueuse/core"
+import { flow, pipe } from "fp-ts/function"
+import * as R from "fp-ts/Record"
+import * as O from "fp-ts/Option"
+import { translateToNewRequest } from "@hoppscotch/data"
 import { useI18n, useToast } from "~/helpers/utils/composables"
 import { copyToClipboard } from "~/helpers/utils/clipboard"
 
 const t = useI18n()
 const toast = useToast()
 
-defineProps<{
-  shortCode: {
+const props = defineProps<{
+  shortcode: {
     id: string
     request: string
     createdOn: string
@@ -78,13 +76,9 @@ const emit = defineEmits<{
   (e: "delete-short-code", codeID: string): void
 }>()
 
-const deleteShortCode = (codeID: string) => {
+const deleteShortcode = (codeID: string) => {
   emit("delete-short-code", codeID)
 }
-
-const timeStampRef = ref()
-const copyIconRefs = ref("copy")
-const timestampHovered = useElementHover(timeStampRef)
 
 const requestMethodLabels = {
   get: "text-green-500",
@@ -94,19 +88,35 @@ const requestMethodLabels = {
   default: "text-gray-500",
 }
 
+const timeStampRef = ref()
+const copyIconRefs = ref("copy")
+const timestampHovered = useElementHover(timeStampRef)
+
+const parseShortcodeRequest = computed(() =>
+  flow(JSON.parse, translateToNewRequest)(props.shortcode.request)
+)
+
+const getRequestLabelColor = computed(() =>
+  pipe(
+    requestMethodLabels,
+    R.lookup(parseShortcodeRequest.value.method.toLowerCase()),
+    O.getOrElse(() => requestMethodLabels.default)
+  )
+)
+
+const dateStamp = computed(() =>
+  new Date(props.shortcode.createdOn).toLocaleDateString()
+)
+const timeStamp = computed(() =>
+  new Date(props.shortcode.createdOn).toLocaleTimeString()
+)
+
 const copyShortCode = (codeID: string) => {
   copyToClipboard(`https://hopp.sh/r/${codeID}`)
   toast.success(`${t("state.copied_to_clipboard")}`)
   copyIconRefs.value = "check"
   setTimeout(() => (copyIconRefs.value = "copy"), 1000)
 }
-
-const parseShortcodeRequest = (request: string) => JSON.parse(request)
-
-const getRequestLabelColor = (method: string) =>
-  requestMethodLabels[
-    method.toLowerCase() as keyof typeof requestMethodLabels
-  ] || requestMethodLabels.default
 </script>
 
 <style lang="scss">

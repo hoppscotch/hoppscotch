78b8d9692f412381d3f232c3f27f14573c0db265
amk-dev <akash.k.mohan98@gmail.com>
2022-05-16 21:35:36 +0530

    fix: add type HoppExtensionStatusHook

--- packages/hoppscotch-app/types/pw-ext-hook.d.ts
+++ packages/hoppscotch-app/types/pw-ext-hook.d.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from "axios"
 import { NetworkResponse } from "~/helpers/network"
+import { ExtensionStatus } from "~/newstore/HoppExtension"
 
 export interface PWExtensionHook {
   getVersion: () => { major: number; minor: number }
@@ -7,4 +8,12 @@ export interface PWExtensionHook {
     req: AxiosRequestConfig & { wantsBinary: boolean }
   ) => Promise<NetworkResponse>
   cancelRunningRequest: () => void
+}
+
+export type HoppExtensionStatusHook = {
+  status: ExtensionStatus
+  _subscribers: {
+    status?: ((...args: any[]) => any)[] | undefined
+  }
+  subscribe(prop: "status", func: (...args: any[]) => any): void
 }
--- packages/hoppscotch-app/types/window.d.ts
+++ packages/hoppscotch-app/types/window.d.ts
@@ -1,13 +1,8 @@
-import { PWExtensionHook } from "./pw-ext-hook"
-import { ExtensionStatus } from "~/newstore/HoppExtension"
+import { HoppExtensionStatusHook, PWExtensionHook } from "./pw-ext-hook"
 
 declare global {
   interface Window {
     __POSTWOMAN_EXTENSION_HOOK__: PWExtensionHook | undefined
-    __HOPP_EXTENSION_STATUS_PROXY__:
-      | {
-          status: ExtensionStatus
-        }
-      | undefined
+    __HOPP_EXTENSION_STATUS_PROXY__: HoppExtensionStatusHook | undefined
   }
 }

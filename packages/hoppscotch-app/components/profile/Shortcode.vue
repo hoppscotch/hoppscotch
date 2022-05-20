fb15cbc6fddf9d01a7f8b359cf31b844df136a9b
Nivedin <nivedinp@gmail.com>
2022-05-20 15:25:49 +0000

    fix: used pipe instead of flow for pcopy icon type, change methodLabel to readonly const

--- packages/hoppscotch-app/components/profile/Shortcode.vue
+++ packages/hoppscotch-app/components/profile/Shortcode.vue
@@ -5,7 +5,7 @@
     </div>
     <div
       class="table-col lg:w-30"
-      :class="getRequestLabelColor"
+      :class="requestLabelColor"
       :data-label="t('shortcodes.method')"
     >
       {{ parseShortcodeRequest.method }}
@@ -40,7 +40,7 @@
         :title="t('action.copy')"
         color="green"
         :svg="copyIconRefs"
-        @click.native="copyShortCode(shortcode.id)"
+        @click.native="copyShortcode(shortcode.id)"
       />
       <ButtonSecondary
         v-tippy="{ theme: 'tooltip' }"
@@ -56,8 +56,8 @@
 <script setup lang="ts">
 import { ref, computed } from "@nuxtjs/composition-api"
 import { useElementHover } from "@vueuse/core"
-import { flow, pipe } from "fp-ts/function"
-import * as R from "fp-ts/Record"
+import { pipe } from "fp-ts/function"
+import * as RR from "fp-ts/ReadonlyRecord"
 import * as O from "fp-ts/Option"
 import { translateToNewRequest } from "@hoppscotch/data"
 import { useI18n, useToast } from "~/helpers/utils/composables"
@@ -85,21 +85,21 @@ const requestMethodLabels = {
   put: "text-blue-500",
   delete: "text-red-500",
   default: "text-gray-500",
-}
+} as const
 
 const timeStampRef = ref()
-const copyIconRefs = ref("copy")
+const copyIconRefs = ref<"copy" | "check">("copy")
 const timestampHovered = useElementHover(timeStampRef)
 
 const parseShortcodeRequest = computed(() =>
-  flow(JSON.parse, translateToNewRequest)(props.shortcode.request)
+  pipe(props.shortcode.request, JSON.parse, translateToNewRequest)
 )
 
-const getRequestLabelColor = computed(() =>
+const requestLabelColor = computed(() =>
   pipe(
     requestMethodLabels,
-    R.lookup(parseShortcodeRequest.value.method.toLowerCase()),
-    O.getOrElse(() => requestMethodLabels.default)
+    RR.lookup(parseShortcodeRequest.value.method.toLowerCase()),
+    O.getOrElseW(() => requestMethodLabels.default)
   )
 )
 
@@ -110,7 +110,7 @@ const timeStamp = computed(() =>
   new Date(props.shortcode.createdOn).toLocaleTimeString()
 )
 
-const copyShortCode = (codeID: string) => {
+const copyShortcode = (codeID: string) => {
   copyToClipboard(`https://hopp.sh/r/${codeID}`)
   toast.success(`${t("state.copied_to_clipboard")}`)
   copyIconRefs.value = "check"

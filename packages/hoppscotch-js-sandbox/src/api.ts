12fc3b1f10d593237a572fa5a5f3423b3bd66fb5
devblin <dhruwxl@gmail.com>
2022-07-07 19:11:10 +0530

    feat: added new api defining functions

---
+++ packages/hoppscotch-js-sandbox/src/api.ts
@@ -0,0 +1,127 @@
+import { identity } from "fp-ts/function"
+import { QuickJSHandle, QuickJSContext } from "quickjs-emscripten"
+import { PreRequestScriptReport } from "./preRequest"
+import { TestScriptReport } from "./test-runner"
+
+export type PreRequestScriptNamespaces = "artifact" | "env"
+
+export type TestScriptNamespaces = "env" | "test" | "expect" | "response"
+
+export type APINamespaces = PreRequestScriptNamespaces | TestScriptNamespaces
+
+/**
+ * Defines what should be returned on a `defineAPI` block
+ */
+type APIMeta<Exposed> = {
+  rootHandle: QuickJSHandle
+  exposes: Exposed
+}
+
+/**
+ * Just a collection of information defining an API
+ */
+export type APIDefinition<ID extends APINamespaces, Exposed> = {
+  id: ID
+  creationFunc: (vm: QuickJSContext) => APIMeta<Exposed>
+}
+
+/**
+ * Defines the implementation of an API
+ * @param id The unique ID of the API
+ * @param func The implementation function
+ * @returns The API definition object
+ */
+export const defineAPI = <ID extends APINamespaces, Exposed>(
+  id: ID,
+  func: APIDefinition<ID, Exposed>["creationFunc"]
+): APIDefinition<ID, Exposed> => ({ id, creationFunc: func })
+
+/* These functions rely or setup the api instance initalization handling */
+
+/**
+ * An intermediate representation of an API that is being initialized
+ */
+type APIInit<Exposed> = {
+  creationFunc: (vm: QuickJSContext) => APIMeta<Exposed>
+  onPreRequestScriptComplete: (
+    report: PreRequestScriptReport
+  ) => PreRequestScriptReport
+  onTestScriptComplete: (report: TestScriptReport) => TestScriptReport
+}
+
+/**
+ * Final representation of an API which is currently active and installed
+ */
+export type APIInstance<
+  ID extends APINamespaces,
+  Exposed
+> = APIInit<Exposed> & { id: ID; rootHandle: QuickJSHandle; exposes: Exposed }
+
+/**
+ * A temporary variable to store the currently initializing API
+ */
+let currentInstance: APIInit<unknown> | null = null
+
+/**
+ * Initalizes a single API instance
+ * @param vm A QJS context
+ * @returns
+ */
+export const initializeAPI =
+  (vm: QuickJSContext) =>
+  <ID extends APINamespaces, Exposed>(
+    api: APIDefinition<ID, Exposed>
+  ): APIInstance<ID, Exposed> => {
+    // Initial State
+    currentInstance = {
+      creationFunc: api.creationFunc,
+      onPreRequestScriptComplete: identity,
+      onTestScriptComplete: identity,
+    }
+
+    const returnValue = api.creationFunc(vm)
+
+    // Final State
+    const result: APIInstance<ID, Exposed> = {
+      ...(currentInstance as APIInit<Exposed>), // Guaranteed because of the function
+      id: api.id,
+      exposes: returnValue.exposes,
+      rootHandle: returnValue.rootHandle,
+    }
+
+    currentInstance = null
+
+    return result
+  }
+
+/**
+ * Defines the function to be called when the pre-request script is complete
+ * @param func The function which takes in the report and spits a report with the updates relevant to the API
+ */
+export const onPreRequestScriptComplete = (
+  func: (report: PreRequestScriptReport) => PreRequestScriptReport
+) => {
+  if (!currentInstance) {
+    throw new Error(
+      "Cannot use 'onPreRequestScriptComplete' outside a defineAPI block"
+    )
+  }
+
+  currentInstance.onPreRequestScriptComplete = func
+}
+
+/**
+ * Defines the function to be called when the test script is complete
+ * @param func The function which takes in the report and spits a report with the updates relevant to the API
+ */
+export const onTestScriptComplete = (
+  func: (report: TestScriptReport) => TestScriptReport
+) => {
+  if (!currentInstance) {
+    throw new Error(
+      "Cannot use 'onTestScriptComplete' outside a defineAPI block"
+    )
+  }
+
+  currentInstance.onTestScriptComplete = func
+}
---
+++ packages/hoppscotch-js-sandbox/src/apiManager.ts
@@ -0,0 +1,260 @@
+import { QuickJSContext, QuickJSHandle } from "quickjs-emscripten"
+import { ADT, match } from "ts-adt"
+import ArtifactAPI from "./apis/artifact"
+import EnvAPI from "./apis/env"
+import TestAPI from "./apis/test"
+import { flow, identity, pipe } from "fp-ts/function"
+import * as O from "fp-ts/Option"
+import * as A from "fp-ts/Array"
+import * as Tu from "fp-ts/Tuple"
+import { APIDefinition, APIInstance, APINamespaces, initializeAPI } from "./api"
+import {
+  getObjectKeysFromHandle,
+  throwErr,
+  unsafeCast,
+  unsafeEffect,
+} from "./utils"
+import { TestScriptReport } from "./test-runner"
+import { PreRequestScriptReport } from "./preRequest"
+
+type APIDefPair<U extends APIDefinition<APINamespaces, unknown>> = {
+  [key in U["id"]]: U
+}
+
+type APIRegistryPair<T extends (...args: any) => any> = APIDefPair<
+  ReturnType<T>
+>
+
+// PLACE ALL VALID APIS HERE
+type APIRegistry = APIRegistryPair<typeof EnvAPI> &
+  APIRegistryPair<typeof TestAPI> &
+  APIRegistryPair<typeof ArtifactAPI>
+
+type APIRegistryID = APIRegistry[keyof APIRegistry] extends APIDefinition<
+  infer U,
+  any
+>
+  ? U
+  : never
+
+type APIRegistryExposedWithID<ID extends APIRegistryID> =
+  APIRegistry[ID] extends APIDefinition<ID, infer U> ? U : never
+
+/**
+ * Temporary variable to store the currently completed instance of APIs during a init call
+ */
+let completedAPIs: Array<APIInstance<APINamespaces, unknown>> = []
+
+/**
+ * Access an already initialized API's exposed functions
+ * @param apiID The ID of the API you want to expose
+ * @returns The exposed info for the requested API
+ */
+export const useAPIExposed = <ID extends APIRegistryID>(
+  apiID: ID
+): APIRegistryExposedWithID<ID> =>
+  pipe(
+    completedAPIs,
+    A.findFirst((def) => def.id === apiID),
+    O.map((x) => x.exposes),
+    O.getOrElseW(() =>
+      throwErr(
+        `Could not find API with ID ${apiID}. Make sure the API is above in the initialization order`
+      )
+    ),
+    (d) => unsafeCast<APIRegistryExposedWithID<ID>>(d) // We can't tell TypeScript directly how this will be true easily, hence a shortcut
+  )
+
+/**
+ * Defines how the API will be mixed into the root handle
+ */
+export type APIDirLocation = ADT<{
+  // This API should be mixed with the root object
+  // For example, testing api should inject to pw.expect and pw.test (where root object is pw)
+  mixWithRoot: Record<string, unknown>
+
+  // This API should be put into its own namespace
+  // For example, Artifact API injects its methods to hopp.artifact namespace (where root object is hopp)
+  namespaced: { namespace: string }
+}>
+
+/**
+ * Constructor for MixWithRoot API Mix Location
+ */
+export const MixWithRoot = <APIDirLocation>{ _type: "mixWithRoot" }
+
+/**
+ * Constructor for Namespaced API Mix Location
+ * @param namespace Namespace of the API
+ */
+export const Namespaced = <T extends APINamespaces>(namespace: T) =>
+  <APIDirLocation>{ _type: "namespaced", namespace }
+
+/**
+ * Defines the structure of one API Directory Entry
+ */
+type APIDirEntry<ID extends APINamespaces, Exposed> = [
+  APIDefinition<ID, Exposed>,
+  APIDirLocation
+]
+
+/**
+ * Constructor for a single API directory entry.
+ * Used for allowing for Type Inference
+ */
+export const api: <ID extends APINamespaces, Exposed>(
+  x: APIDirEntry<ID, Exposed>
+) => APIDirEntry<ID, Exposed> = identity
+
+/**
+ * Initalizes an API directory
+ * @param vm A QJS Context
+ * @returns Array of Tuples of the Instantiated APIs and the mix location
+ */
+const initAPIs =
+  (vm: QuickJSContext) => (apis: Array<APIDirEntry<APINamespaces, unknown>>) =>
+    pipe(
+      apis,
+      A.map(
+        flow(
+          Tu.mapFst(initializeAPI(vm)),
+          // Add the element to the completedAPIs list so we can get the list in `useExposedAPIs`
+          unsafeEffect(([instance]) => completedAPIs.push(instance))
+        )
+      ),
+      // Once all initializations are done, clear the completedAPIs as nothing more is going to be inited
+      unsafeEffect(() => {
+        completedAPIs = []
+      })
+    )
+
+/**
+ * Defines how an API is to be injected to the root handle directly
+ * @param vm A QJS Context
+ * @param rootHandle Root Handle to Inject to
+ * @param api The instantiated API to inject
+ */
+const injectToRoot = (
+  vm: QuickJSContext,
+  rootHandle: QuickJSHandle,
+  api: APIInstance<APINamespaces, unknown>
+) => {
+  pipe(
+    api.rootHandle,
+    getObjectKeysFromHandle(vm),
+    A.map((key) => vm.setProp(rootHandle, key, vm.getProp(api.rootHandle, key)))
+  )
+}
+
+/**
+ * Defines how an API should be injected to a namespace
+ * @param vm A QJS Context
+ * @param rootHandle The root handle to inject namespace to
+ * @param api The instantiated API to hook to
+ * @param namespace Namespace to place under
+ */
+const injectToNamespace = (
+  vm: QuickJSContext,
+  rootHandle: QuickJSHandle,
+  api: APIInstance<APINamespaces, unknown>,
+  namespace: string
+) => {
+  vm.setProp(rootHandle, namespace, api.rootHandle)
+}
+
+/**
+ * Decides how to inject an API from its location specified in the directory
+ * @param vm A QJS Context
+ * @param rootHandle The root handle to inject to
+ */
+const injectAPI =
+  (vm: QuickJSContext, rootHandle: QuickJSHandle) =>
+  ([initedAPI, location]: [
+    APIInstance<APINamespaces, unknown>,
+    APIDirLocation
+  ]) =>
+    pipe(
+      location,
+      match({
+        mixWithRoot: () => injectToRoot(vm, rootHandle, initedAPI),
+        namespaced: ({ namespace }) =>
+          injectToNamespace(vm, rootHandle, initedAPI, namespace),
+      })
+    )
+
+export const installAPIs = (
+  vm: QuickJSContext,
+  rootHandle: QuickJSHandle,
+  apis: APIDirEntry<APINamespaces, unknown>[]
+) =>
+  pipe(
+    apis,
+    initAPIs(vm),
+
+    // We want to inject the APIs (effect) but do not want their return value
+    unsafeEffect(A.map(injectAPI(vm, rootHandle))),
+
+    A.map(Tu.fst)
+  )
+
+/**
+ * An ADT describing how an API completes
+ */
+type Completer = ADT<{
+  preRequestScript: { report: PreRequestScriptReport }
+  testScript: { report: TestScriptReport }
+}>
+
+/**
+ * Constructor for a Completer for a Pre Request Script
+ * @param report The Pre Request Report
+ */
+export const PreRequestCompleter = (report: PreRequestScriptReport) =>
+  ({ _type: "preRequestScript", report } as const)
+
+/**
+ * Constructor for a Completer for a Test Script
+ * @param report The Test Script Report
+ */
+export const TestScriptCompleter = (report: TestScriptReport) =>
+  ({ _type: "testScript", report } as const)
+
+/**
+ * Completes a single API
+ * @param completer The Completer data to apply
+ * @param instance The instance to complete
+ */
+const completeAPI = (
+  completer: Completer,
+  instance: APIInstance<APINamespaces, unknown>
+) =>
+  pipe(
+    completer,
+    match({
+      preRequestScript: ({ report }) =>
+        PreRequestCompleter(instance.onPreRequestScriptComplete(report)),
+      testScript: ({ report }) =>
+        TestScriptCompleter(instance.onTestScriptComplete(report)),
+    }),
+    // Close the handle of the API instance
+    unsafeEffect(() => {
+      instance.rootHandle.dispose()
+    })
+  )
+
+/**
+ * Completes all the APIs specified sequentially
+ * @param instances All the instances to be completed
+ * @param completer The completer to apply initially
+ * @returns The completed report
+ */
+export const completeAPIs = <T extends Completer>(
+  instances: APIInstance<APINamespaces, unknown>[],
+  completer: T
+): T["report"] =>
+  pipe(
+    instances,
+    A.reduce(completer, completeAPI),
+    (x) => x.report
+    // unsafeCast<T> // Just to keep the public API more clean
+  )

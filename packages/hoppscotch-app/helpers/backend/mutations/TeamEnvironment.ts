ae005b83f89ffed493194220645b717ca4f30682
Nivedin <nivedinp@gmail.com>
2022-07-14 16:07:48 +0000

    feat: added team-environment graphQl mutations

---
+++ packages/hoppscotch-app/helpers/backend/mutations/TeamEnvironment.ts
@@ -0,0 +1,83 @@
+import { runMutation } from "../GQLClient"
+import {
+  CreateDuplicateEnvironmentDocument,
+  CreateDuplicateEnvironmentMutation,
+  CreateDuplicateEnvironmentMutationVariables,
+  CreateTeamEnvironmentDocument,
+  CreateTeamEnvironmentMutation,
+  CreateTeamEnvironmentMutationVariables,
+  DeleteAllVariablesFromTeamEnvironmentDocument,
+  DeleteAllVariablesFromTeamEnvironmentMutation,
+  DeleteAllVariablesFromTeamEnvironmentMutationVariables,
+  DeleteTeamEnvironmentDocument,
+  DeleteTeamEnvironmentMutation,
+  DeleteTeamEnvironmentMutationVariables,
+  UpdateTeamEnvironmentDocument,
+  UpdateTeamEnvironmentMutation,
+  UpdateTeamEnvironmentMutationVariables,
+} from "../graphql"
+
+type DeleteTeamEnvironmentError = "team_environment/not_found"
+
+type UpdateTeamEnvironmentError = "team_environment/not_found"
+
+type DeleteAllTeamEnvironmentError = "team_environment/not_found"
+
+type DuplicateTeamEnvironmentError = "team_environment/not_found"
+
+export const createTeamEnvironment = (
+  variables: string,
+  teamID: string,
+  name: string
+) =>
+  runMutation<
+    CreateTeamEnvironmentMutation,
+    CreateTeamEnvironmentMutationVariables,
+    ""
+  >(CreateTeamEnvironmentDocument, {
+    variables,
+    teamID,
+    name,
+  })
+
+export const deleteTeamEnvironment = (id: string) =>
+  runMutation<
+    DeleteTeamEnvironmentMutation,
+    DeleteTeamEnvironmentMutationVariables,
+    DeleteTeamEnvironmentError
+  >(DeleteTeamEnvironmentDocument, {
+    id,
+  })
+
+export const updateTeamEnvironment = (
+  variables: string,
+  id: string,
+  name: string
+) =>
+  runMutation<
+    UpdateTeamEnvironmentMutation,
+    UpdateTeamEnvironmentMutationVariables,
+    UpdateTeamEnvironmentError
+  >(UpdateTeamEnvironmentDocument, {
+    variables,
+    id,
+    name,
+  })
+
+export const deleteAllVariablesFromTeamEnvironment = (id: string) =>
+  runMutation<
+    DeleteAllVariablesFromTeamEnvironmentMutation,
+    DeleteAllVariablesFromTeamEnvironmentMutationVariables,
+    DeleteAllTeamEnvironmentError
+  >(DeleteAllVariablesFromTeamEnvironmentDocument, {
+    id,
+  })
+
+export const createDuplicateEnvironment = (id: string) =>
+  runMutation<
+    CreateDuplicateEnvironmentMutation,
+    CreateDuplicateEnvironmentMutationVariables,
+    DuplicateTeamEnvironmentError
+  >(CreateDuplicateEnvironmentDocument, {
+    id,
+  })

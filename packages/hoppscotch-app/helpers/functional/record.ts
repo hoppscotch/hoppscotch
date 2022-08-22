f280a45a4a44bdeee5735c3390060bb8bcd69b96
Nivedin <nivedinp@gmail.com>
2022-08-22 15:34:03 +0530

    fix: form data with same key send only last one

--- packages/hoppscotch-app/helpers/functional/record.ts
+++ packages/hoppscotch-app/helpers/functional/record.ts
@@ -7,3 +7,20 @@ export const tupleToRecord = <
   tuples.length > 0
     ? (Object.assign as any)(...tuples.map(([key, val]) => ({ [key]: val })))
     : {}
+
+export const tupleToRecordWithSameKeys = <
+  KeyType extends string | number | symbol,
+  ValueType
+>(
+  tuples: [KeyType, ValueType][]
+): Record<KeyType, ValueType[]> => {
+  const out = {} as Record<KeyType, ValueType[]>
+  for (const [key, value] of tuples) {
+    if (!out[key]) {
+      out[key] = [value]
+    } else {
+      out[key].push(value)
+    }
+  }
+  return out
+}

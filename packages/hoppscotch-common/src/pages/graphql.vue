ced5c5f026adf7b728e4b6357c8fc08675a68651
Anwarul Islam <anwaarulislaam@gmail.com>
2022-08-29 04:51:24 +0600

    feat: add tab to graphql page

--- packages/hoppscotch-common/src/pages/graphql.vue
+++ packages/hoppscotch-common/src/pages/graphql.vue
@@ -1,11 +1,26 @@
 <template>
   <AppPaneLayout layout-id="graphql">
     <template #primary>
-      <GraphqlRequest :conn="gqlConn" />
-      <GraphqlRequestOptions :conn="gqlConn" />
-    </template>
-    <template #secondary>
-      <GraphqlResponse :conn="gqlConn" />
+      <SmartWindows
+        :id="'communication_tab'"
+        v-model="currentTabId"
+        @removeTab="removeTab"
+        @addTab="addNewTab"
+        @sort="sortTabs"
+      >
+        <template v-for="tab in tabs" :key="'removable_tab_' + tab.id">
+          <SmartWindow
+            :id="tab.id"
+            :label="tab.name"
+            :is-removable="tabs.length > 1"
+            class="flex flex-col flex-1 overflow-y-auto"
+          >
+            <GraphqlRequest :conn="gqlConn" />
+            <GraphqlRequestOptions :conn="gqlConn" />
+            <GraphqlResponse :conn="gqlConn" />
+          </SmartWindow>
+        </template>
+      </SmartWindows>
     </template>
     <template #sidebar>
       <GraphqlSidebar :conn="gqlConn" />
@@ -14,6 +29,9 @@
 </template>
 
 <script setup lang="ts">
+import { computed, onBeforeUnmount, watch, ref } from "vue"
+import { useReadonlyStream } from "@composables/stream"
+import { useI18n } from "@composables/i18n"
 import { usePageHead } from "@composables/head"
 import { useI18n } from "@composables/i18n"
 import { GQLConnection } from "@helpers/GQLConnection"
@@ -29,6 +47,35 @@ usePageHead({
 })
 
 const gqlConn = new GQLConnection()
+
+const currentTabId = ref("new")
+const defaultTab = {
+  id: "new",
+  name: "Untitled",
+}
+const tabs = ref([defaultTab])
+
+const changeTab = (id: string) => {
+  currentTabId.value = id
+}
+const addNewTab = () => {
+  tabs.value.push({
+    id: uniqueId("new_"),
+    name: "Untitled",
+  })
+  changeTab(tabs.value[tabs.value.length - 1].id)
+}
+const sortTabs = (e: { oldIndex: number; newIndex: number }) => {
+  const newTabs = [...tabs.value]
+  newTabs.splice(e.newIndex, 0, newTabs.splice(e.oldIndex, 1)[0])
+  tabs.value = newTabs
+}
+const removeTab = (tabID: string) => {
+  const index = tabs.value.findIndex((tab) => tab.id === tabID)
+  tabs.value.splice(index, 1)
+}
+
+const isLoading = useReadonlyStream(gqlConn.isLoading$, false)
 
 onBeforeUnmount(() => {
   if (gqlConn.connected$.value) {

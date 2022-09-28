6a2796fbcf829e1951ba3295c27e1ddbb31c4f19
Andrew Bastin <andrewbastin.k@gmail.com>
2022-09-28 17:06:40 +0530

    chore: cleanup

--- packages/hoppscotch-app/vite.config.ts
+++ packages/hoppscotch-app/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from "vite"
-import { META_TAGS } from "./meta"
+import { APP_INFO, META_TAGS } from "./meta"
 import generateSitemap from "vite-plugin-pages-sitemap"
 import HtmlConfig from "vite-plugin-html-config"
 import Vue from "@vitejs/plugin-vue"
@@ -18,26 +18,6 @@ import * as path from "path"
 import { VitePluginFonts } from "vite-plugin-fonts"
 
 const ENV = loadEnv("development", process.cwd())
-
-export const APP_INFO = {
-  name: "Hoppscotch",
-  shortDescription: "Open source API development ecosystem",
-  description:
-    "Helps you create requests faster, saving precious time on development.",
-  keywords:
-    "hoppscotch, hopp scotch, hoppscotch online, hoppscotch app, postwoman, postwoman chrome, postwoman online, postwoman for mac, postwoman app, postwoman for windows, postwoman google chrome, postwoman chrome app, get postwoman, postwoman web, postwoman android, postwoman app for chrome, postwoman mobile app, postwoman web app, api, request, testing, tool, rest, websocket, sse, graphql, socketio",
-  loading: {
-    color: "var(--divider-dark-color)",
-    background: "var(--primary-color)",
-    accent: "var(--accent-color)",
-  },
-  app: {
-    background: "#202124",
-  },
-  social: {
-    twitter: "@hoppscotch_io",
-  },
-} as const
 
 export default defineConfig({
   // TODO: Migrate @hoppscotch/data to full ESM

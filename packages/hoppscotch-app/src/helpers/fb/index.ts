1f3119b3e25622bbce473aea9dfda4885847a993
amk-dev <akash.k.mohan98@gmail.com>
2022-10-26 18:27:31 +0530

    fix: fix typo in env variable name

--- packages/hoppscotch-app/src/helpers/fb/index.ts
+++ packages/hoppscotch-app/src/helpers/fb/index.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_TITLE,
+  appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 }
 

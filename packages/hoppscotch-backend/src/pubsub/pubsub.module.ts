4da11955f1387dfa0113c4a6f7b4904b31fd22c5
ankitsridhar16 <ankit.sridhar16@gmail.com>
2022-12-07 23:17:56 +0530

    style: prettier fix for pubsub

--- packages/hoppscotch-backend/src/pubsub/pubsub.module.ts
+++ packages/hoppscotch-backend/src/pubsub/pubsub.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
-import { PubSubService } from "./pubsub.service";
+import { Module } from '@nestjs/common';
+import { PubSubService } from './pubsub.service';
 
 @Module({
   providers: [PubSubService],
-  exports: [PubSubService]
+  exports: [PubSubService],
 })
 export class PubSubModule {}

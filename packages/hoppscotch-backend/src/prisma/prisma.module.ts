06ef17048ae5064202f5a5129363048b061ae086
ankitsridhar16 <ankit.sridhar16@gmail.com>
2022-12-07 20:52:31 +0530

    chore: added prisma module

---
+++ packages/hoppscotch-backend/src/prisma/prisma.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common/decorators';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}

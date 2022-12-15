6bd4fd91ffdcb8542a16bad122dfef2fcce07779
ankitsridhar16 <ankit.sridhar16@gmail.com>
2022-12-15 23:25:21 +0530

    chore: updated user resolver with updated service methods

--- packages/hoppscotch-backend/src/user-environment/user.resolver.ts
+++ packages/hoppscotch-backend/src/user-environment/user.resolver.ts
@@ -2,6 +2,8 @@ import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { User } from 'src/user/user.model';
 import { UserEnvironment } from './user-environments.model';
 import { UserEnvironmentsService } from './user-environments.service';
+import * as E from 'fp-ts/Either';
+import { throwErr } from '../utils';
 
 @Resolver(() => User)
 export class UserEnvsUserResolver {
@@ -20,8 +22,9 @@ export class UserEnvsUserResolver {
   async globalEnvironments(
     @Parent() user: User,
   ): Promise<UserEnvironment | string> {
-    return await this.userEnvironmentsService.fetchUserGlobalEnvironments(
-      user.uid,
-    );
+    const userEnvironment =
+      await this.userEnvironmentsService.fetchUserGlobalEnvironment(user.uid);
+    if (E.isLeft(userEnvironment)) throwErr(userEnvironment.left);
+    return userEnvironment.right;
   }
 }

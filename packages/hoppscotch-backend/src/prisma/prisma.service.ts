3392b1a1caab6701282d17650470db3d445a903e
ankitsridhar16 <ankit.sridhar16@gmail.com>
2022-12-15 23:21:57 +0530

    chore: minor changes to Dockerfilefor prisma service

--- packages/hoppscotch-backend/src/prisma/prisma.service.ts
+++ packages/hoppscotch-backend/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from 'prisma/prisma-client/scripts/default-index';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService

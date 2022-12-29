acafc072db28db247321d9c624734d3620d2c145
Liyas Thomas <liyascthomas@gmail.com>
2022-12-29 11:10:16 +0530

    chore: minor ui improvements

--- packages/hoppscotch-common/src/index.ts
+++ packages/hoppscotch-common/src/index.ts
@@ -46,10 +46,10 @@ export function createHoppApp(el: string | Element, platformDef: PlatformDef) {
 
   console.info(
     "%cWE ♥️ OPEN SOURCE",
-    "margin:8px 0;font-family:Inter;font-weight:600;font-size:60px;color:violet;"
+    "margin:8px 0;font-family:sans-serif;font-weight:600;font-size:60px;color:violet;"
   )
   console.info(
     "%cContribute: https://github.com/hoppscotch/hoppscotch",
-    "margin:8px 0;font-family:Inter;font-weight:500;font-size:24px;color:violet;"
+    "margin:8px 0;font-family:sans-serif;font-weight:500;font-size:24px;color:violet;"
   )
 }

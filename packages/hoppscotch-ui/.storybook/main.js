d1b54ea722f430aed661744c77353e27dd595b87
Anwarul Islam <anwaarulislaam@gmail.com>
2023-01-16 18:03:46 +0600

    feat: icons resolved

--- packages/hoppscotch-ui/.storybook/main.js
+++ packages/hoppscotch-ui/.storybook/main.js
@@ -1,5 +1,9 @@
 const path = require("path")
 const WindiCSS = require("vite-plugin-windicss").default
+const IconResolver = require("unplugin-icons/resolver")
+const Icons = require("unplugin-icons/vite")
+const { FileSystemIconLoader } = require("unplugin-icons/loaders")
+const Components = require("unplugin-vue-components/vite")
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -20,6 +24,27 @@ module.exports = {
     config.plugins.push(
       WindiCSS({
         config: path.join(__dirname, "..", "windi.config.ts"),
+      }),
+      Components({
+        dts: "./src/components.d.ts",
+        dirs: ["./src/components"],
+        directoryAsNamespace: true,
+        resolvers: [
+          IconResolver({
+            prefix: "icon",
+            customCollections: ["hopp", "auth", "brands"],
+          }),
+        ],
+      }),
+      Icons({
+        compiler: "vue3",
+        customCollections: {
+          hopp: FileSystemIconLoader("../hoppscotch-common/assets/icons"),
+          auth: FileSystemIconLoader("../hoppscotch-common/assets/icons/auth"),
+          brands: FileSystemIconLoader(
+            "../hoppscotch-common/assets/icons/brands"
+          ),
+        },
       })
     )
     config.resolve.alias = {

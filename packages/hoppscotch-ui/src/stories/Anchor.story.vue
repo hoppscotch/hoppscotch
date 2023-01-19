8958287bc239ef1d2c24c5d18e6e7fc90fce0bae
Anwarul Islam <anwaarulislaam@gmail.com>
2023-01-19 21:27:38 +0600

    feat: new components added to stories

---
+++ packages/hoppscotch-ui/src/stories/Anchor.story.vue
@@ -0,0 +1,20 @@
+<template>
+  <Story title="Anchor">
+    <div class="text-secondaryLight text-tiny">
+      By signing in, you are agreeing to our
+      <SmartAnchor
+        class="link text-red-800"
+        to="https://docs.hoppscotch.io/terms"
+        blank
+        label="Terms of Service"
+      />
+      and
+      <SmartAnchor
+        class="link text-red-600"
+        to="https://docs.hoppscotch.io/privacy"
+        blank
+        label="Privacy Policy"
+      />
+    </div>
+  </Story>
+</template>

74e4a77ce663473d0c032673d4b1c5a593373976
ankitsridhar16 <ankit.sridhar16@gmail.com>
2023-01-23 15:04:05 +0530

    chore: added topics for user history subscriptions

--- packages/hoppscotch-backend/src/pubsub/topicsDefs.ts
+++ packages/hoppscotch-backend/src/pubsub/topicsDefs.ts
@@ -1,4 +1,5 @@
 import { UserEnvironment } from '../user-environment/user-environments.model';
+import { UserHistory } from '../user-history/user-history.model';
 
 // A custom message type that defines the topic and the corresponding payload.
 // For every module that publishes a subscription add its type def and the possible subscription type.
@@ -7,4 +8,8 @@ export type TopicDef = {
     topic: `user_environment/${string}/${'created' | 'updated' | 'deleted'}`
   ]: UserEnvironment;
   [topic: `user_environment/${string}/deleted_many`]: number;
+  [
+    topic: `user_history/${string}/${'created' | 'updated' | 'deleted'}`
+  ]: UserHistory;
+  [topic: `user_history/${string}/deleted_many`]: number;
 };

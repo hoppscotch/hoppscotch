a90bc865865d1a9ec46a599349e276c140bf429a
Andrew Bastin <andrewbastin.k@gmail.com>
2023-01-27 03:22:50 -0500

    chore: remove importing of all components in hopp-ui to allow code splitting

--- packages/hoppscotch-ui/src/index.ts
+++ packages/hoppscotch-ui/src/index.ts
@@ -1,12 +1,4 @@
 import { Plugin } from "vue"
-import * as components from "./components/"
-
-const HoppUI = { ...components }
-
-// Hopp UI will be available globally when in a browser environment
-if (typeof window !== "undefined") {
-  ;(window as any)["HoppUI"] = HoppUI
-}
 
 import "./assets/scss/styles.scss"
 import "virtual:windi.css"
@@ -19,14 +11,8 @@ export type HoppUIPluginOptions = {
 
 const plugin: Plugin = {
   install(app, options: HoppUIPluginOptions = {}) {
-    for (const key in components) {
-      // @ts-expect-error
-      app.component(key, components[key])
-      app.provide("HOPP_UI_OPTIONS", options)
-    }
+    app.provide("HOPP_UI_OPTIONS", options)
   },
 }
 
 export default plugin
-
-export * from "./components/"

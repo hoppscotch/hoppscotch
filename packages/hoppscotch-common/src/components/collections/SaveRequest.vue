fa7adb748334bc7e8c1cff0854ea7275567d5f07
Andrew Bastin <andrewbastin.k@gmail.com>
2023-01-31 00:28:03 +0530

    chore: correct some stuff in SaveRequest

--- packages/hoppscotch-common/src/components/collections/SaveRequest.vue
+++ packages/hoppscotch-common/src/components/collections/SaveRequest.vue
@@ -78,7 +78,7 @@ import {
   createRequestInCollection,
   updateTeamRequest,
 } from "~/helpers/backend/mutations/TeamRequest"
-import { Picked } from "~/helpers/types/HoppPicked.js"
+import { Picked } from "~/helpers/types/HoppPicked"
 import { getGQLSession, useGQLRequestName } from "~/newstore/GQLSession"
 import {
   getRESTRequest,

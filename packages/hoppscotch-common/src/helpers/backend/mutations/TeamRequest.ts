8d7ea65a12e8337c74edb266796926a128c84055
Nivedin <nivedinp@gmail.com>
2023-02-01 22:53:06 +0530

    feat: added mutation scripts for coll and req

--- packages/hoppscotch-common/src/helpers/backend/mutations/TeamRequest.ts
+++ packages/hoppscotch-common/src/helpers/backend/mutations/TeamRequest.ts
@@ -9,16 +9,27 @@ import {
   MoveRestTeamRequestDocument,
   MoveRestTeamRequestMutation,
   MoveRestTeamRequestMutationVariables,
+  UpdateLookUpRequestOrderDocument,
+  UpdateLookUpRequestOrderMutation,
+  UpdateLookUpRequestOrderMutationVariables,
   UpdateRequestDocument,
   UpdateRequestMutation,
   UpdateRequestMutationVariables,
 } from "../graphql"
 
+type DeleteRequestErrors = "team_req/not_found"
+
 type MoveRestTeamRequestErrors =
   | "team_req/not_found"
   | "team_req/invalid_target_id"
+  | "team/invalid_coll_id"
+  | "team_req/not_required_role"
+  | "bug/team_req/no_req_id"
 
-type DeleteRequestErrors = "team_req/not_found"
+type UpdateLookUpRequestOrderErrors =
+  | "team_req/not_found"
+  | "team/request_and_next_request_are_same"
+  | "team_req/requests_not_from_same_collection"
 
 export const createRequestInCollection = (
   collectionID: string,
@@ -61,12 +72,27 @@ export const deleteTeamRequest = (requestID: string) =>
     requestID,
   })
 
-export const moveRESTTeamRequest = (requestID: string, collectionID: string) =>
+export const moveRESTTeamRequest = (collectionID: string, requestID: string) =>
   runMutation<
     MoveRestTeamRequestMutation,
     MoveRestTeamRequestMutationVariables,
     MoveRestTeamRequestErrors
   >(MoveRestTeamRequestDocument, {
+    collectionID,
     requestID,
+  })
+
+export const updateOrderRESTTeamRequest = (
+  requestID: string,
+  nextRequestID: string,
+  collectionID: string
+) =>
+  runMutation<
+    UpdateLookUpRequestOrderMutation,
+    UpdateLookUpRequestOrderMutationVariables,
+    UpdateLookUpRequestOrderErrors
+  >(UpdateLookUpRequestOrderDocument, {
+    requestID,
+    nextRequestID,
     collectionID,
   })

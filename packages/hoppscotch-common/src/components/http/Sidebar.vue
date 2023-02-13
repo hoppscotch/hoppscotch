ea77dfa1b1ce1527e2a06120d07ac8bbde8dcbf5
Nivedin <nivedinp@gmail.com>
2023-02-13 18:12:22 +0530

    chore: removed the reduntend code

--- packages/hoppscotch-common/src/components/http/Sidebar.vue
+++ packages/hoppscotch-common/src/components/http/Sidebar.vue
@@ -33,30 +33,12 @@
 import IconClock from "~icons/lucide/clock"
 import IconLayers from "~icons/lucide/layers"
 import IconFolder from "~icons/lucide/folder"
-import IconUser from "~icons/lucide/user"
-import IconUsers from "~icons/lucide/users"
-import IconMore from "~icons/lucide/more-vertical"
-import { computed, ref } from "vue"
+import { ref } from "vue"
 import { useI18n } from "@composables/i18n"
-import { useReadonlyStream } from "~/composables/stream"
-import { workspaceStatus$ } from "~/newstore/workspace"
-import IconEdit from "~icons/lucide/edit"
-import { TippyComponent } from "vue-tippy"
 
 const t = useI18n()
 
 type RequestOptionTabs = "history" | "collections" | "env"
 
 const selectedNavigationTab = ref<RequestOptionTabs>("history")
-
-const workspace = useReadonlyStream(workspaceStatus$, { type: "personal" })
-
-const teamWorkspaceName = computed(() => {
-  if (workspace.value.type === "team" && workspace.value.teamName) {
-    return workspace.value.teamName
-  } else {
-    return `${t("workspace.team")}`
-  }
-})
-const tippyActions = ref<TippyComponent | null>(null)
 </script>

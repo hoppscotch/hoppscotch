80c6f600dbe14360838e9e932a0944249235cdec
Balu Babu <balub997@gmail.com>
2023-03-03 15:52:26 +0530

    hotfix: refresh token cookie migration (HBE-167) (#31)
    * chore: removed signed cookies from refresh token strategy
    
    * chore: removed signed cookies from refresh RTCookie decorator

--- packages/hoppscotch-backend/src/decorators/rt-cookie.decorator.ts
+++ packages/hoppscotch-backend/src/decorators/rt-cookie.decorator.ts
@@ -7,6 +7,6 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export const RTCookie = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req.signedCookies['refresh_token'];
+    return ctx.getContext().req.cookies['refresh_token'];
   },
 );

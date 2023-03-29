cc802b1e9f4fc337d3bba448dc321c36f849112a
Akash K <57758277+amk-dev@users.noreply.github.com>
2023-03-30 00:09:08 +0530

    chore: move history firebase things to hoppscotch-web (#2954)

---
+++ packages/hoppscotch-common/src/platform/history.ts
@@ -0,0 +1,3 @@
+export type HistoryPlatformDef = {
+  initHistorySync: () => void
+}
--- packages/hoppscotch-web/src/main.ts
+++ packages/hoppscotch-web/src/main.ts
@@ -3,6 +3,7 @@ import { def as authDef } from "./firebase/auth"
 import { def as envDef } from "./environments"
 import { def as collectionsDef } from "./collections"
 import { def as settingsDef } from "./settings"
+import { def as historyDef } from "./history"
 
 createHoppApp("#app", {
   auth: authDef,
@@ -10,5 +11,6 @@ createHoppApp("#app", {
     environments: envDef,
     collections: collectionsDef,
     settings: settingsDef,
+    history: historyDef,
   },
 })

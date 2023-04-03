c8b88b9b598b3296257957c2f7bed7fde76cf1f8
amk-dev <akash.k.mohan98@gmail.com>
2023-04-04 04:06:11 +0530

    chore: init firebase in hoppscotch-web

--- packages/hoppscotch-web/src/main.ts
+++ packages/hoppscotch-web/src/main.ts
@@ -1,4 +1,5 @@
 import { createHoppApp } from "@hoppscotch/common"
+import { initializeFirebase } from "./firebase/init"
 import { def as authDef } from "./firebase/auth"
 import { def as envDef } from "./environments"
 import { def as collectionsDef } from "./collections"
@@ -6,6 +7,8 @@ import { def as settingsDef } from "./settings"
 import { def as historyDef } from "./history"
 import { def as tabStateDef } from "./tab"
 import { def as analyticsDef } from "./analytics"
+
+initializeFirebase()
 
 createHoppApp("#app", {
   auth: authDef,

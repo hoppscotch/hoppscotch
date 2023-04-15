3c08e02099515d21e0ae71cc3c8b74575ce5bfed
SAMUEL J MATHEW <1846982+samuelj90@users.noreply.github.com>
2023-04-15 13:18:06 +0000

    fix: updating authorization feature url

--- packages/hoppscotch-common/src/components/http/Authorization.vue
+++ packages/hoppscotch-common/src/components/http/Authorization.vue
@@ -100,7 +100,7 @@
         >
         <HoppButtonSecondary
           v-tippy="{ theme: 'tooltip' }"
-          to="https://docs.hoppscotch.io/features/authorization"
+          to="https://docs.hoppscotch.io/documentation/features/authorization"
           blank
           :title="t('app.wiki')"
           :icon="IconHelpCircle"
@@ -127,7 +127,7 @@
       <HoppButtonSecondary
         outline
         :label="t('app.documentation')"
-        to="https://docs.hoppscotch.io/features/authorization"
+        to="https://docs.hoppscotch.io/documentation/features/authorization"
         blank
         :icon="IconExternalLink"
         reverse
@@ -164,7 +164,7 @@
           class="link"
           :label="t('authorization.learn')"
           :icon="IconExternalLink"
-          to="https://docs.hoppscotch.io/features/authorization"
+          to="https://docs.hoppscotch.io/documentation/features/authorization"
           blank
           reverse
         />

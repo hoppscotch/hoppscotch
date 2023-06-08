4bca17a1652a56737b1ff069caab9c963d932e24
Anwarul Islam <anwaarulislaam@gmail.com>
2023-06-08 06:59:31 +0600

    feat: placeholder component added

---
+++ packages/hoppscotch-ui/src/components/smart/Placeholder.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center p-4 text-secondaryLight"
+  >
+    <img
+      :src="src"
+      loading="lazy"
+      class="inline-flex flex-col object-contain object-center w-16 h-16 mb-4"
+      :alt="alt"
+    />
+    <span class="pb-4 text-center">
+      {{ text }}
+    </span>
+
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    src: string
+    alt?: string
+    text?: string
+  }>(),
+  {
+    alt: "",
+    text: "",
+  }
+)
+</script>

7de1f676aa8a12a19e259e6335c3c8dc65794c37
Omer Baflah <aldonramos@gmail.com>
2023-06-20 17:36:35 +0300

    fix: typo in auth error doc block

--- packages/hoppscotch-backend/src/types/AuthError.ts
+++ packages/hoppscotch-backend/src/types/AuthError.ts
@@ -2,7 +2,7 @@ import { HttpStatus } from '@nestjs/common';
 
 /**
  ** Custom interface to handle errors specific to Auth module
- ** Since it's REST we need to return the HTTP status code along with the error message
+ ** Since its REST we need to return the HTTP status code along with the error message
  */
 export type AuthError = {
   message: string;

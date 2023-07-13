162bca4efa6bc8d3312ed53fb7ef23008e52c907
Nivedin <nivedinp@gmail.com>
2023-07-13 16:32:48 +0530

    chore: update URL regex

--- packages/hoppscotch-common/src/services/context-menu/menu/url.menu.ts
+++ packages/hoppscotch-common/src/services/context-menu/menu/url.menu.ts
@@ -14,7 +14,7 @@ import { getI18n } from "~/modules/i18n"
 
 //regex containing both url
 const url = new RegExp(
-  /^(?:(?:https?|ftp):\/\/)?(?:www\.)?(?:[A-Za-z0-9_-]+\.)*[A-Za-z0-9_-]+(?::\d+)?$/
+  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
 )
 
 export class URLMenuService extends Service implements ContextMenu {

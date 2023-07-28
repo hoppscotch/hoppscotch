c79fcbecebc39a15e75d7b04c76cb304447db308
Mir Arif Hasan <arif.ishan05@gmail.com>
2023-07-28 18:26:42 +0600

    chore: handled internal server error for missing auth providers

--- packages/hoppscotch-backend/src/auth/guards/github-sso.guard.ts
+++ packages/hoppscotch-backend/src/auth/guards/github-sso.guard.ts
@@ -1,8 +1,16 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthProvider, authProviderCheck, throwHTTPErr } from '../helper';
 
 @Injectable()
-export class GithubSSOGuard extends AuthGuard('github') {
+export class GithubSSOGuard extends AuthGuard('github') implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    if (!authProviderCheck(AuthProvider.GITHUB))
+      throwHTTPErr({ message: 'GitHub auth is not enabled', statusCode: 404 });
+
+    return true;
+  }
+
   getAuthenticateOptions(context: ExecutionContext) {
     const req = context.switchToHttp().getRequest();
 
--- packages/hoppscotch-backend/src/auth/guards/microsoft-sso-.guard.ts
+++ packages/hoppscotch-backend/src/auth/guards/microsoft-sso-.guard.ts
@@ -1,8 +1,22 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthProvider, authProviderCheck, throwHTTPErr } from '../helper';
 
 @Injectable()
-export class MicrosoftSSOGuard extends AuthGuard('microsoft') {
+export class MicrosoftSSOGuard
+  extends AuthGuard('microsoft')
+  implements CanActivate
+{
+  canActivate(context: ExecutionContext): boolean {
+    if (!authProviderCheck(AuthProvider.MICROSOFT))
+      throwHTTPErr({
+        message: 'Microsoft auth is not enabled',
+        statusCode: 404,
+      });
+
+    return true;
+  }
+
   getAuthenticateOptions(context: ExecutionContext) {
     const req = context.switchToHttp().getRequest();
 

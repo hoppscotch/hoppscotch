419e376f466791e14d6afca5fb6cbc3f5a8e094a
Mir Arif Hasan <arif.ishan05@gmail.com>
2023-07-29 15:02:54 +0600

    fix: provider return type in SSO guards

--- packages/hoppscotch-backend/src/auth/guards/github-sso.guard.ts
+++ packages/hoppscotch-backend/src/auth/guards/github-sso.guard.ts
@@ -1,14 +1,17 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthProvider, authProviderCheck, throwHTTPErr } from '../helper';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class GithubSSOGuard extends AuthGuard('github') implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     if (!authProviderCheck(AuthProvider.GITHUB))
       throwHTTPErr({ message: 'GitHub auth is not enabled', statusCode: 404 });
 
-    return true;
+    return super.canActivate(context);
   }
 
   getAuthenticateOptions(context: ExecutionContext) {

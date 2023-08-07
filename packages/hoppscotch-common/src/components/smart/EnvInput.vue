22454919e57d9d486b0c8714467f7056e07621a9
Liyas Thomas <liyascthomas@gmail.com>
2023-08-07 09:26:52 +0530

    chore: minor ui improvements

--- packages/hoppscotch-common/src/components/smart/EnvInput.vue
+++ packages/hoppscotch-common/src/components/smart/EnvInput.vue
@@ -12,18 +12,20 @@
       ></div>
 
       <div v-if="hasApplicableInspector">
-        <tippy interactive theme="popover">
+        <tippy interactive trigger="click" theme="popover">
           <div class="flex justify-center items-center flex-1 felx-col">
             <HoppButtonSecondary
+              v-tippy="{ theme: 'tooltip' }"
               :icon="IconAlertTriangle"
               :class="severityColor(getHighestSeverity.severity)"
+              :title="t('inspections.description')"
             />
           </div>
           <template #content="{ hide }">
-            <div
-              class="flex flex-col items-start flex-1 px-2 divide-y divide-divider"
-            >
-              <span class="flex justify-between flex-1 w-full">
+            <div class="flex flex-col space-y-2 items-start flex-1">
+              <div
+                class="flex justify-between border rounded pl-2 border-divider bg-popover sticky top-0 flex-1 w-full"
+              >
                 <span class="flex items-center flex-1">
                   <icon-lucide-activity class="mr-2 svg-icons text-accent" />
                   <span class="font-bold">
@@ -37,8 +39,12 @@
                   :title="t('app.wiki')"
                   :icon="IconHelpCircle"
                 />
-              </span>
-              <div v-for="(inspector, index) in inspectors" :key="index">
+              </div>
+              <div
+                v-for="(inspector, index) in inspectors"
+                :key="index"
+                class="flex w-full"
+              >
                 <div
                   v-if="
                     inspector.isApplicable &&
@@ -47,15 +53,17 @@
                       ? inspector.index === envIndex
                       : true)
                   "
-                  class="flex justify-center items-center w-full py-3"
+                  class="flex items-center p-2 border rounded border-dividerDark border-dashed items-center w-full"
                 >
                   <div class="flex space-x-4 items-center">
                     <span v-if="inspector.text.type === 'text'" class="flex-1">
                       {{ inspector.text.text }}
                     </span>
                     <span v-if="inspector.action">
-                      <HoppButtonPrimary
+                      <HoppButtonSecondary
                         :label="inspector.action.text"
+                        outline
+                        filled
                         @click="
                           () => {
                             inspector.action?.apply()
@@ -306,13 +314,13 @@ onMounted(() => {
 const severityColor = (severity: number) => {
   switch (severity) {
     case 1:
-      return "text-green-500"
+      return "text-green-500 hover:text-green-600"
     case 2:
-      return "text-yellow-500"
+      return "text-yellow-500 hover:text-yellow-600"
     case 3:
-      return "text-red-500"
+      return "text-red-500 hover:text-red-600"
     default:
-      return "text-gray-500"
+      return "text-gray-500 hover:text-gray-600"
   }
 }
 

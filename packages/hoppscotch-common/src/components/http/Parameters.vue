005581ee7da3e0ae268cccb20579ed44cb48f205
James George <jamesgeorge998001@gmail.com>
2023-09-12 00:02:10 -0700

    fix: broken link to `REST API Testing` docs (#3333)
    fix: broken link to REST API Testing docs

--- packages/hoppscotch-common/src/components/http/Parameters.vue
+++ packages/hoppscotch-common/src/components/http/Parameters.vue
@@ -9,7 +9,7 @@
       <div class="flex">
         <HoppButtonSecondary
           v-tippy="{ theme: 'tooltip' }"
-          to="https://docs.hoppscotch.io/documentation/getting-started/rest/using-parameters"
+          to="https://docs.hoppscotch.io/documentation/features/rest-api-testing"
           blank
           :title="t('app.wiki')"
           :icon="IconHelpCircle"

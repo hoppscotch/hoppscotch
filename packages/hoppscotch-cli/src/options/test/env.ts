f226974101f618b9c9e4f19b7a49fc5b452bc5eb
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2023-09-21 14:20:42 +0530

    feat: introducing zod for checking json format for environment variables

--- packages/hoppscotch-cli/src/options/test/env.ts
+++ packages/hoppscotch-cli/src/options/test/env.ts
@@ -1,27 +1,30 @@
 import { error } from "../../types/errors";
-import { HoppEnvs, HoppEnvPair } from "../../types/request";
+import { HoppEnvs, HoppEnvPair, HoppEnvObject } from "../../types/request";
 import { readJsonFile } from "../../utils/mutators";
-
 /**
  * Parses env json file for given path and validates the parsed env json object.
  * @param path Path of env.json file to be parsed.
  * @returns For successful parsing we get HoppEnvs object.
  */
 export async function parseEnvsData(path: string) {
-  const contents = await readJsonFile(path)
+  const contents = await readJsonFile(path);
+  const envPairs: Array<HoppEnvPair> = [];
+  const HoppEnvPairResult = HoppEnvPair.safeParse(contents);
+  const HoppEnvObjectResult = HoppEnvObject.safeParse(contents);
 
-  if(!(contents && typeof contents === "object" && !Array.isArray(contents))) {
-    throw error({ code: "MALFORMED_ENV_FILE", path, data: null })
+  if (!(HoppEnvPairResult.success || HoppEnvObjectResult.success)) {
+    throw error({ code: "MALFORMED_ENV_FILE", path, data: error });
   }
 
-  const envPairs: Array<HoppEnvPair> = []
-
-  for( const [key,value] of Object.entries(contents)) {
-    if(typeof value !== "string") {
-      throw error({ code: "MALFORMED_ENV_FILE", path, data: {value: value} })
+  if (HoppEnvPairResult.success) {
+    for (const [key, value] of Object.entries(HoppEnvPairResult.data)) {
+      envPairs.push({ key, value });
     }
-
-    envPairs.push({key, value})
+  } else if (HoppEnvObjectResult.success) {
+    const key = HoppEnvObjectResult.data.variables[0].key;
+    const value = HoppEnvObjectResult.data.variables[0].value;
+    envPairs.push({ key, value });
   }
-  return <HoppEnvs>{ global: [], selected: envPairs }
+
+  return <HoppEnvs>{ global: [], selected: envPairs };
 }

aea9bde93a7bcc9466221f71e81cf43de7d3bb27
Anwarul Islam <anwaarulislaam@gmail.com>
2023-10-05 15:42:04 +0600

    fix: an unknown keyboard event issue

--- packages/hoppscotch-common/src/helpers/keybindings.ts
+++ packages/hoppscotch-common/src/helpers/keybindings.ts
@@ -131,7 +131,8 @@ function generateKeybindingString(ev: KeyboardEvent): ShortcutKey | null {
 }
 
 function getPressedKey(ev: KeyboardEvent): Key | null {
-  const val = ev.code.toLowerCase()
+  // Sometimes the property code is not available on the KeyboardEvent object
+  const val = (ev.code ?? "").toLowerCase()
 
   // Check arrow keys
   if (val === "arrowup") return "up"

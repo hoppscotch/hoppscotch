7dee372dc21021195ccaaeb33b1b1a06758c59cf
jamesgeorge007 <jamesgeorge998001@gmail.com>
2023-10-05 15:15:17 +0530

    chore: update comment about the watcher purpose

--- packages/hoppscotch-common/src/components/collections/index.vue
+++ packages/hoppscotch-common/src/components/collections/index.vue
@@ -377,8 +377,8 @@ const updateSelectedTeam = (team: SelectedTeam) => {
 const workspace = workspaceService.currentWorkspace
 
 // Used to switch collection type and team when user switch workspace in the global workspace switcher
-// Check if there is a teamID in the workspace, if yes, switch to team collection and select the team
-// If there is no teamID, switch to my environment
+// Check if there is a teamID in the workspace, if yes, switch to team collections and select the team
+// If there is no teamID, switch to my collections
 watch(
   () => {
     const space = workspace.value

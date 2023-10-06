17d6ae15a5e89550af3cc6ea8a29ca3bac8283ba
Rakibul Yeasin <ryeasin03@gmail.com>
2023-10-06 12:27:26 +0600

    fix: Cannot set custom method #3406 (#3408)
    * fix: #3406
    
    * chore: remove console log
    
    * fix: an unknown keyboard event issue
    
    ---------
    
    Co-authored-by: Liyas Thomas <liyascthomas@gmail.com>
    Co-authored-by: Anwarul Islam <anwaarulislaam@gmail.com>

--- packages/hoppscotch-common/src/components/http/Request.vue
+++ packages/hoppscotch-common/src/components/http/Request.vue
@@ -426,7 +426,7 @@ const updateMethod = (method: string) => {
 
 const onSelectMethod = (e: Event | any) => {
   // type any because of value property not being recognized by TS in the event.target object. It is a valid property though.
-  updateMethod(e.value)
+  updateMethod(e.target.value)
 }
 
 const clearContent = () => {

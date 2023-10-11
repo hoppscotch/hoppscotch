82464ce8ea8beed6b9ac587e836ffb711604f950
amk-dev <akash.k.mohan98@gmail.com>
2023-10-11 15:15:49 +0530

    refactor: refactor gql collections import export

---
+++ packages/hoppscotch-common/src/helpers/import-export/export/gqlCollectionsGistExporter.ts
@@ -0,0 +1,19 @@
+import * as E from "fp-ts/Either"
+import { createGist } from "~/helpers/gist"
+
+export const gqlCollectionsGistExporter = async (
+  gqlCollectionsJSON: string,
+  accessToken: string
+) => {
+  const res = await createGist(
+    gqlCollectionsJSON,
+    "hoppscotch-collections.json",
+    accessToken
+  )()
+
+  if (E.isLeft(res)) {
+    return E.left(res.left)
+  } else {
+    return E.right(res.right.data.html_url as string)
+  }
+}

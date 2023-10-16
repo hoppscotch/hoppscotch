f5db54484cfe7e018f9f7c01dea1ec8cbffc97e5
Mir Arif Hasan <arif.ishan05@gmail.com>
2023-10-16 12:53:55 +0600

    HBE-266 Update NestJS packages (#3389)
    * build: update npm nest packages
    
    * build: removed depricated apollo-server-plugin package
    
    * build: pnpm-lock file added
    
    * build: swc integrated
    
    * Revert "build: swc integrated"
    
    This reverts commit 803a01f38f210dfbcd603665893d29af565c8908.
    
    * feat: upgrade graphql* packages version
    
    * feat: upgrade point release
    
    * feat: update pnpm-lock file

--- packages/hoppscotch-backend/src/guards/throttler-behind-proxy.guard.ts
+++ packages/hoppscotch-backend/src/guards/throttler-behind-proxy.guard.ts
@@ -3,8 +3,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
-  protected getTracker(req: Record<string, any>): string {
+  protected async getTracker(req: Record<string, any>): Promise<string> {
     return req.ips.length ? req.ips[0] : req.ip; // individualize IP extraction to meet your own needs
-    // learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For#directives
   }
 }

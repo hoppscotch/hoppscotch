489370f82b936b946979ba2bafa11b9c5ea6a5e8
Mir Arif Hasan <arif.ishan05@gmail.com>
2023-10-19 14:49:20 +0600

    feat: admin extends user with omitting some fields

--- packages/hoppscotch-backend/src/admin/admin.model.ts
+++ packages/hoppscotch-backend/src/admin/admin.model.ts
@@ -1,5 +1,9 @@
-import { ObjectType, PartialType } from '@nestjs/graphql';
+import { ObjectType, OmitType, PartialType } from '@nestjs/graphql';
 import { User } from 'src/user/user.model';
 
 @ObjectType()
-export class Admin extends PartialType(User) {}
+export class Admin extends OmitType(User, [
+  'isAdmin',
+  'currentRESTSession',
+  'currentGQLSession',
+]) {}

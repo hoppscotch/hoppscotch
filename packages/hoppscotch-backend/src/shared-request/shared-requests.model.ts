f149582a62931b764845d6b01489fc30d73b6dea
Balu Babu <balub997@gmail.com>
2023-10-25 20:47:13 +0530

    chore: created shared-request model

---
+++ packages/hoppscotch-backend/src/shared-request/shared-requests.model.ts
@@ -0,0 +1,25 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class SharedRequest {
+  @Field(() => ID, {
+    description: 'The 12 digit alphanumeric code',
+  })
+  id: string;
+
+  @Field({
+    description: 'JSON string representing the request data',
+  })
+  request: string;
+
+  @Field({
+    description: 'JSON string representing the properties for an embed',
+    nullable: true,
+  })
+  properties: string;
+
+  @Field({
+    description: 'Timestamp of when the Shortcode was created',
+  })
+  createdOn: Date;
+}

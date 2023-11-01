4f2ea55f717078f8e0fe742df8140a5d2112a95d
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2023-11-01 21:42:38 +0530

    chore: provided jsdoc comments for the banner service properties

--- packages/hoppscotch-common/src/services/banner.service.ts
+++ packages/hoppscotch-common/src/services/banner.service.ts
@@ -1,6 +1,9 @@
 import { Service } from "dioc"
 import { ref } from "vue"
 
+/**
+ * The different types of banners that can be used.
+ */
 export type BannerType = "info" | "warning" | "error"
 
 export type BannerContent = {
@@ -10,11 +13,16 @@ export type BannerContent = {
   alternateText?: string
 }
 
+/**
+ * This service is used to display a banner on the app.
+ * It can used to display information, warnings or errors.
+ */
 export class BannerService extends Service {
   public static readonly ID = "BANNER_SERVICE"
-  constructor() {
-    super()
-  }
 
+  /**
+   * This is a reactive variable that can be used to set the contents of the banner
+   * and use it to render the banner on components.
+   */
   public content = ref<BannerContent | undefined>(undefined)
 }

be2217d0e741e2cd7a41705fb3670ca795a20e19
Raphael Hehl <raphael@rhehl.de>
2023-11-10 14:24:41 +0100

    added getTracker to all Guards

--- packages/hoppscotch-backend/src/guards/gql-auth.guard.ts
+++ packages/hoppscotch-backend/src/guards/gql-auth.guard.ts
@@ -9,4 +9,7 @@ export class GqlAuthGuard extends AuthGuard('jwt') {
     const { req, headers } = ctx.getContext();
     return headers ? headers : req;
   }
+  protected async getTracker(req: Record<string, any>): Promise<string> {
+    return req.ips.length ? req.ips[0] : req.ip; // individualize IP extraction to meet your own needs
+  }
 }

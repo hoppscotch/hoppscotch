9783d7a073afe2cb7afe84524bb7309da44818bf
Raphael Hehl <raphael@rhehl.de>
2023-12-05 12:33:10 +0100

    Update mailer.module.ts

--- packages/hoppscotch-backend/src/mailer/mailer.module.ts
+++ packages/hoppscotch-backend/src/mailer/mailer.module.ts
@@ -1,27 +1,36 @@
-import { Module } from '@nestjs/common';
 import { MailerModule as NestMailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { MailerService } from './mailer.service';
+import { Module } from '@nestjs/common';
+import { MAILER_FROM_ADDRESS_UNDEFINED } from 'src/errors';
 import { throwErr } from 'src/utils';
-import {
-  MAILER_FROM_ADDRESS_UNDEFINED,
-  MAILER_SMTP_URL_UNDEFINED,
-} from 'src/errors';
+import { MailerService } from './mailer.service';
 
 @Module({
   imports: [
-    NestMailerModule.forRoot({
-      transport:
-        process.env.MAILER_SMTP_URL ?? throwErr(MAILER_SMTP_URL_UNDEFINED),
-      defaults: {
-        from:
-          process.env.MAILER_ADDRESS_FROM ??
-          throwErr(MAILER_FROM_ADDRESS_UNDEFINED),
-      },
-      template: {
-        dir: __dirname + '/templates',
-        adapter: new HandlebarsAdapter(),
-      },
+    NestMailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: process.env.SMTP_DOMAIN,
+          port: 587,
+          secure: false, // upgrade later with STARTTLS
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASSWORD,
+          },
+          tls: {
+            rejectUnauthorized: false,
+          },
+        },
+        defaults: {
+          from:
+            process.env.MAILER_ADDRESS_FROM ??
+            throwErr(MAILER_FROM_ADDRESS_UNDEFINED),
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new HandlebarsAdapter(),
+        },
+      }),
     }),
   ],
   providers: [MailerService],

9d2b7cc03fff531ef578dfe1b98c7e32012a3fd9
nivedin <nivedinp@gmail.com>
2023-12-11 17:39:40 +0530

    refactor: update inherited property schema

--- packages/hoppscotch-common/src/helpers/types/HoppInheritedProperties.ts
+++ packages/hoppscotch-common/src/helpers/types/HoppInheritedProperties.ts
@@ -1,14 +1,19 @@
-import { HoppRESTRequest } from "@hoppscotch/data"
+import {
+  GQLHeader,
+  HoppGQLAuth,
+  HoppRESTHeader,
+  HoppRESTAuth,
+} from "@hoppscotch/data"
 
 export type HoppInheritedProperty = {
   auth: {
     parentID: string
     parentName: string
-    inheritedAuth: HoppRESTRequest["auth"]
+    inheritedAuth: HoppRESTAuth | HoppGQLAuth
   }
   headers: {
     parentID: string
     parentName: string
-    inheritedHeader?: HoppRESTRequest["headers"][number]
+    inheritedHeader: HoppRESTHeader | GQLHeader
   }[]
 }

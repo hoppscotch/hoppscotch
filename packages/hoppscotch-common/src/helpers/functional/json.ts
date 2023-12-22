ee2e8227fc277a227495cac1969a972d54ef8399
jamesgeorge007 <jamesgeorge998001@gmail.com>
2023-12-22 20:59:05 +0530

    chore: address review comment

--- packages/hoppscotch-common/src/helpers/functional/json.ts
+++ packages/hoppscotch-common/src/helpers/functional/json.ts
@@ -1,15 +1,17 @@
 import * as O from "fp-ts/Option"
 import { flow } from "fp-ts/function"
 
+type SafeParseJSON = {
+  (str: string, convertToArray: true): O.Option<Array<unknown>>
+  (str: string, convertToArray?: false): O.Option<Record<string, unknown>>
+}
+
 /**
  * Checks and Parses JSON string
  * @param str Raw JSON data to be parsed
  * @returns Option type with some(JSON data) or none
  */
-export const safeParseJSON = (
-  str: string,
-  convertToArray = false
-): O.Option<Record<string, unknown> | Array<unknown>> =>
+export const safeParseJSON: SafeParseJSON = (str, convertToArray = false) =>
   O.tryCatch(() => {
     const data = JSON.parse(str)
     if (convertToArray) {

26d9801cefede1942f80028de1c8baf92db8e4a9
jamesgeorge007 <jamesgeorge998001@gmail.com>
2023-12-22 14:03:58 +0530

    feat: support importing environments individually

--- packages/hoppscotch-common/src/helpers/functional/json.ts
+++ packages/hoppscotch-common/src/helpers/functional/json.ts
@@ -6,8 +6,17 @@ import { flow } from "fp-ts/function"
  * @param str Raw JSON data to be parsed
  * @returns Option type with some(JSON data) or none
  */
-export const safeParseJSON = (str: string): O.Option<object> =>
-  O.tryCatch(() => JSON.parse(str))
+export const safeParseJSON = (
+  str: string,
+  convertToArray = false
+): O.Option<Record<string, unknown> | Array<unknown>> =>
+  O.tryCatch(() => {
+    const data = JSON.parse(str)
+    if (convertToArray) {
+      return Array.isArray(data) ? data : [data]
+    }
+    return data
+  })
 
 /**
  * Checks if given string is a JSON string

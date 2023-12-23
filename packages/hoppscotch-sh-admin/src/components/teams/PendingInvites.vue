21b07d9de5d5e557f27bae882b3fa1cfd60d1b80
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2023-12-23 13:16:34 +0530

    refactor: code improvements to pending invites component

--- packages/hoppscotch-sh-admin/src/components/teams/PendingInvites.vue
+++ packages/hoppscotch-sh-admin/src/components/teams/PendingInvites.vue
@@ -1,103 +1,76 @@
 <template>
   <div class="border rounded divide-y divide-dividerLight border-divider my-8">
-    <div v-if="fetching" class="flex items-center justify-center p-4">
-      <HoppSmartSpinner />
-    </div>
-    <div v-else>
-      <div v-if="team" class="divide-y divide-dividerLight">
-        <div
-          v-for="(invitee, index) in pendingInvites"
-          :key="`invitee-${index}`"
-          class="flex divide-x divide-dividerLight"
-        >
-          <input
-            v-if="invitee"
-            class="flex flex-1 px-4 py-2 bg-transparent text-secondaryLight"
-            placeholder="Email"
-            :name="'param' + index"
-            :value="invitee.inviteeEmail"
-            readonly
+    <HoppSmartPlaceholder
+      v-if="team && pendingInvites?.length === 0"
+      text="No pending invites"
+    />
+
+    <div v-else class="divide-y divide-dividerLight">
+      <div
+        v-for="(invitee, index) in pendingInvites"
+        :key="`invitee-${index}`"
+        class="flex divide-x divide-dividerLight"
+      >
+        <input
+          v-if="invitee"
+          class="flex flex-1 px-4 py-2 bg-transparent text-secondaryLight"
+          :placeholder="t('teams.email_title')"
+          :name="'param' + index"
+          :value="invitee.inviteeEmail"
+          readonly
+        />
+        <input
+          class="flex flex-1 px-4 py-2 bg-transparent text-secondaryLight"
+          :placeholder="t('teams.permission')"
+          :name="'value' + index"
+          :value="invitee.inviteeRole"
+          readonly
+        />
+        <div class="flex">
+          <HoppButtonSecondary
+            v-tippy="{ theme: 'tooltip' }"
+            :title="t('teams.remove')"
+            :icon="IconTrash"
+            color="red"
+            :loading="isLoadingIndex === index"
+            @click="removeInvitee(invitee.id, index)"
           />
-          <input
-            class="flex flex-1 px-4 py-2 bg-transparent text-secondaryLight"
-            placeholder="Permissions"
-            :name="'value' + index"
-            :value="invitee.inviteeRole"
-            readonly
-          />
-          <div class="flex">
-            <HoppButtonSecondary
-              v-tippy="{ theme: 'tooltip' }"
-              :title="t('teams.remove')"
-              :icon="IconTrash"
-              color="red"
-              :loading="isLoadingIndex === index"
-              @click="removeInvitee(invitee.id, index)"
-            />
-          </div>
         </div>
       </div>
-      <HoppSmartPlaceholder
-        v-if="team && pendingInvites?.length === 0"
-        text="No pending invites"
-      >
-        <template #body>
-          <div v-if="!fetching && error" class="flex flex-col items-center p-4">
-            <icon-lucide-help-circle class="mb-4 svg-icons" />
-            {{ t('error.something_went_wrong') }}
-          </div>
-        </template>
-      </HoppSmartPlaceholder>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import IconTrash from '~icons/lucide/trash';
-import { useMutation, useClientHandle } from '@urql/vue';
-import { ref, onMounted } from 'vue';
+import { useMutation } from '@urql/vue';
+import { useVModel } from '@vueuse/core';
+import { computed, ref } from 'vue';
+import { useI18n } from '~/composables/i18n';
+import { useToast } from '~/composables/toast';
 import {
   RevokeTeamInvitationDocument,
-  TeamInfoDocument,
   TeamInfoQuery,
 } from '~/helpers/backend/graphql';
-import { useToast } from '~/composables/toast';
-import { useRoute } from 'vue-router';
-import { useI18n } from '~/composables/i18n';
+import IconTrash from '~icons/lucide/trash';
 
 const t = useI18n();
-
 const toast = useToast();
 
-// Get details of the team
-const fetching = ref(true);
-const error = ref(false);
-const { client } = useClientHandle();
-const route = useRoute();
-const team = ref<TeamInfoQuery['infra']['teamInfo'] | undefined>();
-const pendingInvites = ref<
-  TeamInfoQuery['infra']['teamInfo']['teamInvitations'] | undefined
->();
+const props = defineProps<{
+  team: TeamInfoQuery['infra']['teamInfo'];
+}>();
 
-const getTeamInfo = async () => {
-  fetching.value = true;
-  const result = await client
-    .query(TeamInfoDocument, { teamID: route.params.id.toString() })
-    .toPromise();
+const emit = defineEmits<{
+  (event: 'update:team', team: TeamInfoQuery['infra']['teamInfo']): void;
+}>();
 
-  if (result.error) {
-    error.value = true;
-    return toast.error(`${t('teams.load_info_error')}`);
-  }
-
-  if (result.data?.infra.teamInfo) {
-    team.value = result.data.infra.teamInfo;
-    pendingInvites.value = team.value.teamInvitations;
-  }
-  fetching.value = false;
-};
-
-onMounted(async () => await getTeamInfo());
+const team = useVModel(props, 'team', emit);
+const pendingInvites = computed({
+  get: () => team.value?.teamInvitations,
+  set: (value) => {
+    team.value.teamInvitations = value;
+  },
+});
 
 // Remove Invitation
 const isLoadingIndex = ref<null | number>(null);
@@ -110,7 +83,7 @@ const removeInvitee = async (id: string, index: number) => {
   isLoadingIndex.value = index;
   const result = await revokeTeamInvitation(id);
   if (result.error) {
-    toast.error(`${t('state.remove_invitee_failure')}`);
+    toast.error(t('state.remove_invitee_failure'));
   } else {
     if (pendingInvites.value) {
       pendingInvites.value = pendingInvites.value.filter(
@@ -118,7 +91,7 @@ const removeInvitee = async (id: string, index: number) => {
           return invite.id !== id;
         }
       );
-      toast.success(`${t('state.remove_invitee_success')}`);
+      toast.success(t('state.remove_invitee_success'));
     }
   }
   isLoadingIndex.value = null;

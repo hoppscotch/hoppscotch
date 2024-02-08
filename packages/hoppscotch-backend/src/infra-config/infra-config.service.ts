b0b7df0a3ef662656ef144296fa4305a07e73b42
mirarifhasan <arif.ishan05@gmail.com>
2024-02-08 15:30:05 +0600

    feat: removed unnecessary checks

--- packages/hoppscotch-backend/src/infra-config/infra-config.service.ts
+++ packages/hoppscotch-backend/src/infra-config/infra-config.service.ts
@@ -51,16 +51,8 @@ export class InfraConfigService implements OnModuleInit {
    */
   async initializeInfraConfigTable() {
     try {
-      // Get all the 'names' of the properties from ENUM to be saved in the 'infra_config' table
-      const enumValues = Object.values(InfraConfigEnum);
-
       // Fetch the default values (value in .env) for configs to be saved in 'infra_config' table
       const infraConfigDefaultObjs = await getDefaultInfraConfigs();
-
-      // Cross-check if all the 'names' are listed in the default-values-list and ENUM at the same time
-      if (enumValues.length !== infraConfigDefaultObjs.length) {
-        throw new Error(INFRA_CONFIG_NOT_LISTED);
-      }
 
       // Eliminate the rows (from 'infraConfigDefaultObjs') that are already present in the database table
       const dbInfraConfigs = await this.prisma.infraConfig.findMany();

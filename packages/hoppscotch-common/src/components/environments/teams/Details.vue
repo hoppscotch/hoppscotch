a78eb2dee3cadd08847cd6ca39e551940d8af228
nivedin <nivedinp@gmail.com>
2024-02-09 20:47:01 +0530

    chore: update i18n texts

--- packages/hoppscotch-common/src/components/environments/teams/Details.vue
+++ packages/hoppscotch-common/src/components/environments/teams/Details.vue
@@ -229,7 +229,7 @@ const tabsData: ComputedRef<
     },
     {
       id: "secret",
-      label: t("environment.secret"),
+      label: t("environment.secrets"),
       emptyStateLabel: t("empty.secret_environments"),
       isSecret: true,
       variables: secretVars.value,

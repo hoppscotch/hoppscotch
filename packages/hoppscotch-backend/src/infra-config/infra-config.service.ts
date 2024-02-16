a99a21ed3979bb5242de9ff514a84e5a104bb389
Balu Babu <balub997@gmail.com>
2024-02-16 13:38:27 +0530

    feat: added analytics related infra-configs

--- packages/hoppscotch-backend/src/infra-config/infra-config.service.ts
+++ packages/hoppscotch-backend/src/infra-config/infra-config.service.ts
@@ -19,7 +19,7 @@ import {
 } from 'src/errors';
 import { throwErr, validateSMTPEmail, validateSMTPUrl } from 'src/utils';
 import { ConfigService } from '@nestjs/config';
-import { ServiceStatus, getConfiguredSSOProviders, stopApp } from './helper';
+import { ServiceStatus, generateAnalyticsUserId, getConfiguredSSOProviders, stopApp } from './helper';
 import { EnableAndDisableSSOArgs, InfraConfigArgs } from './input-args';
 import { AuthProvider } from 'src/auth/helper';
 
@@ -72,6 +72,14 @@ export class InfraConfigService implements OnModuleInit {
       {
         name: InfraConfigEnum.VITE_ALLOWED_AUTH_PROVIDERS,
         value: getConfiguredSSOProviders(),
+      },
+      {
+        name: InfraConfigEnum.ALLOW_ANALYTICS_COLLECTION,
+        value: true.toString(),
+      },
+      {
+        name: InfraConfigEnum.ANALYTICS_USER_ID,
+        value: generateAnalyticsUserId(),
       },
     ];
 

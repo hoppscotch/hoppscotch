272d7a28eb4381775bd18dccb39e20b1333a300c
Balu Babu <balub997@gmail.com>
2024-02-16 13:47:53 +0530

    chore: removed Cron decorator from capture method in posthog service file

--- packages/hoppscotch-backend/src/posthog/posthog.service.ts
+++ packages/hoppscotch-backend/src/posthog/posthog.service.ts
@@ -20,7 +20,6 @@ export class PosthogService {
   async onModuleInit() {
     if (this.configService.get('INFRA.ALLOW_ANALYTICS_COLLECTION') === 'true') {
       console.log('Initializing PostHog');
-      // Instantiate PostHog client only if env variable is true
       this.postHogClient = new PostHog(this.POSTHOG_API_KEY, {
         // ToDo: Check and change host if required.
         host: 'https://eu.posthog.com',
@@ -42,7 +41,6 @@ export class PosthogService {
   }
 
   // ToDo: Change expression to every week in the end
-  @Cron(CronExpression.EVERY_5_SECONDS)
   async capture() {
     if (!this.postHogClient) {
       throwErr(POSTHOG_CLIENT_NOT_INITIALIZED);

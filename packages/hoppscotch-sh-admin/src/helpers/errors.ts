70c4ef56993270eae793c4dbf4d8440a3e15d9ef
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2024-02-19 19:17:53 +0530

    feat: code for implementing newsletter

--- packages/hoppscotch-sh-admin/src/helpers/errors.ts
+++ packages/hoppscotch-sh-admin/src/helpers/errors.ts
@@ -7,3 +7,21 @@ export const UNAUTHORIZED = 'Unauthorized' as const;
 
 // Sometimes the backend returns Unauthorized error message as follows:
 export const GRAPHQL_UNAUTHORIZED = '[GraphQL] Unauthorized' as const;
+
+/**
+ * Email not found
+ */
+export const EMAIL_NOT_FOUND = 'email/not_found';
+
+/**
+ * Request Failed
+ */
+export const REQ_FAILED = 'request/failed';
+
+export const INVALID_EMAIL = 'invalid/email' as const;
+
+/**
+ * Subscriber already exists
+ * (NewsletterService)
+ */
+export const SUBSCRIBER_ALREADY_EXISTS = 'newsletter/subscriber_already_exists';

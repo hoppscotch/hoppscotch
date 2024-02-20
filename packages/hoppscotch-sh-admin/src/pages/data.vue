785c8c6ecbed9b8536a2c948665c706b65751009
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2024-02-20 20:31:00 +0530

    refactor: new i18n strings for data sharing and data component

--- packages/hoppscotch-sh-admin/src/pages/data.vue
+++ packages/hoppscotch-sh-admin/src/pages/data.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen">
     <div class="text-center">
-      <h2 class="text-lg">Welcome to</h2>
+      <h2 class="text-lg">{{ t('data_sharing.welcome') }}</h2>
       <img src="/assets/images/hoppscotch-title.svg" alt="" class="mt-2 w-56" />
     </div>
     <div
@@ -9,36 +9,40 @@
     >
       <div>
         <div class="flex flex-col">
-          <p class="text-lg font-bold text-white">Make Hoppscotch Better</p>
+          <p class="text-lg font-bold text-white">
+            {{ t('data_sharing.title') }}
+          </p>
           <p class="text-sm font-light">
-            Share anonymous data usage to improve Hoppscotch
+            {{ t('data_sharing.description') }}
           </p>
           <HoppSmartToggle
             :on="shareData"
             @change="shareData = !shareData"
             class="my-5 text-white w-min justify-start"
           >
-            Share data and make Hoppscotch better
+            {{ t('data_sharing.toggle_description') }}
           </HoppSmartToggle>
           <HoppButtonSecondary
             outline
             filled
             :icon="IconShieldQuestion"
-            label="See what is shared"
+            :label="t('data_sharing.see_shared')"
             to="http://docs.hoppscotch.io"
             class="w-min mx-auto mb-2"
           />
         </div>
 
         <div class="mt-5">
-          <p class="text-lg font-bold text-white">Stay in Touch</p>
+          <p class="text-lg font-bold text-white">
+            {{ t('newsletter.title') }}
+          </p>
           <p class="text-sm font-light">Get updates about our latest news</p>
           <HoppSmartToggle
             :on="shareEmail"
             @change="shareEmail = !shareEmail"
             class="my-5 text-white"
           >
-            Get updates about the latest at Hoppscotch
+            {{ t('newsletter.toggle_description') }}
           </HoppSmartToggle>
         </div>
       </div>
@@ -46,7 +50,7 @@
       <div class="flex flex-col w-48 mx-auto mt-5">
         <HoppButtonPrimary
           :icon="IconLogIn"
-          label="Continue to Dashboard"
+          :label="t('app.continue_to_dashboard')"
           @click="submitSelection"
         />
         <a href="http://docs.hoppscotch.io">
@@ -54,7 +58,7 @@
             outline
             filled
             :icon="IconBookOpenText"
-            label="Read Documentation"
+            :label="t('app.read_documentation')"
             class="mt-3 px-5"
           />
         </a>
@@ -67,6 +71,7 @@
 import { useMutation } from '@urql/vue';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { useI18n } from '~/composables/i18n';
 import { useToast } from '~/composables/toast';
 import { auth } from '~/helpers/auth';
 import {
@@ -78,6 +83,7 @@ import IconBookOpenText from '~icons/lucide/book-open-text';
 import IconLogIn from '~icons/lucide/log-in';
 import IconShieldQuestion from '~icons/lucide/shield-question';
 
+const t = useI18n();
 const toast = useToast();
 const router = useRouter();
 const user = auth.getCurrentUser();

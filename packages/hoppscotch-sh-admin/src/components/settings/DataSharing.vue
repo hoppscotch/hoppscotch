55293a038207e8265ecc1a4ea652316f3f686ed1
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2024-02-20 17:54:02 +0530

    refactor: new i18n string for data sharing component

--- packages/hoppscotch-sh-admin/src/components/settings/DataSharing.vue
+++ packages/hoppscotch-sh-admin/src/components/settings/DataSharing.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="md:grid md:grid-cols-3 md:gap-4 border-divider border-b py-8">
     <div class="px-8 md:col-span-1">
-      <h3 class="heading">Data Sharing</h3>
+      <h3 class="heading">{{ t('configs.dataSharing.title') }}</h3>
       <p class="my-1 text-secondaryLight">
-        Share anonymous data usage to improve Hoppscotch
+        {{ t('configs.dataSharing.description') }}
       </p>
     </div>
 
     <div class="mt-5 mx-8 md:col-span-2">
       <section v-if="dataSharingConfigs">
-        <h4 class="font-semibold text-secondaryDark">Data Sharing</h4>
+        <h4 class="font-semibold text-secondaryDark">
+          {{ t('configs.dataSharing.title') }}
+        </h4>
 
         <div class="space-y-4 py-4">
           <div class="flex items-center">
@@ -17,24 +19,16 @@
               :on="dataSharingConfigs.enabled"
               @change="dataSharingConfigs.enabled = !dataSharingConfigs.enabled"
             >
-              Share data and make Hoppscotch better
+              {{ t('configs.dataSharing.toggle_description') }}
             </HoppSmartToggle>
           </div>
-
-          <!-- <div class="ml-12">
-            <div class="mt-5">
-              <label>
-                {{ t('configs.site_protection.note') }}
-              </label>
-            </div>
-          </div> -->
         </div>
 
         <HoppButtonSecondary
           outline
           filled
           :icon="IconShieldQuestion"
-          label="See what is shared"
+          :label="t('configs.dataSharing.see_shared')"
           to="http://docs.hoppscotch.io"
           class="w-min my-2"
         />

832bcffe3e3a68362abfb99bccd5f80fcd4f8569
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2024-02-23 12:33:35 +0530

    feat: new setup component that handles all things setup related

---
+++ packages/hoppscotch-sh-admin/src/pages/setup.vue
@@ -0,0 +1,25 @@
+<template>
+  <SetupDataSharing @onSetupComplete="dataAnalyticsSetup" />
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { auth } from '~/helpers/auth';
+
+const router = useRouter();
+const dataAnalyticsSetup = async (status: boolean) => {
+  if (status) {
+    const result = await auth.updateFirstTimeInfraSetupStatus();
+    if (result) {
+      console.log('Data sharing setup complete');
+
+      router.push('/dashboard');
+    }
+  }
+};
+</script>
+
+<route lang="yaml">
+meta:
+  layout: empty
+</route>

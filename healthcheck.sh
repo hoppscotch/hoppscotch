6c1d8d16c076281bf59388903845d9b5715c49d7
Balu Babu <balub997@gmail.com>
2024-03-21 13:37:25 +0530

    chore: added logic to make script with with subpath

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,13 +2,17 @@
 
 curlCheck() {
   if ! curl -s --head "$1" | head -n 1 | grep -q "HTTP/1.[01] [23].."; then
-    echo "URL request failed!"
+    echo "$1 URL request failed!"
     exit 1
   else
     echo "URL request succeeded!"
   fi
 }
 
-curlCheck "http://localhost:3000"
-curlCheck "http://localhost:3100"
-curlCheck "http://localhost:3170/ping"
+if [ "$ENABLE_SUBPATH_BASED_ACCESS" = "true" ]; then
+  curlCheck "http://localhost:80/backend/ping"
+else
+  curlCheck "http://localhost:3000"
+  curlCheck "http://localhost:3100"
+  curlCheck "http://localhost:3170/ping"
+fi

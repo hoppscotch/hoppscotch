27d58c0f48de738797d07588727b8dae19287b56
jamesgeorge007 <jamesgeorge998001@gmail.com>
2024-04-11 17:54:37 +0530

    fix: condition to spawn child process

--- packages/hoppscotch-cli/bin/hopp.js
+++ packages/hoppscotch-cli/bin/hopp.js
@@ -7,21 +7,11 @@ import { cli } from "../dist/index.js";
 import { cloneDeep } from "lodash-es"
 import { spawnSync } from "child_process";
 
-/**
- * @template T
- * @param {T[]} arr
- * @param {T} val
- * @retunrns {boolean}
- */
-function arrayContains(arr, val) {
-  return arr.indexOf(val) > -1;
-}
-
 const nodeVersion = parseInt(process.versions.node.split(".")[0]);
 
 // As per isolated-vm documentation, we need to supply `--no-node-snapshot` for node >= 20
 // src: https://github.com/laverdet/isolated-vm?tab=readme-ov-file#requirements
-if (nodeVersion >= 20 && arrayContains(process.execArgv, "--no-node-snapshot")) {
+if (nodeVersion >= 20 && !process.execArgv.includes("--no-node-snapshot")) {
   const argCopy = cloneDeep(process.argv);
 
   // Replace first argument with --no-node-snapshot

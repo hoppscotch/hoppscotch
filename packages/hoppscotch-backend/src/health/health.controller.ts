145465b35850f1c98f99ec9c34ef3ea1be5cb888
Balu Babu <balub997@gmail.com>
2024-05-07 14:23:15 +0530

    feat: added healthcheck for database with prisma

--- packages/hoppscotch-backend/src/health/health.controller.ts
+++ packages/hoppscotch-backend/src/health/health.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private prismaHealth: PrismaHealthIndicator,
+    private prisma: PrismaService,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      async () => this.prismaHealth.pingCheck('prisma', this.prisma),
+    ]);
+  }
+}

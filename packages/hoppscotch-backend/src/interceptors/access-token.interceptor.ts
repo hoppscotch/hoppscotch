a15366d2da3015150eba544cbaf630b720eeac58
Balu Babu <balub997@gmail.com>
2024-05-21 01:10:28 +0530

    chore: modified access-token interceptor

--- packages/hoppscotch-backend/src/interceptors/access-token.interceptor.ts
+++ packages/hoppscotch-backend/src/interceptors/access-token.interceptor.ts
@@ -5,7 +5,7 @@ import {
   NestInterceptor,
   UnauthorizedException,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { AccessTokenService } from 'src/access-token/access-token.service';
 import * as E from 'fp-ts/Either';
 
@@ -13,10 +13,7 @@ import * as E from 'fp-ts/Either';
 export class AccessTokenInterceptor implements NestInterceptor {
   constructor(private readonly accessTokenService: AccessTokenService) {}
 
-  async intercept(
-    context: ExecutionContext,
-    handler: CallHandler,
-  ): Promise<Observable<any>> {
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     const req = context.switchToHttp().getRequest();
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1];
@@ -24,11 +21,15 @@ export class AccessTokenInterceptor implements NestInterceptor {
       throw new UnauthorizedException();
     }
 
-    const userAccessToken = await this.accessTokenService.updateLastUsedforPAT(
-      token,
-    );
-    if (E.isLeft(userAccessToken)) throw new UnauthorizedException();
+    return handler.handle().pipe(
+      map(async (data) => {
+        const userAccessToken =
+          await this.accessTokenService.updateLastUsedforPAT(token);
+        if (E.isLeft(userAccessToken)) throw new UnauthorizedException();
 
-    return handler.handle();
+        console.log('After...');
+        return data;
+      }),
+    );
   }
 }

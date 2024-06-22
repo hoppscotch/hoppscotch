3106ca136042a2ed8512d1fda03ec509caf6344d
jamesgeorge007 <25279263+jamesgeorge007@users.noreply.github.com>
2024-06-22 18:56:42 +0530

    feat: add `Environment Properties` modal
    Add a new `properties` action under the context menu for environments.

---
+++ packages/hoppscotch-common/src/components/environments/Properties.vue
@@ -0,0 +1,108 @@
+<template>
+  <HoppSmartModal
+    dialog
+    :full-width-body="true"
+    title="Environment Properties"
+    @close="hideModal"
+  >
+    <template #body>
+      <HoppSmartTabs
+        v-model="activeTab"
+        styles="sticky overflow-x-auto flex-shrink-0 bg-primary top-0 z-10 !-py-4"
+        render-inactive-tabs
+      >
+        <HoppSmartTab id="details" label="Details">
+          <div
+            class="flex flex-shrink-0 items-center justify-between border-b border-dividerLight bg-primary pl-4"
+          >
+            <span>Environment ID</span>
+
+            <!-- TODO: Make it point to the section about accessing environments via the ID -->
+            <HoppButtonSecondary
+              v-tippy="{ theme: 'tooltip' }"
+              to="https://docs.hoppscotch.io/documentation/clients/cli"
+              blank
+              :title="t('app.wiki')"
+              :icon="IconHelpCircle"
+            />
+          </div>
+
+          <div class="p-4">
+            <div
+              class="flex items-center justify-between py-2 px-4 rounded-md bg-primaryLight"
+            >
+              <div class="text-secondaryDark">
+                {{ environmentID }}
+              </div>
+
+              <HoppButtonSecondary
+                filled
+                :icon="copyIcon"
+                @click="copyEnvironmentID"
+              />
+            </div>
+          </div>
+
+          <div
+            class="bg-bannerInfo px-4 py-2 flex items-center sticky bottom-0"
+          >
+            <icon-lucide-info class="svg-icons mr-2" />
+            This environment ID will be used for CLI collection runner for
+            Hoppscotch.
+          </div>
+        </HoppSmartTab>
+      </HoppSmartTabs>
+    </template>
+
+    <template #footer>
+      <HoppButtonPrimary
+        :label="t('action.close')"
+        outline
+        filled
+        @click="hideModal"
+      />
+    </template>
+  </HoppSmartModal>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "@composables/i18n"
+import { refAutoReset, useVModel } from "@vueuse/core"
+import { useToast } from "~/composables/toast"
+
+import { copyToClipboard } from "~/helpers/utils/clipboard"
+import IconCheck from "~icons/lucide/check"
+import IconCopy from "~icons/lucide/copy"
+import IconHelpCircle from "~icons/lucide/help-circle"
+
+const t = useI18n()
+const toast = useToast()
+
+const props = defineProps<{
+  modelValue: string
+  environmentID: string
+}>()
+
+const emit = defineEmits<{
+  (e: "hide-modal"): void
+  (e: "update:modelValue"): void
+}>()
+
+const activeTab = useVModel(props, "modelValue", emit)
+
+const copyIcon = refAutoReset<typeof IconCopy | typeof IconCheck>(
+  IconCopy,
+  1000
+)
+
+const hideModal = () => {
+  emit("hide-modal")
+}
+
+const copyEnvironmentID = () => {
+  copyToClipboard(props.environmentID)
+  copyIcon.value = IconCheck
+
+  toast.success(`${t("state.copied_to_clipboard")}`)
+}
+</script>

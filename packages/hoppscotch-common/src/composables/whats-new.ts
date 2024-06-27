54d43cbe14dea416f363903101b680e61e3063ee
jamesgeorge007 <25279263+jamesgeorge007@users.noreply.github.com>
2024-06-27 14:06:02 +0530

    chore: cleanup

--- packages/hoppscotch-common/src/composables/whats-new.ts
+++ packages/hoppscotch-common/src/composables/whats-new.ts
@@ -1,49 +1,74 @@
 import { toast as sonner } from "@hoppscotch/ui"
+import * as E from "fp-ts/Either"
 import { markRaw } from "vue"
+
 import WhatsNewDialog from "~/components/app/WhatsNewDialog.vue"
 import { getService } from "~/modules/dioc"
-import { PersistenceService } from "~/services/persistence"
-import { version } from "./../../package.json"
 import { InterceptorService } from "~/services/interceptor.service"
-import * as E from "fp-ts/Either"
+import { PersistenceService } from "~/services/persistence"
+import { version as hoppscotchCommonPkgVersion } from "./../../package.json"
 
 export const useWhatsNewDialog = async function () {
   const persistenceService = getService(PersistenceService)
 
-  const oldVersion = persistenceService.getLocalConfig("hopp_v")
-  if (oldVersion === version) return
+  const versionFromLocalStorage = persistenceService.getLocalConfig("hopp_v")
 
-  const latestMajorVersion = version.split(".").slice(0, 2).join(".")
+  // Set new entry `hopp_v` under `localStorage` if not present
+  if (!versionFromLocalStorage) {
+    persistenceService.setLocalConfig("hopp_v", hoppscotchCommonPkgVersion)
+    return
+  }
+
+  // Already on the latest version
+  if (versionFromLocalStorage === hoppscotchCommonPkgVersion) {
+    return
+  }
+
+  const getMajorVersion = (v: string) => v.split(".").slice(0, 2).join(".")
+
+  const majorVersionFromLocalStorage = getMajorVersion(versionFromLocalStorage)
+  const hoppscotchCommonPkgMajorVersion = getMajorVersion(
+    hoppscotchCommonPkgVersion
+  )
 
   // Skipping the minor version update. e.g. 2024.1.0 -> 2024.1.1
   // Checking major version update. e.g. 2024.1 -> 2024.2
 
-  const isUpdatedToLatestMajorVersion =
-    oldVersion?.split(".").slice(0, 2).join(".") != latestMajorVersion // If the major version is different from the old version
-
-  if (isUpdatedToLatestMajorVersion) {
-    const notesUrl = await getReleaseNotes(latestMajorVersion)
+  // Show the release notes during a major version update
+  if (majorVersionFromLocalStorage !== hoppscotchCommonPkgMajorVersion) {
+    const notesUrl = await getReleaseNotes(hoppscotchCommonPkgMajorVersion)
     if (notesUrl) {
       sonner.custom(markRaw(WhatsNewDialog), {
         componentProps: {
           notesUrl,
-          version,
+          version: hoppscotchCommonPkgVersion,
         },
       })
     }
   }
 
-  persistenceService.setLocalConfig("hopp_v", version)
+  persistenceService.setLocalConfig("hopp_v", hoppscotchCommonPkgVersion)
 }
 
-async function getReleaseNotes(v: string) {
+async function getReleaseNotes(v: string): Promise<string | undefined> {
   const interceptorService = getService(InterceptorService)
 
   const res = await interceptorService.runRequest({
     url: `https://releases.hoppscotch.com/releases/${v}.json`,
   }).response
 
-  if (E.isLeft(res) || res.right.status !== 200) return
+  if (E.isLeft(res) || res.right.status !== 200) {
+    return
+  }
 
-  return (res.right.data as any).release_notes
+  if (!(res.right.data instanceof ArrayBuffer)) {
+    return
+  }
+
+  try {
+    const data = JSON.parse(new TextDecoder().decode(res.right.data))
+    return data.release_notes
+  } catch (err) {
+    console.error(err)
+  }
 }

220e860eb2c537591b553c806d5848587ca561f6
Anwarul Islam <anwaarulislaam@gmail.com>
2024-06-27 12:49:25 +0600

    feat: used interceptor for fetching release notes

--- packages/hoppscotch-common/src/composables/whats-new.ts
+++ packages/hoppscotch-common/src/composables/whats-new.ts
@@ -4,14 +4,16 @@ import WhatsNewDialog from "~/components/app/WhatsNewDialog.vue"
 import { getService } from "~/modules/dioc"
 import { PersistenceService } from "~/services/persistence"
 import { version } from "./../../package.json"
+import { InterceptorService } from "~/services/interceptor.service"
+import * as E from "fp-ts/Either"
 
 export const useWhatsNewDialog = async function () {
   const persistenceService = getService(PersistenceService)
 
   const oldVersion = persistenceService.getLocalConfig("hopp_v")
-  const latestMajorVersion = version.split(".").slice(0, 2).join(".")
-
   if (oldVersion === version) return
+
+  const latestMajorVersion = version.split(".").slice(0, 2).join(".")
 
   // Skipping the minor version update. e.g. 2024.1.0 -> 2024.1.1
   // Checking major version update. e.g. 2024.1 -> 2024.2
@@ -35,7 +37,13 @@ export const useWhatsNewDialog = async function () {
 }
 
 async function getReleaseNotes(v: string) {
-  const res = await fetch(`https://releases.hoppscotch.com/releases/${v}.json`)
-  const { release_notes } = await res.json()
-  return release_notes
+  const interceptorService = getService(InterceptorService)
+
+  const res = await interceptorService.runRequest({
+    url: `https://releases.hoppscotch.com/releases/${v}.json`,
+  }).response
+
+  if (E.isLeft(res) || res.right.status !== 200) return
+
+  return (res.right.data as any).release_notes
 }

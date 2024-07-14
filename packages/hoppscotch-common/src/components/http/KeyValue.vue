98a5e5cf6ec375d45978702469965fafb86c7c20
Anwarul Islam <anwaarulislaam@gmail.com>
2024-07-14 20:23:14 +0600

    feat: key-value component added for reuse

---
+++ packages/hoppscotch-common/src/components/http/KeyValue.vue
@@ -0,0 +1,140 @@
+<template>
+  <div
+    class="draggable-content group flex divide-x divide-dividerLight border-b border-dividerLight"
+  >
+    <span>
+      <HoppButtonSecondary
+        v-tippy="{
+          theme: 'tooltip',
+          delay: [500, 20],
+          content: index !== total - 1 ? t('action.drag_to_reorder') : null,
+        }"
+        :icon="IconGripVertical"
+        class="opacity-0"
+        :class="{
+          'draggable-handle cursor-grab group-hover:opacity-100':
+            index !== total - 1,
+        }"
+        tabindex="-1"
+      />
+    </span>
+    <SmartEnvInput
+      :model-value="name"
+      @update:model-value="emit('update:name', $event)"
+      :placeholder="t('count.key')"
+      :auto-complete-env="true"
+      :envs="envs"
+      @change="
+        updateEntity(index, {
+          id: entityId,
+          key: $event,
+          value: value,
+          active: entityActive,
+        })
+      "
+    />
+    <SmartEnvInput
+      :model-value="value"
+      @update:model-value="emit('update:value', $event)"
+      :placeholder="t('count.value')"
+      :auto-complete-env="true"
+      :envs="envs"
+      @change="
+        updateEntity(index, {
+          id: entityId,
+          key: name,
+          value: $event,
+          active: entityActive,
+        })
+      "
+    />
+
+    <input
+      :model-value="description"
+      @update:model-value="emit('update:description', $event.target.value)"
+      :placeholder="t('count.description')"
+      class="flex flex-1 bg-transparent px-4"
+      type="text"
+    />
+    <span>
+      <HoppButtonSecondary
+        v-tippy="{ theme: 'tooltip' }"
+        :title="
+          isActive
+            ? entityActive
+              ? t('action.turn_off')
+              : t('action.turn_on')
+            : t('action.turn_off')
+        "
+        :icon="
+          isActive
+            ? entityActive
+              ? IconCheckCircle
+              : IconCircle
+            : IconCheckCircle
+        "
+        color="green"
+        @click="
+          updateEntity(index, {
+            id: entityId,
+            key: name,
+            value: value,
+            active: isActive ? !entityActive : false,
+          })
+        "
+      />
+    </span>
+    <span>
+      <HoppButtonSecondary
+        v-tippy="{ theme: 'tooltip' }"
+        :title="t('action.remove')"
+        :icon="IconTrash"
+        color="red"
+        @click="deleteEntity(index)"
+      />
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import IconGripVertical from "~icons/lucide/grip-vertical"
+import IconCheckCircle from "~icons/lucide/check-circle"
+import IconCircle from "~icons/lucide/circle"
+import IconTrash from "~icons/lucide/trash"
+import { useI18n } from "~/composables/i18n"
+import { AggregateEnvironment } from "~/newstore/environments"
+
+const t = useI18n()
+
+const props = defineProps<{
+  total: number
+  index: number
+  entityId: number
+  isActive: boolean
+  entityActive: boolean
+  name: string
+  value: string
+  description?: string
+  envs?: AggregateEnvironment[]
+}>()
+
+const emit = defineEmits<{
+  (e: "update:name", value: string): void
+  (e: "update:value", value: string): void
+  (e: "update:description", value: string): void
+  (e: "deleteEntity", value: number): void
+  (e: "updateEntity", { index, payload }: { index: number; payload: any }): void
+}>()
+
+function updateEntity(index: number, payload: any) {
+  console.log(payload)
+  emit("updateEntity", {
+    index,
+    payload,
+  })
+}
+
+function deleteEntity(index: number) {
+  emit("deleteEntity", index)
+}
+</script>

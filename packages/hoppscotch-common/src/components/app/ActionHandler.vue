6ddc142a655978c6f63562ef3b5285fcafa19bf3
nivedin <nivedinp@gmail.com>
2024-08-29 19:40:36 +0530

    chore: minor code refactor

--- packages/hoppscotch-common/src/components/app/ActionHandler.vue
+++ packages/hoppscotch-common/src/components/app/ActionHandler.vue
@@ -2,32 +2,17 @@
   <AppShortcuts :show="showShortcuts" @close="showShortcuts = false" />
   <AppShare :show="showShare" @hide-modal="showShare = false" />
   <FirebaseLogin v-if="showLogin" @hide-modal="showLogin = false" />
-  <HoppSmartSlideOver
-    :show="isDrawerOpen"
-    :title="t('response.data_schema')"
-    @close="closeDrawer()"
-  >
-    <template #content>
-      <HttpResponseInterface :show="isDrawerOpen" />
-    </template>
-  </HoppSmartSlideOver>
+  <HttpResponseInterface :show="isDrawerOpen" @close="isDrawerOpen = false" />
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
-import { useI18n } from "~/composables/i18n"
 import { defineActionHandler } from "~/helpers/actions"
-
-const t = useI18n()
 
 const showShortcuts = ref(false)
 const showShare = ref(false)
 const showLogin = ref(false)
 const isDrawerOpen = ref(false)
-
-const closeDrawer = () => {
-  isDrawerOpen.value = false
-}
 
 defineActionHandler("flyouts.keybinds.toggle", () => {
   showShortcuts.value = !showShortcuts.value

523183e364769d9e502d4977af94bdcf0f9674c1
Anwarul Islam <anwaarulislaam@gmail.com>
2024-09-11 18:35:57 +0600

    feat: digest auth added

--- packages/hoppscotch-data/src/rest/v/8.ts
+++ packages/hoppscotch-data/src/rest/v/8.ts
@@ -64,6 +64,8 @@ export const HoppRESTAuthDigest = z.object({
   disableRetry: z.boolean().catch(false),
 })
 
+export type HoppRESTAuthDigest = z.infer<typeof HoppRESTAuthDigest>
+
 export const HoppRESTAuth = z
   .discriminatedUnion("authType", [
     HoppRESTAuthNone,
@@ -73,6 +75,7 @@ export const HoppRESTAuth = z
     HoppRESTAuthOAuth2,
     HoppRESTAuthAPIKey,
     HoppRESTAuthAWSSignature,
+    HoppRESTAuthDigest,
   ])
   .and(
     z.object({

f88bb5d01f67f3c4b247f9db09f8b6bc77dc6c2c
Pranay <pranaypandeyofficial@gmail.com>
2024-10-09 02:25:31 +0530

    feat: add button to export test results as json

--- packages/hoppscotch-common/src/components/http/TestResult.vue
+++ packages/hoppscotch-common/src/components/http/TestResult.vue
@@ -14,12 +14,20 @@
         <label class="truncate font-semibold text-secondaryLight">
           {{ t("test.report") }}
         </label>
-        <HoppButtonSecondary
-          v-tippy="{ theme: 'tooltip' }"
-          :title="t('action.clear')"
-          :icon="IconTrash2"
-          @click="clearContent()"
-        />
+        <div>
+          <HoppButtonSecondary
+            v-tippy="{ theme: 'tooltip' }"
+            :title="t('export.title')"
+            :icon="IconShare"
+            @click="share"
+          />
+          <HoppButtonSecondary
+            v-tippy="{ theme: 'tooltip' }"
+            :title="t('action.clear')"
+            :icon="IconTrash2"
+            @click="clearContent()"
+          />
+        </div>
       </div>
       <div class="divide-y-4 divide-dividerLight border-b border-dividerLight">
         <div v-if="haveEnvVariables" class="flex flex-col">
@@ -214,11 +222,13 @@ import {
   selectedEnvironmentIndex$,
   setSelectedEnvironmentIndex,
 } from "~/newstore/environments"
+import { ExportTestResults } from "~/helpers/import-export/export/testResults"
 
 import IconCheck from "~icons/lucide/check"
 import IconExternalLink from "~icons/lucide/external-link"
 import IconTrash2 from "~icons/lucide/trash-2"
 import IconClose from "~icons/lucide/x"
+import IconShare from "~icons/lucide/share"
 
 import { GlobalEnvironment } from "@hoppscotch/data"
 import { useVModel } from "@vueuse/core"
@@ -306,5 +316,10 @@ const addEnvToGlobal = () => {
     variables: testResults.value.envDiff.selected.additions,
     isSecret: false,
   })
+}
+
+const share = () => {
+  if (!testResults.value) return
+  ExportTestResults(testResults.value)
 }
 </script>

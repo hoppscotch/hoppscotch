9a28757669e2c0932a4af23bb3a8eafaa846dc12
Oliver Ross <u7683699@anu.edu.au>
2024-10-26 18:10:54 +1100

    copied logout into switch vue

--- packages/hoppscotch-common/src/components/firebase/Switch.vue
+++ packages/hoppscotch-common/src/components/firebase/Switch.vue
@@ -1,3 +1,68 @@
 <template>
-    <h1>Testupdate</h1>
-</template>
+    <div class="flex" @click="openLogoutModal()">
+      <HoppSmartItem
+        ref="logoutItem"
+        :icon="IconLogOut"
+        :label="`${t('auth.logout')}`"
+        :outline="outline"
+        :shortcut="shortcut"
+        @click="openLogoutModal()"
+      />
+      <HoppSmartConfirmModal
+        :show="confirmLogout"
+        :title="`${t('confirm.logout')}`"
+        @hide-modal="confirmLogout = false"
+        @resolve="logout"
+      />
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from "vue"
+  import IconLogOut from "~icons/lucide/log-out"
+  import { useToast } from "@composables/toast"
+  import { useI18n } from "@composables/i18n"
+  import { platform } from "~/platform"
+  import { defineActionHandler } from "~/helpers/actions"
+  
+  defineProps({
+    outline: {
+      type: Boolean,
+      default: false,
+    },
+    shortcut: {
+      type: Array,
+      default: () => [],
+    },
+  })
+  
+  const emit = defineEmits<{
+    (e: "confirm-logout"): void
+  }>()
+  
+  const confirmLogout = ref(false)
+  const logoutItem = ref<HTMLButtonElement>()
+  
+  const toast = useToast()
+  const t = useI18n()
+  
+  const logout = async () => {
+    try {
+      await platform.auth.signOutUser()
+      toast.success(`${t("auth.logged_out")}`)
+    } catch (e) {
+      console.error(e)
+      toast.error(`${t("error.something_went_wrong")}`)
+    }
+  }
+  
+  const openLogoutModal = () => {
+    emit("confirm-logout")
+    confirmLogout.value = true
+  }
+  
+  defineActionHandler("user.logout", () => {
+    openLogoutModal()
+  })
+  </script>
+  

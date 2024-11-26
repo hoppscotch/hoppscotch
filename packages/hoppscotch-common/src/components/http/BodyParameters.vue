5b88133d49850769a96fdf7257c93b6e35435bbd
amk-dev <akash.k.mohan98@gmail.com>
2024-11-27 04:53:43 +0530

    feat: add autocomplete for content type fields

--- packages/hoppscotch-common/src/components/http/BodyParameters.vue
+++ packages/hoppscotch-common/src/components/http/BodyParameters.vue
@@ -121,6 +121,7 @@
                 entry.contentType ? entry.contentType : `Auto (Content Type)`
               "
               :auto-complete-env="true"
+              :auto-complete-source="autoCompleteContenTypes"
               :envs="envs"
               @change="
                 updateBodyParam(index, {
@@ -254,6 +255,63 @@ const idTicker = ref(0)
 const deletionToast = ref<{ goAway: (delay: number) => void } | null>(null)
 
 const bodyParams = pluckRef(body, "body")
+
+const autoCompleteContenTypes = [
+  "application/atom+xml",
+  "application/ecmascript",
+  "application/json",
+  "application/vnd.api+json",
+  "application/javascript",
+  "application/octet-stream",
+  "application/ogg",
+  "application/pdf",
+  "application/postscript",
+  "application/rdf+xml",
+  "application/rss+xml",
+  "application/soap+xml",
+  "application/font-woff",
+  "application/x-yaml",
+  "application/xhtml+xml",
+  "application/xml",
+  "application/xml-dtd",
+  "application/xop+xml",
+  "application/zip",
+  "application/gzip",
+  "application/graphql",
+  "application/x-www-form-urlencoded",
+  "audio/basic",
+  "audio/L24",
+  "audio/mp4",
+  "audio/mpeg",
+  "audio/ogg",
+  "audio/vorbis",
+  "audio/vnd.rn-realaudio",
+  "audio/vnd.wave",
+  "audio/webm",
+  "image/gif",
+  "image/jpeg",
+  "image/pjpeg",
+  "image/png",
+  "image/svg+xml",
+  "image/tiff",
+  "message/http",
+  "message/imdn+xml",
+  "message/partial",
+  "message/rfc822",
+  "multipart/mixed",
+  "multipart/alternative",
+  "multipart/related",
+  "multipart/form-data",
+  "multipart/signed",
+  "multipart/encrypted",
+  "text/cmd",
+  "text/css",
+  "text/csv",
+  "text/html",
+  "text/plain",
+  "text/vcard",
+  "text/xml",
+]
 
 // The UI representation of the parameters list (has the empty end param)
 const workingParams = ref<WorkingFormDataKeyValue[]>([

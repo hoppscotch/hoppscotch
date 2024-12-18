a8aa50c329a2dde36c1d1e4c7dea130e8f165cd7
curiouscorrelation <curiouscorrelation@gmail.com>
2024-12-18 23:31:56 +0530

    fix(ui): mismatched extension button size/position

--- packages/hoppscotch-common/src/components/settings/Extension.vue
+++ packages/hoppscotch-common/src/components/settings/Extension.vue
@@ -12,29 +12,27 @@
       {{ t("settings.extension_ver_not_reported") }}
     </span>
   </div>
-  <div class="flex flex-col space-y-2 py-4">
-    <span>
-      <HoppSmartItem
-        to="https://chrome.google.com/webstore/detail/hoppscotch-browser-extens/amknoiejhlmhancpahfcfcfhllgkpbld"
-        blank
-        :icon="IconChrome"
-        label="Chrome"
-        :info-icon="hasChromeExtInstalled ? IconCheckCircle : null"
-        :active-info-icon="hasChromeExtInstalled"
-        outline
-      />
-    </span>
-    <span>
-      <HoppSmartItem
-        to="https://addons.mozilla.org/en-US/firefox/addon/hoppscotch"
-        blank
-        :icon="IconFirefox"
-        label="Firefox"
-        :info-icon="hasFirefoxExtInstalled ? IconCheckCircle : null"
-        :active-info-icon="hasFirefoxExtInstalled"
-        outline
-      />
-    </span>
+  <div class="flex gap-2 py-4 w-fit">
+    <HoppSmartItem
+      to="https://chrome.google.com/webstore/detail/hoppscotch-browser-extens/amknoiejhlmhancpahfcfcfhllgkpbld"
+      blank
+      :icon="IconChrome"
+      label="Chrome"
+      :info-icon="hasChromeExtInstalled ? IconCheckCircle : null"
+      :active-info-icon="hasChromeExtInstalled"
+      outline
+      class="w-28"
+    />
+    <HoppSmartItem
+      to="https://addons.mozilla.org/en-US/firefox/addon/hoppscotch"
+      blank
+      :icon="IconFirefox"
+      label="Firefox"
+      :info-icon="hasFirefoxExtInstalled ? IconCheckCircle : null"
+      :active-info-icon="hasFirefoxExtInstalled"
+      outline
+      class="w-28"
+    />
   </div>
 </template>
 

387de1e64c76d76dbda933007aa05b4f41c65dd8
Anwarul Islam <anwaarulislaam@gmail.com>
2025-01-27 06:36:23 +0600

    chore: clean up

--- packages/hoppscotch-common/src/components/graphql/Arguments.vue
+++ packages/hoppscotch-common/src/components/graphql/Arguments.vue
@@ -1,57 +1,20 @@
 <template>
   <template v-if="'args' in field">
-    <GraphqlExplorerSection v-if="args.length > 0" title="Arguments">
+    <GraphqlExplorerSection v-if="field.args.length > 0" title="Arguments">
       <GraphqlArgument
-        v-for="arg in args"
+        v-for="arg in field.args"
         :key="arg.name"
         :arg="arg"
         :show-add-button="true"
       />
     </GraphqlExplorerSection>
-
-    <template v-if="deprecatedArgs.length > 0">
-      <GraphqlExplorerSection
-        v-if="showDeprecated || args.length === 0"
-        title="Deprecated Arguments"
-      >
-        <GraphqlArgument
-          v-for="arg in deprecatedArgs"
-          :key="arg.name"
-          :arg="arg"
-          :show-add-button="true"
-        />
-      </GraphqlExplorerSection>
-
-      <button v-else type="button" @click="handleShowDeprecated">
-        Show Deprecated Arguments
-      </button>
-    </template>
   </template>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue"
 import { GraphQLField } from "graphql"
 
-const props = defineProps<{
+defineProps<{
   field: GraphQLField<any, any>
 }>()
-
-const showDeprecated = ref(false)
-
-const handleShowDeprecated = () => {
-  showDeprecated.value = true
-}
-
-const args = computed(() =>
-  props.field.args
-    ? props.field.args.filter((argument) => !argument.deprecationReason)
-    : []
-)
-
-const deprecatedArgs = computed(() =>
-  props.field.args
-    ? props.field.args.filter((argument) => argument.deprecationReason)
-    : []
-)
 </script>

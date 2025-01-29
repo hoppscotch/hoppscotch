3e3459ab7765cebc4615e70ea404c7a8a408bcac
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2025-01-28 19:56:44 -0600

    feat; new test file for history ui provider

--- packages/hoppscotch-common/src/services/history-ui-provider.service.ts
+++ packages/hoppscotch-common/src/services/history-ui-provider.service.ts
@@ -4,6 +4,9 @@ import { getI18n } from "~/modules/i18n"
 
 type HistoryUIProviderTitle = (t: ReturnType<typeof getI18n>) => string
 
+/**
+ * This service is used to provide custom UI items for the history section.
+ */
 export class HistoryUIProviderService extends Service {
   public static readonly ID = "HISTORY_UI_PROVIDER_SERVICE"
 

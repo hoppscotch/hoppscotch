b881f5985d3cbe1f6d4f250c82e9c56b7cd8fef5
jamesgeorge007 <25279263+jamesgeorge007@users.noreply.github.com>
2025-02-17 15:02:54 +0530

    refactor: introduce UI platform definitions for custom business logic

--- packages/hoppscotch-common/src/components/embeds/index.vue
+++ packages/hoppscotch-common/src/components/embeds/index.vue
@@ -1,60 +1,9 @@
 <template>
-  <div
-    v-if="pageLoading"
-    class="flex flex-1 flex-col items-center justify-center p-4"
-  >
-    <HoppSmartSpinner class="mb-4" />
-  </div>
-
-  <!-- Shortcodes are behind login on subdomain based cloud instances -->
-  <template
-    v-else-if="
-      platform.organization && !platform.organization.isDefaultCloudInstance
-    "
-  >
-    <div
-      v-if="loadingCurrentUser"
-      class="flex flex-1 flex-col items-center justify-center p-4"
-    >
-      <HoppSmartSpinner class="mb-4" />
-    </div>
-
-    <HoppSmartPlaceholder
-      v-else-if="currentUser === null"
-      :src="`/images/states/${colorMode.value}/login.svg`"
-      :alt="t('empty.shared_requests_logout')"
-      :text="t('empty.shared_requests_logout')"
-    >
-      <template #body>
-        <HoppButtonPrimary :label="t('auth.login')" @click="showLogin = true" />
-      </template>
-    </HoppSmartPlaceholder>
-
-    <AppPaneLayout v-else layout-id="embed-primary" :is-embed="true">
-      <template #primary>
-        <EmbedsHeader :shared-request-u-r-l="sharedRequestURL" />
-        <div class="flex flex-col flex-1">
-          <EmbedsRequest
-            :model-tab="modelTab"
-            :shared-request-u-r-l="sharedRequestURL"
-          />
-          <div class="flex flex-col flex-1">
-            <HttpRequestOptions
-              v-model="tab.document.request"
-              v-model:option-tab="selectedOptionTab"
-              :properties="properties"
-              :envs="tabRequestVariables"
-            />
-          </div>
-        </div>
-      </template>
-      <template #secondary>
-        <HttpResponse :document="tab.document" :is-embed="true" />
-      </template>
-    </AppPaneLayout>
-
-    <FirebaseLogin v-if="showLogin" @hide-modal="showLogin = false" />
-  </template>
+  <component
+    :is="platform.ui?.additionalEmbedsComponent"
+    v-if="showCustomEmbedsView"
+    v-bind="props"
+  />
 
   <AppPaneLayout v-else layout-id="embed-primary" :is-embed="true">
     <template #primary>
@@ -81,12 +30,9 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, useModel } from "vue"
+import { computed, useModel } from "vue"
 import { ref } from "vue"
 import { HoppTab } from "~/services/tab"
-import { useI18n } from "~/composables/i18n"
-import { useReadonlyStream } from "~/composables/stream"
-import { useColorMode } from "~/composables/theming"
 import { HoppRequestDocument } from "~/helpers/rest/document"
 import { platform } from "~/platform"
 import { RESTOptionTabs } from "../http/RequestOptions.vue"
@@ -101,63 +47,12 @@ const tab = useModel(props, "modelTab")
 
 const selectedOptionTab = ref<RESTOptionTabs>(props.properties[0])
 
-const organizationDomain = ref<string>("")
-
-const pageLoading = ref(false)
-const showLogin = ref(false)
-
-const currentUser = useReadonlyStream(
-  platform.auth.getCurrentUserStream(),
-  platform.auth.getCurrentUser()
+const shortcodeBaseURL = computed(
+  () => import.meta.env.VITE_SHORTCODE_BASE_URL ?? "https://hopp.sh"
 )
-const probableUser = useReadonlyStream(
-  platform.auth.getProbableUserStream(),
-  platform.auth.getProbableUser()
-)
-
-const t = useI18n()
-const colorMode = useColorMode()
-
-const loadingCurrentUser = computed(() => {
-  if (!probableUser.value) return false
-  else if (!currentUser.value) return true
-  return false
-})
-
-onMounted(async () => {
-  pageLoading.value = true
-
-  const { organization } = platform
-
-  if (!organization || organization.isDefaultCloudInstance) {
-    pageLoading.value = false
-    return
-  }
-
-  const orgInfo = await organization.getOrgInfo()
-
-  if (orgInfo) {
-    const { orgDomain } = orgInfo
-
-    organizationDomain.value = orgDomain
-  }
-
-  pageLoading.value = false
-})
-
-const shortcodeBaseURL = computed(() => {
-  const { organization } = platform
-
-  if (!organization || organization.isDefaultCloudInstance) {
-    return import.meta.env.VITE_SHORTCODE_BASE_URL ?? "https://hopp.sh"
-  }
-
-  const rootDomain = organization.getRootDomain()
-  return `https://${organizationDomain.value}.${rootDomain}`
-})
 
 const sharedRequestURL = computed(() => {
-  return `${shortcodeBaseURL.value}/r/${props.sharedRequestID}`
+  return `${shortcodeBaseURL.value}/e/${props.sharedRequestID}`
 })
 
 const tabRequestVariables = computed(() => {
@@ -167,5 +62,15 @@ const tabRequestVariables = computed(() => {
     secret: false,
     sourceEnv: "RequestVariable",
   }))
+})
+
+const showCustomEmbedsView = computed(() => {
+  const { organization, ui } = platform
+
+  return (
+    organization &&
+    !organization.isDefaultCloudInstance &&
+    ui?.additionalEmbedsComponent
+  )
 })
 </script>

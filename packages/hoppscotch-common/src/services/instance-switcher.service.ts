8c67c832d245a260de3ba77d5424af8530b31dcf
jamesgeorge007 <25279263+jamesgeorge007@users.noreply.github.com>
2025-03-19 14:41:07 +0530

    chore: bump version to `2025.2.3`

--- packages/hoppscotch-common/src/services/instance-switcher.service.ts
+++ packages/hoppscotch-common/src/services/instance-switcher.service.ts
@@ -48,7 +48,7 @@ export class InstanceSwitcherService extends Service<ConnectionState> {
       const instance: VendoredInstance = {
         type: "vendored",
         displayName: "Hoppscotch",
-        version: "25.2.2",
+        version: "25.2.3",
       }
 
       this.state$.next({
@@ -121,7 +121,7 @@ export class InstanceSwitcherService extends Service<ConnectionState> {
       const instance: VendoredInstance = {
         type: "vendored",
         displayName: "Hoppscotch",
-        version: "25.2.2",
+        version: "25.2.3",
       }
 
       this.state$.next({

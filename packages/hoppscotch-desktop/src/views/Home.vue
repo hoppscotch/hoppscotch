d674a2e8d209764e66e408315c0f11247794a4ab
jamesgeorge007 <25279263+jamesgeorge007@users.noreply.github.com>
2025-06-25 14:15:28 +0530

    chore: bump version to `2025.6.0`

--- packages/hoppscotch-desktop/src/views/Home.vue
+++ packages/hoppscotch-desktop/src/views/Home.vue
@@ -299,7 +299,7 @@ const loadVendored = async () => {
     const vendoredInstance: VendoredInstance = {
       type: "vendored",
       displayName: "Hoppscotch",
-      version: "25.5.4",
+      version: "25.6.0",
     }
 
     const connectionState: ConnectionState = {

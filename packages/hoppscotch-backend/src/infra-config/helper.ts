2cc28d826b637fae66aaecc2957b5c4546e4ed93
mirarifhasan <arif.ishan05@gmail.com>
2025-07-28 20:47:16 +0600

    Revert "feat: using infraConfig in bootstrap"
    This reverts commit 147dba632f095dad816afdd4e46ed736e8a3b8ff.

--- packages/hoppscotch-backend/src/infra-config/helper.ts
+++ packages/hoppscotch-backend/src/infra-config/helper.ts
@@ -4,7 +4,6 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { InfraConfigEnum } from 'src/types/InfraConfig';
 import { decrypt, encrypt } from 'src/utils';
 import { randomBytes } from 'crypto';
-import { ConfigService } from '@nestjs/config';
 
 export enum ServiceStatus {
   ENABLE = 'ENABLE',
@@ -394,9 +393,7 @@ export async function getConfiguredSSOProvidersFromInfraConfig() {
   const providerConfigKeys = getAuthProviderRequiredKeys(env);
 
   const allowedAuthProviders: string[] =
-    env['INFRA'].VITE_ALLOWED_AUTH_PROVIDERS?.split(',')
-      .map((p) => p.trim())
-      .filter((p) => p) ?? [];
+    env['INFRA'].VITE_ALLOWED_AUTH_PROVIDERS?.split(',') ?? [];
 
   const configuredAuthProviders = allowedAuthProviders.filter((provider) => {
     const requiredKeys = providerConfigKeys[provider];

a0fbb7b076aea5788813cccc617fe2717d009b2c
Shreyas <CuriousCorrelation@gmail.com>
2025-08-21 16:24:30 +0530

    chore(native): bump and align dependencies (#5331)
    - This standardises package versions between desktop, agent, appload, relay
      all the native components to resolve version inconsistencies and prepare
      for unified bumps in the future.
    
    - Account for recent minor dependency bumps as a follow-up to #5329
    
    Co-authored-by: jamesgeorge007 <25279263+jamesgeorge007@users.noreply.github.com>

--- packages/hoppscotch-desktop/src/main.ts
+++ packages/hoppscotch-desktop/src/main.ts
@@ -2,13 +2,13 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 
-import '@hoppscotch/ui/style.css'
-import './assets/scss/styles.scss'
-import './assets/scss/tailwind.scss'
-import '@fontsource-variable/inter'
-import '@fontsource-variable/material-symbols-rounded'
-import '@fontsource-variable/roboto-mono'
+import "@hoppscotch/ui/style.css"
+import "./assets/scss/styles.scss"
+import "./assets/scss/tailwind.scss"
+import "@fontsource-variable/inter"
+import "@fontsource-variable/material-symbols-rounded"
+import "@fontsource-variable/roboto-mono"
 
 const app = createApp(App)
 app.use(router)
-app.mount('#app')
+app.mount("#app")

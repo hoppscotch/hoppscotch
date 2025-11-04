1d7c1d411ac51cc4875c9811488c502ea0aeb219
curiouscorrelation <curiouscorrelation@gmail.com>
2025-11-04 15:05:53 +0530

    chore: update `devenv`

--- devenv.nix
+++ devenv.nix
@@ -167,6 +167,10 @@ in {
       npm.enable = true;
       pnpm.enable = true;
     };
+    go = {
+      enable = true;
+      package = pkgs.go_1_24;
+    };
     rust = {
       enable = true;
       channel = "nightly";

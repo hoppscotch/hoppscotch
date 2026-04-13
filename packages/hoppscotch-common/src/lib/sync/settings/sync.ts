2571ba966cc44b8657022b484ea3608b464f242b
Anwarul Islam <anwaarulislaam@gmail.com>
2026-04-13 13:34:45 +0600

    feat(settings): add user settings management and synchronization functions

---
+++ packages/hoppscotch-common/src/lib/sync/settings/sync.ts
@@ -0,0 +1,26 @@
+import { settingsStore } from "~/newstore/settings"
+import { getSyncInitFunction, StoreSyncDefinitionOf } from ".."
+import { updateUserSettings } from "./api"
+
+export const settingsSyncDefinition: StoreSyncDefinitionOf<
+  typeof settingsStore
+> = {
+  toggleSetting() {
+    updateUserSettings(JSON.stringify(settingsStore.value))
+  },
+  toggleNestedSetting() {
+    updateUserSettings(JSON.stringify(settingsStore.value))
+  },
+  applySetting() {
+    updateUserSettings(JSON.stringify(settingsStore.value))
+  },
+  applyNestedSetting() {
+    updateUserSettings(JSON.stringify(settingsStore.value))
+  },
+}
+
+export const settingsSyncer = getSyncInitFunction(
+  settingsStore,
+  settingsSyncDefinition,
+  () => true
+)

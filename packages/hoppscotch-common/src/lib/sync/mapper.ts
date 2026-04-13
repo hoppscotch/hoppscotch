0a19f1f9f140999b5af1316fabfe1b790db843b7
Anwarul Islam <anwaarulislaam@gmail.com>
2026-04-13 13:06:53 +0600

    feat(sync): implement collection, environment, and history synchronization

---
+++ packages/hoppscotch-common/src/lib/sync/mapper.ts
@@ -0,0 +1,42 @@
+export const createMapper = <
+  LocalIDType extends string | number,
+  BackendIDType extends string | number,
+>() => {
+  const backendIDByLocalIDMap = new Map<
+    LocalIDType,
+    BackendIDType | undefined
+  >()
+  const localIDByBackendIDMap = new Map<
+    BackendIDType,
+    LocalIDType | undefined
+  >()
+
+  return {
+    addEntry(localIdentifier: LocalIDType, backendIdentifier: BackendIDType) {
+      backendIDByLocalIDMap.set(localIdentifier, backendIdentifier)
+      localIDByBackendIDMap.set(backendIdentifier, localIdentifier)
+    },
+    getValue() {
+      return backendIDByLocalIDMap
+    },
+    getBackendIDByLocalID(localIdentifier: LocalIDType) {
+      return backendIDByLocalIDMap.get(localIdentifier)
+    },
+    getLocalIDByBackendID(backendId: BackendIDType) {
+      return localIDByBackendIDMap.get(backendId)
+    },
+    removeEntry(backendId?: BackendIDType, index?: LocalIDType) {
+      if (backendId) {
+        const index = localIDByBackendIDMap.get(backendId)
+
+        localIDByBackendIDMap.delete(backendId)
+        index && backendIDByLocalIDMap.delete(index)
+      } else if (index) {
+        const backendId = backendIDByLocalIDMap.get(index)
+
+        backendIDByLocalIDMap.delete(index)
+        backendId && localIDByBackendIDMap.delete(backendId)
+      }
+    },
+  }
+}

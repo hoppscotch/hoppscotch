303eecb1dd20bcafbd75cf0cb3a35f8aec8c8d1a
Anwarul Islam <anwaarulislaam@gmail.com>
2026-04-13 13:34:45 +0600

    feat(settings): add user settings management and synchronization functions

---
+++ packages/hoppscotch-common/src/lib/sync/settings/index.ts
@@ -0,0 +1,87 @@
+import * as E from "fp-ts/Either"
+import { runGQLSubscription } from "~/helpers/backend/GQLClient"
+import { bulkApplySettings, getDefaultSettings } from "~/newstore/settings"
+import { platform } from "~/platform"
+import { SettingsPlatformDef } from "~/platform/settings"
+import { runDispatchWithOutSyncing } from ".."
+import {
+  createUserSettings,
+  getUserSettings,
+  runUserSettingsUpdatedSubscription,
+} from "./api"
+import { settingsSyncer } from "./sync"
+
+const authEvents$ = platform.auth.getAuthEventsStream()
+
+function initSettingsSync() {
+  const currentUser$ = platform.auth.getCurrentUserStream()
+  settingsSyncer.startStoreSync()
+  settingsSyncer.setupSubscriptions(setupSubscriptions)
+
+  // load the settings
+
+  loadUserSettings()
+
+  currentUser$.subscribe(async (user) => {
+    if (user) {
+      // load the settings
+      loadUserSettings()
+    }
+  })
+
+  authEvents$.subscribe((event) => {
+    if (event.event == "login" || event.event == "token_refresh") {
+      settingsSyncer.startListeningToSubscriptions()
+    }
+
+    if (event.event == "logout") {
+      settingsSyncer.stopListeningToSubscriptions()
+    }
+  })
+}
+
+async function loadUserSettings() {
+  const res = await getUserSettings()
+
+  // create user settings if it doesn't exist
+  E.isLeft(res) &&
+    res.left.error == "user_settings/not_found" &&
+    (await createUserSettings(JSON.stringify(getDefaultSettings())))
+
+  if (E.isRight(res)) {
+    runDispatchWithOutSyncing(() => {
+      bulkApplySettings(JSON.parse(res.right.me.settings.properties))
+    })
+  }
+}
+
+function setupSubscriptions() {
+  let subs: ReturnType<typeof runGQLSubscription>[1][] = []
+
+  const userSettingsUpdatedSub = setupUserSettingsUpdatedSubscription()
+
+  subs = [userSettingsUpdatedSub]
+
+  return () => {
+    subs.forEach((sub) => sub.unsubscribe())
+  }
+}
+
+function setupUserSettingsUpdatedSubscription() {
+  const [userSettingsUpdated$, userSettingsUpdatedSub] =
+    runUserSettingsUpdatedSubscription()
+
+  userSettingsUpdated$.subscribe((res) => {
+    if (E.isRight(res)) {
+      runDispatchWithOutSyncing(() => {
+        bulkApplySettings(JSON.parse(res.right.userSettingsUpdated.properties))
+      })
+    }
+  })
+
+  return userSettingsUpdatedSub
+}
+
+export const def: SettingsPlatformDef = {
+  initSettingsSync,
+}

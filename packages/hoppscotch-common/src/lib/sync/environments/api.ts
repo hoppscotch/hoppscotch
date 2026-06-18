b45067276434a2d62e5de648f85410c92b373b24
Anwarul Islam <anwaarulislaam@gmail.com>
2026-06-19 00:40:38 +0600

    feat(common): global env error message correction

--- packages/hoppscotch-common/src/lib/sync/environments/api.ts
+++ packages/hoppscotch-common/src/lib/sync/environments/api.ts
@@ -87,7 +87,7 @@ export const getGlobalEnvironments = () =>
   runGQLQuery<
     GetGlobalEnvironmentsQuery,
     GetGlobalEnvironmentsQueryVariables,
-    "user_environment/user_env_does_not_exist"
+    "user_environment/global_env_does_not_exist"
   >({
     query: GetGlobalEnvironmentsDocument,
     variables: {},

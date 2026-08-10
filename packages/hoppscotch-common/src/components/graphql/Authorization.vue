bf774861d7cb6630005c4cde8203bdb4c6db6763
Mohiuddin <mohiuddin3551@gmail.com>
2026-08-10 18:15:05 +0000

    fix: rename bearer auth label to bearer token

--- packages/hoppscotch-common/src/components/graphql/Authorization.vue
+++ packages/hoppscotch-common/src/components/graphql/Authorization.vue
@@ -296,7 +296,7 @@ const authTypes: AuthType[] = [
   },
   {
     key: "bearer",
-    label: "Bearer",
+    label: "Bearer Token",
   },
   {
     key: "oauth-2",
@@ -317,7 +317,7 @@ const authTypes: AuthType[] = [
 
 const AUTH_KEY_NAME: Record<HoppGQLAuth["authType"], string> = {
   basic: "Basic Auth",
-  bearer: "Bearer",
+  bearer: "Bearer Token",
   "oauth-2": "OAuth 2.0",
   "api-key": "API key",
   none: "None",
